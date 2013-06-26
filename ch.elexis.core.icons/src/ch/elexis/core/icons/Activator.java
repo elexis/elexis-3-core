06ff4659b5373a5c34f10f6f0ee99e4748d539ce
Marco Descher <marco@descher.at>
2013-06-26 12:47:38 +0200

    [1603] Core Icon Set

---
+++ ch.elexis.core.icons/src/ch/elexis/core/icons/Activator.java
@@ -0,0 +1,32 @@
+package ch.elexis.core.icons;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	public static final String PLUGIN_ID = "ch.elexis.core.icons";
+	
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
