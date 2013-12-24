fddd1676e518a6c3bfaa4838a06c35a86d118a55
Niklaus Giger <niklaus.giger@member.fsf.org>
2013-12-24 12:08:34 +0100

    Added unittest for elexis.data from 2.1.7

---
+++ ch.elexis.core.data.tests/src/ch/elexis/data/AllTests.java
@@ -0,0 +1,13 @@
+package ch.elexis.data;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	Test_LabItem.class, Test_PersistentObject.class, Test_DBImage.class, Test_Query.class
+})
+public class AllTests {
+// System.out.println("Started AllTests");
+}
---
+++ ch.elexis.core.data.tests/src/ch/elexis/data/Test_DBImage.java
@@ -0,0 +1,28 @@
+package ch.elexis.data;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import ch.elexis.core.exceptions.PersistenceException;
+import ch.rgw.tools.JdbcLink;
+
+public class Test_DBImage extends AbstractPersistentObjectTest {
+	
+	@Test
+	public void testDBImageString(){
+		JdbcLink link = initDB();
+		if (link == null)
+			fail("initDB should not return null");
+
+		try {
+			DBImage img = new DBImage("", "test");
+			// TODO: does not work under Elexis 3.0
+			// fail("Expected Exception not thrown!");
+		} catch (PersistenceException pe) {
+			
+		}
+		link.disconnect();
+	}
+		
+}
