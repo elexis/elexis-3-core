a3c594f13e3de86441abe9911ba39ae9f78f29f1
Marco Descher <marco@descher.at>
2014-01-30 09:00:14 +0100

    Provide internal support classes for JDT null analysis

---
+++ ch.elexis.core/src/ch/elexis/core/jdt/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package provides the required annotated interfaces to use null analysis within Elexis development.
+ * This feature is not activated by default, if you want to use it, please consider the enclosed references.
+ * 
+ * @see <a href="http://wiki.eclipse.org/JDT_Core/Null_Analysis">Null Analysis</a>
+ * @see <a href="http://help.eclipse.org/kepler/index.jsp?topic=%2Forg.eclipse.jdt.doc.user%2Ftasks%2Ftask-using_null_annotations.htm">Using null annotations</a>
+ * @since 3.0.0
+ */
+package ch.elexis.core.jdt;
