1ea59c0d8eab4575d40a0a92e07affa867e36b35
Marco Descher <marco@descher.at>
2014-03-20 11:08:27 +0100

    [2139] Added BenchmarkTimer class

---
+++ ch.elexis.core.data/src/ch/elexis/core/data/util/BenchmarkTimer.java
@@ -0,0 +1,52 @@
+package ch.elexis.core.data.util;
+
+/**
+ * This class can be used for simple benchmarks.
+ */
+public class BenchmarkTimer {
+	
+	private long startTime = 0;
+	private long endTime = 0;
+	
+	public void start(){
+		this.startTime = System.currentTimeMillis();
+	}
+	
+	public void end(){
+		this.endTime = System.currentTimeMillis();
+	}
+	
+	public long getStartTime(){
+		return this.startTime;
+	}
+	
+	public long getEndTime(){
+		return this.endTime;
+	}
+	
+	public long getTotalTime(){
+		return this.endTime - this.startTime;
+	}
+	
+	public String geTotalTimeHumanReadableString() {
+		return convertMS(getTotalTime());
+	}
+	
+	private String convertMS(long ms) {
+	    int seconds = (int) ((ms / 1000) % 60);
+	    int minutes = (int) (((ms / 1000) / 60) % 60);
+	    int hours = (int) ((((ms / 1000) / 60) / 60) % 24);
+
+	    String sec, min, hrs;
+	    if(seconds<10)  sec="0"+seconds;
+	    else            sec= ""+seconds;
+	    if(minutes<10)  min="0"+minutes;
+	    else            min= ""+minutes;
+	    if(hours<10)    hrs="0"+hours;
+	    else            hrs= ""+hours;
+
+	    if(hours == 0)  return min+" min, "+sec+" sec";
+	    else    return hrs+" hrs, "+min+"min, "+sec+" sec";
+	}
+	
+}
