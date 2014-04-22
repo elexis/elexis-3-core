2943815128061adb72c7245c5fb09571a99f641d
lamann <amann@medevit.at>
2014-04-22 11:23:07 +0200

    [1655] changed case sorting (open cases before, closed)

--- ch.elexis.core.ui/src/ch/elexis/core/ui/views/provider/FaelleContentProvider.java
+++ ch.elexis.core.ui/src/ch/elexis/core/ui/views/provider/FaelleContentProvider.java
@@ -1,10 +1,14 @@
 package ch.elexis.core.ui.views.provider;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.Viewer;
 
 import ch.elexis.core.data.events.ElexisEventDispatcher;
 import ch.elexis.core.ui.views.FaelleView;
+import ch.elexis.data.Fall;
 import ch.elexis.data.Patient;
 
 /**
@@ -17,13 +21,23 @@ public class FaelleContentProvider implements IStructuredContentProvider {
 		if (act == null) {
 			return new Object[0];
 		} else {
-			return act.getFaelle();
+			Fall[] cases = act.getFaelle();
+			List<Fall> caseList = new ArrayList<Fall>();
+			
+			for (Fall fall : cases) {
+				if (fall.isOpen()) {
+					caseList.add(0, fall);
+				} else {
+					caseList.add(fall);
+				}
+			}
+			return caseList.toArray();
 		}
 		
 	}
 	
 	public void dispose(){
-		// TODO Auto-generated method stub	
+		// TODO Auto-generated method stub
 	}
 	
 	public void inputChanged(final Viewer viewer, final Object oldInput, final Object newInput){
