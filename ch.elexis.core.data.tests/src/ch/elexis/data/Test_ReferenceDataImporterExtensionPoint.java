a3a681916c61114b7231d23bf472208cd0e1327e
lamann <amann@medevit.at>
2014-05-15 16:11:33 +0200

    [2110] removed tarmed tests from core.data.tests

--- ch.elexis.core.data.tests/src/ch/elexis/data/Test_ReferenceDataImporterExtensionPoint.java
+++ ch.elexis.core.data.tests/src/ch/elexis/data/Test_ReferenceDataImporterExtensionPoint.java
@@ -1,50 +1,20 @@
 package ch.elexis.data;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotSame;
-import static org.junit.Assert.assertTrue;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.util.List;
 
-import org.eclipse.core.runtime.IStatus;
 import org.junit.Test;
 
 import ch.elexis.core.data.extension.ReferenceDataImporterExtensionPoint;
 import ch.elexis.core.data.interfaces.AbstractReferenceDataImporter;
 
 public class Test_ReferenceDataImporterExtensionPoint {
-	private static final String TARMED_ID = "tarmed";
 	
 	@Test
 	public void testGetImporters(){
 		List<AbstractReferenceDataImporter> importers =
 			ReferenceDataImporterExtensionPoint.getImporters();
-		assertNotSame(0, importers.size());
-		
-		boolean hasTarmedImporter = false;
-		for (AbstractReferenceDataImporter importer : importers) {
-			if (TARMED_ID.equals(importer.getReferenceDataIdResponsibleFor())) {
-				hasTarmedImporter = true;
-			}
-		}
-		assertTrue(hasTarmedImporter);
+		assertEquals(0, importers.size());
 	}
-	
-	@Test
-	public void testUpdate() throws FileNotFoundException{
-		File tarmedFile =
-			new File(System.getProperty("user.dir") + File.separator + "rsc" + File.separator
-				+ "tarmed.mdb");
-		InputStream tarmedInStream = new FileInputStream(tarmedFile);
-		
-		IStatus retStatus =
-			ReferenceDataImporterExtensionPoint.update(TARMED_ID, null, tarmedInStream);
-		
-		assertEquals(IStatus.OK, retStatus.getCode());
-	}
-	
 }
