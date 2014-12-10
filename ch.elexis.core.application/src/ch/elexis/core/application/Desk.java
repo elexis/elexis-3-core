522e1b73675d75513930419fb1866719e6632e87
Marco Descher <marco@descher.at>
2014-12-10 08:50:14 +0100

    [3133] Incomprehensible error message on database connection failure

--- ch.elexis.core.application/src/ch/elexis/core/application/Desk.java
+++ ch.elexis.core.application/src/ch/elexis/core/application/Desk.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 MEDEVIT <office@medevit.at>.
+ * Copyright (c) 2013-2014 MEDEVIT <office@medevit.at>.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,6 +14,8 @@ import java.util.Map;
 
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,7 +25,6 @@ import ch.elexis.core.data.activator.CoreHub;
 import ch.elexis.core.data.extension.AbstractCoreOperationAdvisor;
 import ch.elexis.core.data.extension.CoreOperationExtensionPoint;
 import ch.elexis.core.data.preferences.CorePreferenceInitializer;
-import ch.elexis.core.exceptions.PersistenceException;
 import ch.elexis.core.ui.UiDesk;
 import ch.elexis.data.PersistentObject;
 import ch.rgw.io.FileTool;
@@ -49,9 +50,22 @@ public class Desk implements IApplication {
 		try {
 			if (PersistentObject.connect(CoreHub.localCfg) == false)
 				log.error(PersistentObject.class.getName() + " initialization failed.");
-		} catch (PersistenceException pe) {
-			log.error("Initialization error", pe);
+		} catch (Throwable pe) {
+			// error in database connection, we have to exit
+			log.error("Database connection error", pe);
 			pe.printStackTrace();
+			
+			Shell shell = PlatformUI.createDisplay().getActiveShell();
+			StringBuilder sb = new StringBuilder();
+			sb.append("Could not open database connection. Quitting Elexis.\n\n");
+			sb.append("Message: " + pe.getMessage()+"\n\n");
+			while (pe.getCause() != null) {
+				pe = pe.getCause();
+				sb.append("Reason: "+pe.getMessage()+"\n");
+			}
+			MessageDialog.openError(shell, "Error in database connection", sb.toString());
+			
+			return IApplication.EXIT_OK;
 		}
 		
 		// check for initialization parameters
