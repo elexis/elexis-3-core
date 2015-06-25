400f625c1ca5a588abd2a71b0daed323edec60ea
Marco Descher <marco@descher.at>
2015-06-25 10:06:23 +0200

    Revert "[3756]log Error mandantCfg has no userCfg"
    This reverts commit 9f28106d7f3ddc89fd64483076c0baa186da58d4.

--- ch.elexis.core.data/src/ch/elexis/core/data/activator/CoreHub.java
+++ ch.elexis.core.data/src/ch/elexis/core/data/activator/CoreHub.java
@@ -347,8 +347,6 @@ public class CoreHub implements BundleActivator {
 	}
 	
 	public static void setMandant(Mandant newMandant){
-		if (newMandant != null)
-			System.out.println("setMandant.getMandantLabel: " + newMandant.getMandantLabel());
 		if (actMandant != null && mandantCfg != null) {
 			mandantCfg.flush();
 		}
@@ -356,10 +354,6 @@ public class CoreHub implements BundleActivator {
 			mandantCfg = userCfg;
 		} else {
 			mandantCfg = getUserSetting(newMandant);
-			if (mandantCfg == null) {
-				log.error("Mandant " + newMandant.getMandantLabel() + " has no UserSetting. Should have IS_USER (istAnwender) flag in contact details" ); //$NON-NLS-1$
-				mandantCfg = userCfg;
-			}
 		}
 		
 		actMandant = newMandant;
