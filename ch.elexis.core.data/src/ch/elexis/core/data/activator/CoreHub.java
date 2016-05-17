6c7f7bc0537bbc73e45dad63e48da17a9621cea1
Marco Descher <marco@descher.at>
2016-05-17 12:36:38 +0200

    [4698] Briefe#NOTE requires TEXT instead of VARCHAR(255)

--- ch.elexis.core.data/src/ch/elexis/core/data/activator/CoreHub.java
+++ ch.elexis.core.data/src/ch/elexis/core/data/activator/CoreHub.java
@@ -70,7 +70,7 @@ public class CoreHub implements BundleActivator {
 	public static String Version = "3.2.0.qualifier"; //$NON-NLS-1$
 	public static final String APPLICATION_NAME = "Elexis Core"; //$NON-NLS-1$
 	static final String neededJRE = "1.8.0"; //$NON-NLS-1$
-	public static final String DBVersion = "3.2.0"; //$NON-NLS-1$
+	public static final String DBVersion = "3.2.1"; //$NON-NLS-1$
 	
 	protected static Logger log = LoggerFactory.getLogger(CoreHub.class.getName());
 	
--- ch.elexis.core.data/src/ch/elexis/core/data/util/DBUpdate.java
+++ ch.elexis.core.data/src/ch/elexis/core/data/util/DBUpdate.java
@@ -48,7 +48,7 @@ public class DBUpdate {
 		"1.4.5", "1.4.6", "1.5.0", "1.6.0", "1.6.1", "1.6.2", "1.6.3", "1.6.4", "1.7.0", "1.7.1",
 		"1.7.2", "1.8.0", "1.8.1", "1.8.2", "1.8.3", "1.8.4", "1.8.5", "1.8.6", "1.8.7", "1.8.8",
 		"1.8.9", "1.8.10", "1.8.11", "1.8.12", "1.8.13", "1.8.14", "1.8.15", "1.8.16", "1.9.0", "3.0.0",
-		"3.1.0", "3.2.0"
+		"3.1.0", "3.2.0", "3.2.1"
 	};
 	static final String[] cmds =
 		{
@@ -390,7 +390,10 @@ public class DBUpdate {
 			ALTER_TABLE+Brief.TABLENAME+" MODIFY "+Brief.FLD_SUBJECT+" VARCHAR(255);"+
 			ALTER_TABLE+Brief.TABLENAME+" ADD "+Brief.FLD_NOTE+" VARCHAR(255);"+
 			ALTER_TABLE+Brief.TABLENAME+" MODIFY "+Brief.FLD_DATE+" CHAR(24);"+
-			ALTER_TABLE+Brief.TABLENAME+" MODIFY "+Brief.FLD_DATE_MODIFIED+" CHAR(24);"
+			ALTER_TABLE+Brief.TABLENAME+" MODIFY "+Brief.FLD_DATE_MODIFIED+" CHAR(24);",
+			
+			// 3.2.1
+			ALTER_TABLE+Brief.TABLENAME+" MODIFY "+Brief.FLD_NOTE+" TEXT;"
 			};
 	//@formatter:on
 	
