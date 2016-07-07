6c3334669e5ae47174f0828c000af47949a3d193
Marco Descher <marco@descher.at>
2016-07-07 13:32:00 +0200

    [4754] Formula-based lab-results are not submitted to RH

--- ch.elexis.core/src/ch/elexis/core/types/LabItemTyp.java
+++ ch.elexis.core/src/ch/elexis/core/types/LabItemTyp.java
@@ -1,5 +1,35 @@
 package ch.elexis.core.types;
 
+import ch.elexis.core.jdt.Nullable;
+
 public enum LabItemTyp {
-	NUMERIC, TEXT, ABSOLUTE, FORMULA, DOCUMENT
+		NUMERIC(0), TEXT(1), ABSOLUTE(2), FORMULA(3), DOCUMENT(4);
+	
+	private int type;
+	
+	private LabItemTyp(int type){
+		this.type = type;
+	}
+	
+	public int getType(){
+		return type;
+	}
+	
+	public static @Nullable LabItemTyp fromType(int type){
+		for (LabItemTyp lit : LabItemTyp.values()) {
+			if (type == lit.getType()) {
+				return lit;
+			}
+		}
+		return null;
+	}
+	
+	public static @Nullable LabItemTyp fromType(String type){
+		try {
+			int parseInt = Integer.parseInt(type);
+			return LabItemTyp.fromType(parseInt);
+		} catch (NumberFormatException nfe) {
+			return null;
+		}
+	}
 }
