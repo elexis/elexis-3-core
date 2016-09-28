593c1831f1fac9b4e4077e1eb24ab2f194fcfdba
Marco Descher <marco@descher.at>
2016-09-28 07:55:11 +0200

    More info on trying to unlock a null object

--- ch.elexis.core.ui/src/ch/elexis/core/ui/locks/LockResponseHelper.java
+++ ch.elexis.core.ui/src/ch/elexis/core/ui/locks/LockResponseHelper.java
@@ -3,6 +3,7 @@ package ch.elexis.core.ui.locks;
 import java.text.MessageFormat;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import ch.elexis.core.lock.types.LockResponse;
 import ch.elexis.core.model.IPersistentObject;
@@ -11,12 +12,20 @@ import ch.elexis.core.ui.util.SWTHelper;
 public class LockResponseHelper {
 	
 	public static void showInfo(LockResponse lr, IPersistentObject object, Logger log){
+		if (object == null) {
+			if (log == null) {
+				log = LoggerFactory.getLogger(LockResponseHelper.class);
+			}
+			log.warn("showInfo for null object", new Throwable());
+		}
+		
 		if (LockResponse.Status.DENIED_PERMANENT == lr.getStatus()) {
 			SWTHelper.showError(Messages.DenyLockPermanent_Title,
 				Messages.DenyLockPermanent_Message);
 		} else {
 			if (log != null) {
-				log.warn("Unable to acquire lock for " + object.storeToString() + " - "
+				log.warn("Unable to acquire lock for "
+					+ ((object != null) ? object.storeToString() : "null") + " - "
 					+ lr.getLockInfos().getUser() + "@" + lr.getLockInfos().getSystemUuid());
 			}
 			String format = MessageFormat.format(Messages.DenyLock_Message,
