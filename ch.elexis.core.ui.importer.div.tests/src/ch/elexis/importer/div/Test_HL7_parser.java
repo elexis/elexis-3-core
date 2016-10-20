2bed37731a193b0ec42834dcd66196f9660e2a25
Niklaus Giger <niklaus.giger@member.fsf.org>
2016-10-20 16:02:45 +0200

    [4724]Add missing import

--- ch.elexis.core.ui.importer.div.tests/src/ch/elexis/importer/div/Test_HL7_parser.java
+++ ch.elexis.core.ui.importer.div.tests/src/ch/elexis/importer/div/Test_HL7_parser.java
@@ -23,9 +23,14 @@
  *******************************************************************************/
 package ch.elexis.importer.div;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
 
