87260d5dff1b79dae4ee152e87812027431564ef
thomashu <huster@medevit.at>
2016-11-22 17:29:48 +0100

    [5095] added tarmed to findings code systems

--- ch.elexis.core.findings/src/ch/elexis/core/findings/codes/CodingSystem.java
+++ ch.elexis.core.findings/src/ch/elexis/core/findings/codes/CodingSystem.java
@@ -4,10 +4,11 @@ public enum CodingSystem {
 	ELEXIS_COVERAGE_TYPE("www.elexis.info/coverage/type"), ELEXIS_DIAGNOSE_TESSINERCODE(
 			"www.elexis.info/diagnose/tessinercode"), ELEXIS_PRACTITIONER_ROLE(
 					"www.elexis.info/practitioner/role"), ELEXIS_LOCAL_CODESYSTEM(
-			"www.elexis.info/coding/local"),
-		ELEXIS_ENCOUNTER_TYPE("www.elexis.info/encounter/type"),
-		ELEXIS_ICD_CODESYSTEM("www.elexis.info/coding/icd"),
-		ELEXIS_ICPC_CODESYSTEM("www.elexis.info/coding/icpc");
+							"www.elexis.info/coding/local"), ELEXIS_ENCOUNTER_TYPE(
+									"www.elexis.info/encounter/type"), ELEXIS_ICD_CODESYSTEM(
+											"www.elexis.info/coding/icd"), ELEXIS_ICPC_CODESYSTEM(
+													"www.elexis.info/coding/icpc"), ELEXIS_TARMED_CODESYSTEM(
+															"www.elexis.info/billing/tarmed");
 
 	private String system;
 	
