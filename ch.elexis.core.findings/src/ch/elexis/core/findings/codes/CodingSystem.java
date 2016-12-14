a20d75e5bfbba99c63cac0016eaa6efcd36471a9
thomashu <huster@medevit.at>
2016-12-14 10:04:17 +0100

    [5095] updated laboratory coding systems

--- ch.elexis.core.findings/src/ch/elexis/core/findings/codes/CodingSystem.java
+++ ch.elexis.core.findings/src/ch/elexis/core/findings/codes/CodingSystem.java
@@ -4,11 +4,13 @@ public enum CodingSystem {
 	ELEXIS_COVERAGE_TYPE("www.elexis.info/coverage/type"), ELEXIS_DIAGNOSE_TESSINERCODE(
 			"www.elexis.info/diagnose/tessinercode"), ELEXIS_PRACTITIONER_ROLE(
 					"www.elexis.info/practitioner/role"), ELEXIS_LOCAL_CODESYSTEM(
-							"www.elexis.info/coding/local"), ELEXIS_ENCOUNTER_TYPE(
-									"www.elexis.info/encounter/type"), ICD_DE_CODESYSTEM(
-											"http://hl7.org/fhir/sid/icd-10-de"), ICPC2_CODESYSTEM(
-													"http://hl7.org/fhir/sid/icpc-2"), ELEXIS_TARMED_CODESYSTEM(
-															"www.elexis.info/billing/tarmed");
+							"www.elexis.info/coding/local"), ELEXIS_LOCAL_LABORATORY(
+									"www.elexis.info/laboratory/local"), ELEXIS_LOCAL_LABORATORY_VITOLABKEY(
+											"www.elexis.info/laboratory/local/vitolabkey"), ELEXIS_ENCOUNTER_TYPE(
+													"www.elexis.info/encounter/type"), ICD_DE_CODESYSTEM(
+															"http://hl7.org/fhir/sid/icd-10-de"), ICPC2_CODESYSTEM(
+																	"http://hl7.org/fhir/sid/icpc-2"), ELEXIS_TARMED_CODESYSTEM(
+																			"www.elexis.info/billing/tarmed");
 
 	private String system;
 
