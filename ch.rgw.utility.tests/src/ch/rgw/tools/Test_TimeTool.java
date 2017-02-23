11c236fe47e3f41198b9c959a86e3527995ecce0
Niklaus Giger <niklaus.giger@member.fsf.org>
2017-02-23 17:00:44 +0100

    [5684]Fix compilation problem under 3.1-maintenance

--- ch.rgw.utility.tests/src/ch/rgw/tools/Test_TimeTool.java
+++ ch.rgw.utility.tests/src/ch/rgw/tools/Test_TimeTool.java
@@ -1,6 +1,7 @@
 package ch.rgw.tools;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
