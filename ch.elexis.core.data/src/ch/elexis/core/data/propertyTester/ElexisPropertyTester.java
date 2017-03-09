664bb7d9172ea79994ceda88db453b4bf0185a74
sazgin <azgin@medevit.at>
2017-03-09 11:44:37 +0100

    [5707] Bei Standalone Modus soll LockingSymbol (rechts unten) gar nicht angezeigt werden

--- ch.elexis.core.data/src/ch/elexis/core/data/propertyTester/ElexisPropertyTester.java
+++ ch.elexis.core.data/src/ch/elexis/core/data/propertyTester/ElexisPropertyTester.java
@@ -7,26 +7,15 @@ import ch.elexis.core.data.activator.CoreHub;
 import ch.elexis.core.lock.ILocalLockService.Status;
 
 public class ElexisPropertyTester extends PropertyTester {
-	
-	private static final String STANDALONE = "STANDALONE";
+
+	private static final String PROP_STANDALONE = "STANDALONE";
 
 	@Override
 	public boolean test(Object receiver, String property, Object[] args, Object expectedValue){
-		if (property != null)
+		if (PROP_STANDALONE.equals(property))
 		{
-			switch (property)
-			{
-				case STANDALONE:
-				{
-					return !Status.STANDALONE.equals(CoreHub.getLocalLockService().getStatus());
-				}
-				default:
-				{
-					// empty
-				}
-			}
+			return Status.STANDALONE.equals(CoreHub.getLocalLockService().getStatus());
 		}
 		return false;
 	}
-	
 }
