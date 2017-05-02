a38e43eadd220ea268c041dff38f191b831da49f
Niklaus Giger <niklaus.giger@member.fsf.org>
2017-05-02 23:20:23 +0200

    [5684]Somehow we have now 54 instead of 53

--- ch.elexis.core.data.tests/src/ch/elexis/admin/RoleBasedAccessControlTest.java
+++ ch.elexis.core.data.tests/src/ch/elexis/admin/RoleBasedAccessControlTest.java
@@ -54,7 +54,7 @@ public class RoleBasedAccessControlTest extends AbstractPersistentObjectTest {
 		assertEquals(6, roles.size());
 		Role ur = Role.load(RoleConstants.SYSTEMROLE_LITERAL_USER);
 		ACE[] assignedUserRights = ur.getAssignedAccessRights();
-		assertEquals(53, assignedUserRights.length);
+		assertEquals(54, assignedUserRights.length);
 	}
 	
 	@Test
