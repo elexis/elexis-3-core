9f8dc7402e417b85470ff0b7b546682b07417174
sazgin <azgin@medevit.at>
2017-06-02 15:54:21 +0200

    [5984] Neue View Rechnungskorrekturen sowie Kontextmenü (Punkt 14 und 15)

--- ch.elexis.core.ui/src/ch/elexis/core/ui/views/rechnung/InvoiceCorrectionView.java
+++ ch.elexis.core.ui/src/ch/elexis/core/ui/views/rechnung/InvoiceCorrectionView.java
@@ -1,7 +1,6 @@
 package ch.elexis.core.ui.views.rechnung;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Calendar;
 
 import org.eclipse.jface.layout.TableColumnLayout;
 import org.eclipse.jface.viewers.ArrayContentProvider;
@@ -9,64 +8,118 @@ import org.eclipse.jface.viewers.ColumnLabelProvider;
 import org.eclipse.jface.viewers.ColumnViewerToolTipSupport;
 import org.eclipse.jface.viewers.ColumnWeightData;
 import org.eclipse.jface.viewers.ComboViewer;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.LabelProvider;
-import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.jface.window.ToolTip;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.DateTime;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.part.ViewPart;
 import org.eclipse.wb.swt.SWTResourceManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import ch.elexis.core.data.events.ElexisEvent;
+import ch.elexis.core.data.events.ElexisEventDispatcher;
+import ch.elexis.core.data.events.ElexisEventListenerImpl;
 import ch.elexis.core.ui.UiDesk;
+import ch.elexis.core.ui.events.ElexisUiEventListenerImpl;
 import ch.elexis.core.ui.util.SWTHelper;
+import ch.elexis.data.Anwender;
+import ch.elexis.data.Rechnung;
+import ch.elexis.data.dto.InvoiceCorrectionDTO;
+import ch.elexis.data.dto.InvoiceCorrectionDTO.DiagnosesDTO;
+import ch.elexis.data.dto.InvoiceCorrectionDTO.FallDTO;
+import ch.elexis.data.dto.InvoiceCorrectionDTO.KonsultationDTO;
+import ch.elexis.data.dto.InvoiceCorrectionDTO.LeistungDTO;
+import ch.rgw.tools.TimeTool;
 
 public class InvoiceCorrectionView extends ViewPart {
 	public static final String ID = "ch.elexis.core.ui.views.rechnung.InvoiceCorrectionView";
 	private static Logger logger = LoggerFactory.getLogger(InvoiceCorrectionView.class);
 	
+	private InvoiceComposite invoiceComposite;
+	
+	private Rechnung actualInvoice;
+	
+	private final ElexisEventListenerImpl eeli_rn = new ElexisUiEventListenerImpl(Rechnung.class,
+		ElexisEvent.EVENT_CREATE | ElexisEvent.EVENT_DELETE | ElexisEvent.EVENT_UPDATE
+			| ElexisEvent.EVENT_SELECTED | ElexisEvent.EVENT_DESELECTED) {
+		
+		public void runInUi(ElexisEvent ev){
+			switch (ev.getType()) {
+			case ElexisEvent.EVENT_UPDATE:
+				reload((Rechnung) ev.getObject());
+				break;
+			case ElexisEvent.EVENT_DESELECTED: // fall thru
+				reload(null);
+				break;
+			case ElexisEvent.EVENT_DELETE:
+				reload(null);
+				break;
+			case ElexisEvent.EVENT_SELECTED:
+				reload((Rechnung) ev.getObject());
+				break;
+			}
+		}
+	};
+	
+	private final ElexisEventListenerImpl eeli_user =
+		new ElexisUiEventListenerImpl(Anwender.class, ElexisEvent.EVENT_USER_CHANGED) {
+			
+			public void runInUi(ElexisEvent ev){
+				reload(actualInvoice);
+			}
+		};
+	
+	public void reload(Rechnung rechnung){
+		if (invoiceComposite != null) {
+			actualInvoice = rechnung;
+			InvoiceCorrectionDTO invoiceCorrectionDTO = new InvoiceCorrectionDTO(actualInvoice);
+			Composite parent = invoiceComposite.getParent();
+			invoiceComposite.dispose();
+			invoiceComposite = new InvoiceComposite(parent);
+			invoiceComposite.createComponents(invoiceCorrectionDTO);
+			parent.layout(true, true);
+		}
+	}
+	
 	@Override
 	public void createPartControl(Composite parent){
 		parent.setLayout(new GridLayout(1, false));
-		
-		String[] todoValues = new String[] {
-			"102", "SUVA", "06991212", "Uns wurde kein Unfall gemeldet", "Max Mustermann"
-		};
-		
-		List<String[]> invoiences = new ArrayList<>();
-		invoiences.add(new String[] {
-			"1.1.2017", "1", "0000.0002", "Konsultation erste 5 min", "43.23"
-		});
-		
-		List<String[]> diagnoses = new ArrayList<>();
-		diagnoses.add(new String[] {
-			"01 Kapitel I - Bestimmte infektöse und parasitäre Krankheiten - (ADO-B999)"
-		});
-		
-		InvoiceComposite invoiceComposite = new InvoiceComposite(parent);
-		invoiceComposite.createComponents(todoValues, diagnoses, invoiences);
+		invoiceComposite = new InvoiceComposite(parent);
+		invoiceComposite.createComponents(new InvoiceCorrectionDTO());
+		ElexisEventDispatcher.getInstance().addListeners(eeli_rn, eeli_user);
+		Rechnung selected = (Rechnung) ElexisEventDispatcher.getSelected(Rechnung.class);
+		if (selected != null) {
+			reload(selected);
+		}
+	}
+	
+
+	@Override
+	public void dispose(){
+		ElexisEventDispatcher.getInstance().removeListeners(eeli_rn, eeli_user);
+		super.dispose();
 	}
 	
 	@Override
 	public void setFocus(){
 		
 	}
-	
+
 	class InvoiceComposite extends ScrolledComposite {
 		Composite wrapper;
 		
@@ -76,17 +129,20 @@ public class InvoiceCorrectionView extends ViewPart {
 			setLayoutData(SWTHelper.getFillGridData(1, true, 1, true));
 		}
 		
-		public void createComponents(String[] todoValues, List<String[]> diagnoses,
-			List<String[]> invoinces){
+		public void createComponents(InvoiceCorrectionDTO invoiceCorrectionDTO){
 			wrapper = new Composite(this, SWT.NONE);
 			wrapper.setLayout(new GridLayout(1, false));
 			wrapper.setLayoutData(SWTHelper.getFillGridData(1, true, 1, true));
 			
 			InvoiceHeaderComposite invoiceHeaderComposite = new InvoiceHeaderComposite(wrapper);
 			InvoiceContentComposite invoiceContentComposite = new InvoiceContentComposite(wrapper);
+			InvoiceBottomComposite invoiceBottomComposite = new InvoiceBottomComposite(wrapper);
 			
-			invoiceHeaderComposite.createComponents(todoValues);
-			invoiceContentComposite.createComponents(diagnoses, invoinces);
+			invoiceHeaderComposite.createComponents(invoiceCorrectionDTO);
+			if (invoiceCorrectionDTO != null && invoiceCorrectionDTO.getId() != null) {
+				invoiceContentComposite.createComponents(invoiceCorrectionDTO);
+				invoiceBottomComposite.createComponents();
+			}
 			
 			this.setContent(wrapper);
 			this.setExpandHorizontal(true);
@@ -107,37 +163,39 @@ public class InvoiceCorrectionView extends ViewPart {
 	class InvoiceHeaderComposite extends Composite {
 		
 		String[] lbls = new String[] {
-			"Rechnung", "Empfänger", "Telefon Versicherer", "Rückweisungsgrund", "Sachbearbeiter/in"
+			"Rechnung", "Empfänger", "Telefon Versicherer", "Sachbearbeiter/in", "Rückweisungsgrund"
 		};
 		
 		public InvoiceHeaderComposite(Composite parent){
 			super(parent, SWT.BORDER);
-			setLayout(new GridLayout(6, false));
-			setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 6, 1));
+			setLayout(new GridLayout(4, false));
+			setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 4, 1));
 		}
 		
-		public void createComponents(String[] values){
+		public void createComponents(InvoiceCorrectionDTO invoiceCorrectionDTO){
 			
 			Label lblTitle = new Label(this, SWT.NONE);
 			lblTitle.setText("Rechnungsangaben");
 			lblTitle.setFont(SWTResourceManager.getFont("Noto Sans", 9, SWT.BOLD));
 			lblTitle.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 10, 1));
-			
-			this.setBackground(UiDesk.getColor(UiDesk.COL_WHITE));
-			if (values.length == lbls.length) {
-				FormToolkit tk = UiDesk.getToolkit();
+			Color colWhite = UiDesk.getColor(UiDesk.COL_WHITE);
+			this.setBackground(colWhite);
+			GridData gd = new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1);
+			String[] invoiceDetails = invoiceCorrectionDTO.getInvoiceDetails();
+			if (invoiceDetails.length == lbls.length) {
 				int i = 0;
 				for (String lbl : lbls) {
-					tk.createLabel(this, lbl);
-					tk.createLabel(this, values[i++]);
-					if (i == 4) {
-						tk.createLabel(this, "");
-						tk.createLabel(this, "");
-					}
+					String detailText = invoiceDetails[i++];
+					new Label(this, SWT.NONE).setText(lbl);
+					CLabel text = new CLabel(this, SWT.BORDER);
+					text.setBackground(colWhite);
+					text.setLayoutData(
+						i != 5 ? gd : new GridData(SWT.FILL, SWT.FILL, true, false, 3, 1));
+					text.setText(detailText != null ? detailText : "");
 				}
 			} else {
 				logger.error("cannot load invoice header data - values size mismatch [expected: "
-					+ lbls.length + ", current: " + values.length + "]");
+					+ lbls.length + ", current: " + invoiceDetails.length + "]");
 			}
 		}
 	}
@@ -152,30 +210,32 @@ public class InvoiceCorrectionView extends ViewPart {
 			setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		}
 		
-		public void createComponents(List<String[]> diagnoses, List<String[]> invoinces){
+		public void createComponents(InvoiceCorrectionDTO invoiceCorrectionDTO){
+			if (invoiceCorrectionDTO.getFallDTO() != null) {
+				InvoiceContentHeaderComposite invoiceContentHeaderComposite =
+					new InvoiceContentHeaderComposite(this);
+				invoiceContentHeaderComposite.createComponents(invoiceCorrectionDTO.getFallDTO());
+			}
 			
-			InvoiceContentHeaderComposite invoiceContentHeaderComposite =
-				new InvoiceContentHeaderComposite(this);
 			InvoiceContentMiddleComposite invoiceContentMiddleComposite =
 				new InvoiceContentMiddleComposite(this);
-			invoiceContentHeaderComposite.createComponents();
-			invoiceContentMiddleComposite.createComponents(diagnoses, invoinces);
+			invoiceContentMiddleComposite.createComponents(invoiceCorrectionDTO);
 		}
 	}
 	
 	class InvoiceContentHeaderComposite extends Composite {
 		public InvoiceContentHeaderComposite(Composite parent){
 			super(parent, SWT.BORDER);
-			setLayout(new GridLayout(10, false));
-			setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 10, 1));
+			setLayout(new GridLayout(6, false));
+			setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 6, 1));
 		}
 		
-		public void createComponents(){
+		public void createComponents(FallDTO fallDTO){
 			this.setBackground(UiDesk.getColor(UiDesk.COL_WHITE));
 			Label lblTitle = new Label(this, SWT.NONE);
 			lblTitle.setText("Fallangaben");
 			lblTitle.setFont(SWTResourceManager.getFont("Noto Sans", 9, SWT.BOLD));
-			lblTitle.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 10, 1));
+			lblTitle.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 6, 1));
 			
 			Label lblLaw = new Label(this, SWT.NONE);
 			lblLaw.setText("Gesetz");
@@ -191,18 +251,21 @@ public class InvoiceCorrectionView extends ViewPart {
 			cbLaw.setInput(new String[] {
 				"UVG", "KVG"
 			});
-			cbLaw.addSelectionChangedListener(new ISelectionChangedListener() {
-				@Override
-				public void selectionChanged(SelectionChangedEvent event){
-					ISelection selection = event.getSelection();
-					if (selection instanceof StructuredSelection) {
-						if (!selection.isEmpty()) {
-						
-						}
-					}
-				}
-			});
-						
+			cbLaw.setSelection(new StructuredSelection(fallDTO.getAbrechnungsSystem()));
+			
+			Label lblAccidentDate = new Label(this, SWT.NONE);
+			lblAccidentDate.setText("Unfall Datum");
+			DateTime dateAccident = new DateTime(this, SWT.DATE | SWT.DROP_DOWN);
+			TimeTool doa = new TimeTool(fallDTO.getBeginnDate());
+			dateAccident.setDate(doa.get(Calendar.YEAR), doa.get(Calendar.MONTH),
+				doa.get(Calendar.DAY_OF_MONTH));
+			Label lblAccidentNr = new Label(this, SWT.NONE);
+			lblAccidentNr.setText("Unfall Nummer");
+			CLabel txtAccidentNr = new CLabel(this, SWT.BORDER);
+			txtAccidentNr.setBackground(UiDesk.getColor(UiDesk.COL_WHITE));
+			txtAccidentNr.setText(fallDTO.getNumber());
+			txtAccidentNr.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1));
+			
 			Label lblRecipient = new Label(this, SWT.NONE);
 			lblRecipient.setText("Empfänger");
 			
@@ -217,52 +280,15 @@ public class InvoiceCorrectionView extends ViewPart {
 			cbRecipient.setInput(new String[] {
 				"SUVA", "PATIENT"
 			});
-			cbRecipient.addSelectionChangedListener(new ISelectionChangedListener() {
-				@Override
-				public void selectionChanged(SelectionChangedEvent event){
-					ISelection selection = event.getSelection();
-					if (selection instanceof StructuredSelection) {
-						if (!selection.isEmpty()) {
-						
-						}
-					}
-				}
-			});
+			cbRecipient.setSelection(new StructuredSelection(fallDTO.getReceiver()));
 						
 			Label lblGarant = new Label(this, SWT.NONE);
 			lblGarant.setText("Kostenträger");
 			
-			ComboViewer cbGarant = new ComboViewer(this, SWT.BORDER);
-			cbGarant.setContentProvider(ArrayContentProvider.getInstance());
-			cbGarant.setLabelProvider(new LabelProvider() {
-				@Override
-				public String getText(Object element){
-					return String.valueOf(element);
-				}
-			});
-			cbGarant.setInput(new String[] {
-				"SUVA", "PATIENT"
-			});
-			cbGarant.addSelectionChangedListener(new ISelectionChangedListener() {
-				@Override
-				public void selectionChanged(SelectionChangedEvent event){
-					ISelection selection = event.getSelection();
-					if (selection instanceof StructuredSelection) {
-						if (!selection.isEmpty()) {
-						
-						}
-					}
-				}
-			});
-						
-			Label lblAccidentDate = new Label(this, SWT.NONE);
-			lblAccidentDate.setText("Unfall Datum");
-			DateTime dateAccident = new DateTime(this, SWT.DATE | SWT.DROP_DOWN);
-			
-			Label lblAccidentNr = new Label(this, SWT.NONE);
-			lblAccidentNr.setText("Unfall Nummer");
-			Label txtAccidentNr = new Label(this, SWT.NONE);
-			txtAccidentNr.setText("343434");
+			CLabel text = new CLabel(this, SWT.BORDER);
+			text.setBackground(UiDesk.getColor(UiDesk.COL_WHITE));
+			text.setText(fallDTO.getCostReceiver());
+			text.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 3, 1));
 		}
 	}
 	
@@ -277,10 +303,11 @@ public class InvoiceCorrectionView extends ViewPart {
 			setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		}
 		
-		public void createComponents(List<String[]> diagnoses, List<String[]> invoinces){
-			for (int i = 0; i < 3; i++) {
+		public void createComponents(InvoiceCorrectionDTO invoiceCorrectionDTO){
+			for (KonsultationDTO konsultationDTO : invoiceCorrectionDTO.getKonsultationDTOs()) {
 				Group group = new Group(this, SWT.BORDER);
-				group.setText("Konsultation vom " + (i + 1) + ".1.2017");
+				group.setText("Konsultation: " + konsultationDTO.getDate() + " - "
+					+ konsultationDTO.getStateText());
 				group.setFont(SWTResourceManager.getFont("Noto Sans", 9, SWT.BOLD));
 				GridLayout gd = new GridLayout(1, false);
 				gd.marginWidth = 0;
@@ -291,8 +318,8 @@ public class InvoiceCorrectionView extends ViewPart {
 					new InvoiceContentDiagnosisComposite(group);
 				InvoiceContentKonsultationComposite invoiceContentKonsultationComposite =
 					new InvoiceContentKonsultationComposite(group);
-				invoiceContentDiagnosisComposite.createComponents(diagnoses);
-				invoiceContentKonsultationComposite.createComponents(invoinces);
+				invoiceContentDiagnosisComposite.createComponents(konsultationDTO);
+				invoiceContentKonsultationComposite.createComponents(konsultationDTO);
 			}
 			
 		}
@@ -311,7 +338,7 @@ public class InvoiceCorrectionView extends ViewPart {
 			setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		}
 		
-		private void createComponents(List<String[]> values){
+		private void createComponents(KonsultationDTO konsultationDTO){
 			Composite tableArea = new Composite(this, SWT.NONE);
 			tableArea.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
 			tableColumnLayout = new TableColumnLayout();
@@ -322,17 +349,16 @@ public class InvoiceCorrectionView extends ViewPart {
 			Table table = tableViewer.getTable();
 			table.setLayoutData(SWTHelper.getFillGridData(1, true, 1, true));
 			
-			TableViewerColumn tcKonsDate = createTableViewerColumn("Kons. Datum", 4, 0);
-			TableViewerColumn tcSize = createTableViewerColumn("Anzahl", 2, 1);
-			TableViewerColumn tcServiceCode = createTableViewerColumn("Leistungscode", 5, 2);
-			TableViewerColumn tcSericeText = createTableViewerColumn("Leistungstext", 10, 3);
-			TableViewerColumn tcPrice = createTableViewerColumn("Preis", 4, 4);
+			TableViewerColumn tcSize = createTableViewerColumn("Anzahl", 2, 0);
+			TableViewerColumn tcServiceCode = createTableViewerColumn("Leistungscode", 4, 1);
+			TableViewerColumn tcSericeText = createTableViewerColumn("Leistungstext", 12, 2);
+			TableViewerColumn tcPrice = createTableViewerColumn("Preis", 3, 3);
 			
 			table.setHeaderVisible(true);
 			table.setLinesVisible(true);
 			
 			tableViewer.setContentProvider(new ArrayContentProvider());
-			tableViewer.setInput(values);
+			tableViewer.setInput(konsultationDTO.getLeistungDTOs());
 		}
 		
 		private TableViewerColumn createTableViewerColumn(String title, int bound, int colIdx){
@@ -356,7 +382,20 @@ public class InvoiceCorrectionView extends ViewPart {
 			
 			@Override
 			public String getText(Object element){
-				return ((String[]) element)[colIdx];
+				LeistungDTO leistungDTO = (LeistungDTO) element;
+				switch (colIdx) {
+				case 0:
+					return String.valueOf(leistungDTO.getCount());
+				case 1:
+					return leistungDTO.getCode();
+				case 2:
+					return leistungDTO.getText();
+				case 3:
+					return leistungDTO.getBruttoPreis().getAmountAsString();
+				default:
+					return "";
+				}
+				
 			}
 		}
 	}
@@ -375,7 +414,7 @@ public class InvoiceCorrectionView extends ViewPart {
 			setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1));
 		}
 		
-		private void createComponents(List<String[]> values){
+		private void createComponents(KonsultationDTO konsultationDTO){
 			Composite tableArea = new Composite(this, SWT.NONE);
 			tableArea.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 2, 1));
 			tableColumnLayout = new TableColumnLayout();
@@ -394,7 +433,7 @@ public class InvoiceCorrectionView extends ViewPart {
 			table.setLinesVisible(true);
 			
 			tableViewer.setContentProvider(new ArrayContentProvider());
-			tableViewer.setInput(values);
+			tableViewer.setInput(konsultationDTO.getDiagnosesDTOs());
 		}
 		
 		private TableViewerColumn createTableViewerColumn(String title, int bound, int colIdx){
@@ -418,8 +457,42 @@ public class InvoiceCorrectionView extends ViewPart {
 			
 			@Override
 			public String getText(Object element){
-				return ((String[]) element)[colIdx];
+				DiagnosesDTO diagnosesDTO = (DiagnosesDTO) element;
+				switch (colIdx) {
+				case 0:
+					return diagnosesDTO.getLabel();
+				default:
+					return "";
+				}
 			}
+		}
+	}
+	
+	class InvoiceBottomComposite extends Composite {
+		public InvoiceBottomComposite(Composite parent){
+			super(parent, SWT.NONE);
+			GridLayout gd = new GridLayout(1, false);
+			gd.marginWidth = 0;
+			gd.marginHeight = 0;
+			setLayout(gd);
+			setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		}
+		
+		public void createComponents(){
+			Composite parent = new Composite(this, SWT.NONE);
+			GridLayout gd = new GridLayout(3, false);
+			gd.marginWidth = 0;
+			gd.marginHeight = 2;
+			parent.setLayout(gd);
+			parent.setLayoutData(new GridData(SWT.RIGHT, SWT.TOP, true, true, 1, 1));
+			Button btnImmediateSent = new Button(parent, SWT.CHECK);
+			btnImmediateSent.setText("sofort versenden");
+			
+			Button btnCorrection = new Button(parent, SWT.NONE);
+			btnCorrection.setText("Korrigieren");
+			
+			Button btnCancel = new Button(parent, SWT.NONE);
+			btnCancel.setText("Zurücksetzen");
 		}
 	}
 	
