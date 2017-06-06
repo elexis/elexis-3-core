deef74d2d22c4ed9ce01c0e7bb550089b5384d5a
sazgin <azgin@medevit.at>
2017-06-06 15:19:02 +0200

    [6167] NPE at KonsDetailView.updateFallCombo

--- ch.elexis.core.ui/src/ch/elexis/core/ui/views/KonsDetailView.java
+++ ch.elexis.core.ui/src/ch/elexis/core/ui/views/KonsDetailView.java
@@ -448,14 +448,12 @@ public class KonsDetailView extends ViewPart
 	
 	private void updateFallCombo(){
 		Patient pat = ElexisEventDispatcher.getSelectedPatient();
-		if (pat != null) {
+		if (pat != null && comboViewerFall != null) {
 			Fall[] faelle = pat.getFaelle();
-			if (comboViewerFall != null) {
-				comboViewerFall.setInput(faelle);
-				if (actKons != null) {
-					comboFallSelectionListener.ignoreSelectionEventOnce();
-					comboViewerFall.setSelection(new StructuredSelection(actKons.getFall()));
-				}
+			comboViewerFall.setInput(faelle);
+			if (actKons != null) {
+				comboFallSelectionListener.ignoreSelectionEventOnce();
+				comboViewerFall.setSelection(new StructuredSelection(actKons.getFall()));
 			}
 		}
 	}
