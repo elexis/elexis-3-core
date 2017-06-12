eedc3ec348b6b6a001bc153e7e45d5c6be0506e2
sazgin <azgin@medevit.at>
2017-06-12 16:53:50 +0200

    [4382] View: Fälle Sortierung ändern

--- ch.elexis.core.ui/src/ch/elexis/core/ui/views/provider/FaelleContentProvider.java
+++ ch.elexis.core.ui/src/ch/elexis/core/ui/views/provider/FaelleContentProvider.java
@@ -1,8 +1,11 @@
 package ch.elexis.core.ui.views.provider;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
+import org.apache.commons.lang3.ObjectUtils;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.Viewer;
 
@@ -10,6 +13,7 @@ import ch.elexis.core.data.events.ElexisEventDispatcher;
 import ch.elexis.core.ui.views.FaelleView;
 import ch.elexis.data.Fall;
 import ch.elexis.data.Patient;
+import ch.rgw.tools.TimeTool;
 
 /**
  * @since 3.0.0 extracted from {@link FaelleView}
@@ -22,15 +26,35 @@ public class FaelleContentProvider implements IStructuredContentProvider {
 			return new Object[0];
 		} else {
 			Fall[] cases = act.getFaelle();
-			List<Fall> caseList = new ArrayList<Fall>();
-			
-			for (Fall fall : cases) {
-				if (fall.isOpen()) {
-					caseList.add(0, fall);
-				} else {
-					caseList.add(fall);
+			List<Fall> caseList = Arrays.asList(cases);
+			Collections.sort(caseList, new Comparator<Fall>() {
+				@Override
+				public int compare(Fall f1, Fall f2){
+					String gesetz1 = f1.getAbrechnungsSystem();
+					String gesetz2 = f2.getAbrechnungsSystem();
+					
+					// compare gesetz
+					int comp = ObjectUtils.compare(gesetz1, gesetz2);
+					if (comp == 0) {
+						// compare open state
+						boolean o1 = f1.isOpen();
+						boolean o2 = f2.isOpen();
+						
+						comp = ObjectUtils.compare(o2, o1);
+						if (comp == 0) {
+							// compare beginn date
+							TimeTool t1 = new TimeTool(f1.getBeginnDatum());
+							TimeTool t2 = new TimeTool(f2.getBeginnDatum());
+							comp = t1.isEqual(t2) ? 0 : (t1.isBefore(t2) ? 1 : -1);
+							// compare id 
+							if (comp == 0) {
+								comp = ObjectUtils.compare(f2.getId(), f1.getId());
+							}
+						}
+					}
+					return comp;
 				}
-			}
+			});
 			return caseList.toArray();
 		}
 		
