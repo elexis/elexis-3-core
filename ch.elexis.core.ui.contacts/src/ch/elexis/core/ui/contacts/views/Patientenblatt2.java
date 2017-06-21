6ffc50744020cf63b4b27696606032d4c6da9f18
thomashu <huster@medevit.at>
2017-06-21 12:57:36 +0200

    [6482] fixed patient save in standalone mode

--- ch.elexis.core.ui.contacts/src/ch/elexis/core/ui/contacts/views/Patientenblatt2.java
+++ ch.elexis.core.ui.contacts/src/ch/elexis/core/ui/contacts/views/Patientenblatt2.java
@@ -69,6 +69,7 @@ import ch.elexis.core.data.events.ElexisEvent;
 import ch.elexis.core.data.events.ElexisEventDispatcher;
 import ch.elexis.core.data.events.ElexisEventListener;
 import ch.elexis.core.data.util.Extensions;
+import ch.elexis.core.lock.ILocalLockService;
 import ch.elexis.core.model.IPersistentObject;
 import ch.elexis.core.model.MaritalStatus;
 import ch.elexis.core.model.PatientConstants;
@@ -744,6 +745,10 @@ public class Patientenblatt2 extends Composite implements IUnlockable {
 	
 	@SuppressWarnings("unchecked")
 	public void setPatient(final Patient p){
+		// save does not happen via locking in standalone mode
+		if (CoreHub.getLocalLockService().getStatus() == ILocalLockService.Status.STANDALONE) {
+			save();
+		}
 		actPatient = p;
 		ipp.getAutoForm().reload(actPatient);
 		
