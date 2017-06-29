a7efa6aa09a34d3ab4b2fd2a935ed233118d5829
sazgin <azgin@medevit.at>
2017-06-29 18:53:30 +0200

    [6111] Pop-Ups für Pendenzen funktionieren nicht

--- ch.elexis.core.data/src/ch/elexis/data/Reminder.java
+++ ch.elexis.core.data/src/ch/elexis/data/Reminder.java
@@ -338,7 +338,7 @@ public class Reminder extends PersistentObject implements Comparable<Reminder> {
 	}
 	
 	private static final String PS_REMINDERS_DUE_FOR = "SELECT r.ID FROM " + TABLENAME
-			+ " r LEFT JOIN REMINDERS_RESPONSIBLE_LINK rrl ON (r.id = rrl.ReminderId) WHERE rrl.ResponsibleID = ? AND r.deleted = '0' AND r.IdentID = ? AND r.Status != ? AND r.DateDue < ?;";
+			+ " r LEFT JOIN REMINDERS_RESPONSIBLE_LINK rrl ON (r.id = rrl.ReminderId) WHERE rrl.ResponsibleID = ? AND r.deleted = '0' AND r.IdentID = ? AND r.Status != ? AND r.DateDue <= ?;";
 
 	/**
 	 * Alle Reminder holen, die bei einem bestimmten Patienten für einen
