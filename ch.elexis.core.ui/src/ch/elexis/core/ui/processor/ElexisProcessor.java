297f947423b65e9b85378e54781f2dc0e260ce97
sazgin <azgin@medevit.at>
2017-07-25 18:13:31 +0200

    [6693] fastView auf Re-Klick nicht vorhanden (#125) (#126)

---
+++ ch.elexis.core.ui/src/ch/elexis/core/ui/processor/ElexisProcessor.java
@@ -0,0 +1,56 @@
+package ch.elexis.core.ui.processor;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.MApplication;
+import org.eclipse.e4.ui.model.application.ui.SideValue;
+import org.eclipse.e4.ui.model.application.ui.basic.MPartStack;
+import org.eclipse.e4.ui.model.application.ui.basic.MTrimBar;
+import org.eclipse.e4.ui.model.application.ui.basic.MTrimmedWindow;
+import org.eclipse.e4.ui.model.application.ui.menu.MToolControl;
+import org.eclipse.e4.ui.workbench.modeling.EModelService;
+
+import ch.elexis.core.ui.renderer.ElexisRenderer;
+
+public class ElexisProcessor {
+	
+	public ElexisProcessor(){
+	}
+	
+	@Execute
+	public void execute(MApplication mApplication, EModelService eModelService){
+		
+		MPartStack stack =
+			(MPartStack) eModelService.find(ElexisRenderer.ELEXIS_FASTVIEW_STACK, mApplication);
+		
+		// no stack found create one
+		if (stack == null) {
+			MTrimmedWindow window = (MTrimmedWindow) eModelService.find("IDEWindow", mApplication);
+			if (window != null) {
+				MPartStack mPartStack = eModelService.createModelElement(MPartStack.class);
+				mPartStack.setElementId(ElexisRenderer.ELEXIS_FASTVIEW_STACK);
+				mPartStack.setToBeRendered(true);
+				mPartStack.getTags().add("Minimized");
+				mPartStack.setOnTop(false);
+				mPartStack.setVisible(false);
+				mPartStack.getTags().add("NoAutoCollapse");
+				mPartStack.getTags().add("active");
+				window.getChildren().add(mPartStack);
+			
+				MToolControl mToolControl = eModelService.createModelElement(MToolControl.class);
+				mToolControl.setElementId("my.minimized.parts(minimized)");
+				mToolControl.setContributionURI(
+					"bundleclass://org.eclipse.e4.ui.workbench.addons.swt/org.eclipse.e4.ui.workbench.addons.minmax.TrimStack");
+				mToolControl.setToBeRendered(true);
+				mToolControl.setVisible(true);
+				mToolControl.getTags().add("TrimStack");
+				
+				MTrimBar mTrimBar = eModelService.getTrim(window, SideValue.BOTTOM);
+				mTrimBar.getChildren().add(mToolControl);
+				mTrimBar.setVisible(true);
+				mTrimBar.setToBeRendered(true);
+			}
+		}
+
+		
+	}
+}
