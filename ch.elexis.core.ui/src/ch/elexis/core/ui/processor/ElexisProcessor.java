ad2ae1d454629af64dd10129cd3b1cbe4c9582f9
sazgin <azgin@medevit.at>
2017-08-31 16:17:14 +0200

    [7822] [7823] E3.3 Elexis Coolbar, FastView

--- ch.elexis.core.ui/src/ch/elexis/core/ui/processor/ElexisProcessor.java
+++ ch.elexis.core.ui/src/ch/elexis/core/ui/processor/ElexisProcessor.java
@@ -11,6 +11,14 @@ public class ElexisProcessor {
 	
 	@Execute
 	public void execute(MApplication mApplication, EModelService eModelService){
+		
+		/**
+		 * Clears the persisted state for the main toolbar, otherwise the positioning of the
+		 * toolbar-elements are in an incorrect order. That means all persisted toolbar-elements are
+		 * positioned before the dynamically created elements from
+		 * ApplicationActionBarAdvisor#fillCoolBar.
+		 * 
+		 **/
 		MTrimBar mTrimBar =
 			(MTrimBar) eModelService.find("org.eclipse.ui.main.toolbar", mApplication);
 		if (mTrimBar != null && mTrimBar.getChildren() != null) {
