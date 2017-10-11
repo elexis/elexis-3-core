93bc1b17a93fea18a977004d6a1d98daf31053f6
Niklaus Giger <niklaus.giger@member.fsf.org>
2017-10-11 19:56:53 +0200

    [8768] Load correctly p2 configuration

--- ch.elexis.core.ui.p2/src/ch/elexis/core/ui/p2/internal/PreferenceInitializer.java
+++ ch.elexis.core.ui.p2/src/ch/elexis/core/ui/p2/internal/PreferenceInitializer.java
@@ -30,10 +30,11 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 	 * org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences
 	 * ()
 	 */
+	@Override
 	public void initializeDefaultPreferences(){
 		Preferences node = DefaultScope.INSTANCE.getNode(Activator.PLUGIN_ID); //$NON-NLS-1$
 		// default values
-		node.putBoolean(PreferenceConstants.REPOSITORIES_VISIBLE, false);
+		node.putBoolean(PreferenceConstants.REPOSITORIES_VISIBLE, true);
 		node.putBoolean(PreferenceConstants.SHOW_LATEST_VERSION_ONLY, true);
 		node.putBoolean(PreferenceConstants.AVAILABLE_SHOW_ALL_BUNDLES, false);
 		node.putBoolean(PreferenceConstants.INSTALLED_SHOW_ALL_BUNDLES, false);
@@ -42,7 +43,7 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 		node.putInt(PreferenceConstants.RESTART_POLICY,
 			Policy.RESTART_POLICY_PROMPT_RESTART_OR_APPLY);
 		node.putInt(PreferenceConstants.UPDATE_WIZARD_STYLE, Policy.UPDATE_STYLE_MULTIPLE_IUS);
-		node.putBoolean(PreferenceConstants.FILTER_ON_ENV, false);
+		node.putBoolean(PreferenceConstants.FILTER_ON_ENV, true);
 		node.putInt(PreferenceConstants.UPDATE_DETAILS_HEIGHT, SWT.DEFAULT);
 		node.putInt(PreferenceConstants.UPDATE_DETAILS_WIDTH, SWT.DEFAULT);
 	}
