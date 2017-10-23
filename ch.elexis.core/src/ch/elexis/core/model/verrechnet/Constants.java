6d943eed77f93edeb3d0324de10e2fb69cacb38b
thomashu <huster@medevit.at>
2017-10-23 11:47:35 +0200

    [8796] added separate tarif code for medi with indication

--- ch.elexis.core/src/ch/elexis/core/model/verrechnet/Constants.java
+++ ch.elexis.core/src/ch/elexis/core/model/verrechnet/Constants.java
@@ -2,6 +2,8 @@ package ch.elexis.core.model.verrechnet;
 
 public class Constants {
 	public static final String VAT_SCALE = "vat_scale";
+	/** mark something (Verrechnet) as medical indicated, value true / false */
+	public static final String FLD_EXT_INDICATED = "indicated";
 	/** the prescription ID of this if it is an article */
 	public static final String FLD_EXT_PRESC_ID = "prescriptionId";
 }
