9137b5cad417ea78014472955d5f17cbd9da50e4
Txomin Astrain <tastrain@hispeed.ch>
2017-11-25 14:25:05 +0100

    [9096] Refactored 3 constants + changed link to Handbuch

---
+++ UiPreferenceConstants.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2013 MEDEVIT <office@medevit.at>.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     MEDEVIT <office@medevit.at> - initial API and implementation
+ ******************************************************************************/
+package ch.elexis.core.ui.constants;
+
+/**
+ * This class provides preference constants to be referenced by the UI parts requiring configuration
+ * values.
+ * 
+ * @since 3.0.0
+ */
+public class UiPreferenceConstants {
+	
+	public static final String USERSETTINGS2_EXPANDABLE_COMPOSITES_BASE =
+		"view/expandableComposites";
+	public static final String USERSETTINGS2_EXPANDABLE_COMPOSITES =
+		USERSETTINGS2_EXPANDABLE_COMPOSITES_BASE + "/setting";
+	public static final String USERSETTINGS2_EXPANDABLE_COMPOSITES_STATES =
+		USERSETTINGS2_EXPANDABLE_COMPOSITES_BASE + "/states/";
+	public static final String USERSETTINGS2_EXPANDABLECOMPOSITE_STATE_OPEN = "1";
+	public static final String USERSETTINGS2_EXPANDABLECOMPOSITE_STATE_CLOSED = "2";
+	public static final String USERSETTINGS2_EXPANDABLECOMPOSITE_STATE_REMEMBER_STATE = "3";
+	public static final String DEFAULT_BASE_URL = "http://wiki.elexis.info/";
+	public static final String DEFAULT_START_PAGE = "Hauptseite";
+	public static final String DEFAULT_HANDBOOK = "Deutsches_Handbuch_3.1.pdf";
+	
+}
