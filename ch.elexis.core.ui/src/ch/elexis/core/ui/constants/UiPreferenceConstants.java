b7a11e8880bb48ac744e28eb37d00f79f8a73eae
Txomin Astrain <tastrain@hispeed.ch>
2017-12-01 14:52:09 +0100

    [9096] Added user setting to change Handbook url in Help > Manual

--- ch.elexis.core.ui/src/ch/elexis/core/ui/constants/UiPreferenceConstants.java
+++ ch.elexis.core.ui/src/ch/elexis/core/ui/constants/UiPreferenceConstants.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*******************************************************************************
  * Copyright (c) 2013 MEDEVIT <office@medevit.at>.
  * All rights reserved. This program and the accompanying materials
@@ -32,3 +33,40 @@ public class UiPreferenceConstants {
 	public static final String CFG_BASE = "org.iatrix.help.wiki";
 	public static final String CFG_HANDBOOK = CFG_BASE + "/handbook";
 }
+=======
+/*******************************************************************************
+ * Copyright (c) 2013 MEDEVIT <office@medevit.at>.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     MEDEVIT <office@medevit.at> - initial API and implementation
+ ******************************************************************************/
+package ch.elexis.core.ui.constants;
+
+/**
+ * This class provides preference constants to be referenced by the UI parts requiring configuration
+ * values.
+ * 
+ * @since 3.0.0
+ */
+public class UiPreferenceConstants {
+	
+	public static final String USERSETTINGS2_EXPANDABLE_COMPOSITES_BASE =
+		"view/expandableComposites";
+	public static final String USERSETTINGS2_EXPANDABLE_COMPOSITES =
+		USERSETTINGS2_EXPANDABLE_COMPOSITES_BASE + "/setting";
+	public static final String USERSETTINGS2_EXPANDABLE_COMPOSITES_STATES =
+		USERSETTINGS2_EXPANDABLE_COMPOSITES_BASE + "/states/";
+	public static final String USERSETTINGS2_EXPANDABLECOMPOSITE_STATE_OPEN = "1";
+	public static final String USERSETTINGS2_EXPANDABLECOMPOSITE_STATE_CLOSED = "2";
+	public static final String USERSETTINGS2_EXPANDABLECOMPOSITE_STATE_REMEMBER_STATE = "3";
+	public static final String DEFAULT_BASE_URL = "http://wiki.elexis.info/";
+	public static final String DEFAULT_START_PAGE = "Hauptseite";
+	public static final String DEFAULT_HANDBOOK = "https://wiki.elexis.info/Deutsches_Handbuch_3.1";
+	public static final String CFG_BASE = "org.iatrix.help.wiki";
+	public static final String CFG_HANDBOOK = CFG_BASE + "/handbook";	
+}
+>>>>>>> 53090162... [9096] Added user setting to change Handbook url in Help > Manual
