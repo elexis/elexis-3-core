73399378041a26438236bf45e6f5557efc2a96b7
Marco Descher <marco@descher.at>
2018-07-18 11:17:33 +0200

    [14325] Requested change in FallKostentraegerResolver

--- bundles/ch.elexis.core.data/src/ch/elexis/core/data/text/resolver/FallKostentraegerResolver.java
+++ bundles/ch.elexis.core.data/src/ch/elexis/core/data/text/resolver/FallKostentraegerResolver.java
@@ -3,17 +3,23 @@ package ch.elexis.core.data.text.resolver;
 import java.util.Optional;
 
 import ch.elexis.core.data.interfaces.text.ITextResolver;
+import ch.elexis.core.data.util.FallDataAccessor;
 import ch.elexis.data.Fall;
 import ch.elexis.data.Kontakt;
 
 public class FallKostentraegerResolver implements ITextResolver {
 
+	/**
+	 * @see FallDataAccessor#getObject(String, ch.elexis.data.PersistentObject, String, String[])
+	 */
 	@Override
 	public Optional<String> resolve(Object object) {
 		if (object instanceof Fall) {
 			Kontakt costBearer = ((Fall) object).getCostBearer();
 			if (costBearer != null) {
-				return Optional.of(costBearer.getLabel());
+				String label = costBearer.getLabel();
+				String fullName = label.substring(0, label.indexOf(","));
+				return Optional.of(fullName);
 			}
 		}
 		return Optional.empty();
