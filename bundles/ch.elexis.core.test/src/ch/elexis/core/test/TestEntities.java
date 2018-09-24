600f60f602a9c898eec752ec5d9e7eb7ff9f307e
Marco Descher <marco@descher.at>
2018-09-24 10:31:46 +0200

    [14501] core.test - TestEntities, filter for OsgiServiceUtil

---
+++ bundles/ch.elexis.core.test/src/ch/elexis/core/test/TestEntities.java
@@ -0,0 +1,25 @@
+package ch.elexis.core.test;
+
+public class TestEntities {
+	
+	public static final String USER_ADMINISTRATOR_ID = "Administrator";
+	public static final String USER_ADMINISTRATOR_PASS = "administrator";
+	
+	public static final String USER_USER_ID = "user";
+	public static final String USER_USER_PASS = "user";
+	
+	public static final String MANDATOR_ID = "h2c1172107ce2df95065";
+
+	public static final String PATIENT_MALE_ID = "zd8d46d1b8d44330501105";
+	public static final int PATIENT_MALE_PATIENTNR = 1;
+	public static final String PATIENT_FEMALE_ID = "s9b71824bf6b877701111";
+	public static final int PATIENT_FEMALE_PATIENTNR = 2;
+
+	public static final String LABORATORY_ID = "G299ce51c1100e52801137";
+	public static final String LABORATORY_IDENTIFIER = "Testlabor";
+
+	public static final String ARTIKELSTAMM_ITEM_PHARMA_ID = "0768047504015713366300008";
+	public static final String ARTIKELSTAMM_ITEM_PHARMA_ID_2 = "0768047503086821812760004";
+	public static final String ARTIKELSTAMM_ITEM_PHARMA_ID_3 = "0403265109160755548550008";
+	public static final String ARTIKELSTAMM_PRODUCT_PHARMA_ID = "5631802";
+}
--- bundles/ch.elexis.core/src/ch/elexis/core/utils/OsgiServiceUtil.java
+++ bundles/ch.elexis.core/src/ch/elexis/core/utils/OsgiServiceUtil.java
@@ -1,10 +1,13 @@
 package ch.elexis.core.utils;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Optional;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,13 +26,33 @@ public class OsgiServiceUtil {
 	 * @return
 	 */
 	public synchronized static <T extends Object> Optional<T> getService(Class<T> clazz){
+		return getService(clazz, null);
+	}
+	
+	/**
+	 * Get a service from the OSGi service registry. <b>Always</b> release the service using the
+	 * {@link OsgiServiceUtil#ungetService(Object)} method after usage.
+	 * 
+	 * @param clazz
+	 * @param filter
+	 *            provide a filter to select a specific service instance if multiple available
+	 * @return
+	 */
+	public synchronized static <T extends Object> Optional<T> getService(Class<T> clazz,
+		String filter){
 		Bundle bundle = FrameworkUtil.getBundle(clazz);
 		// fallback to our context ...
 		if (bundle.getBundleContext() == null) {
 			bundle = FrameworkUtil.getBundle(OsgiServiceUtil.class);
 		}
-		ServiceReference<T> ref = bundle.getBundleContext().getServiceReference(clazz);
-		if (ref != null) {
+		Collection<ServiceReference<T>> references = Collections.emptyList();
+		try {
+			references = bundle.getBundleContext().getServiceReferences(clazz, filter);
+		} catch (InvalidSyntaxException e) {
+			logger.error("Invalid filter syntax", e);
+		}
+		if (!references.isEmpty() && references.size() == 1) {
+			ServiceReference<T> ref = references.iterator().next();
 			T service = bundle.getBundleContext().getService(ref);
 			if (service != null) {
 				serviceReferences.put(service, ref);
@@ -58,9 +81,8 @@ public class OsgiServiceUtil {
 			}
 			if (bundle.getBundleContext().ungetService(reference)) {
 				serviceReferences.remove(service);
-				logger.info(
-					"Release active service [" + service + "] from " + serviceReferences.size()
-					+ " active references");
+				logger.info("Release active service [" + service + "] from "
+					+ serviceReferences.size() + " active references");
 			} else {
 				serviceReferences.remove(service);
 				logger.info("Release not active service [" + service + "] from "
