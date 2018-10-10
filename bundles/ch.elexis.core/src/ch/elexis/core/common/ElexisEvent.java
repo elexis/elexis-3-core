07606c2a3ba7e00064e729b62093179a9f48c302
Marco Descher <marco@descher.at>
2018-10-10 15:54:28 +0200

    [14501] ElexisEvent.of

--- bundles/ch.elexis.core/src/ch/elexis/core/common/ElexisEvent.java
+++ bundles/ch.elexis.core/src/ch/elexis/core/common/ElexisEvent.java
@@ -1,5 +1,6 @@
 package ch.elexis.core.common;
 
+import java.beans.Transient;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,24 +11,46 @@ public class ElexisEvent {
 	
 	private String topic;
 	private Map<String, String> properties;
-
-	public ElexisEvent() {
+	
+	public ElexisEvent(){
 		properties = new HashMap<String, String>();
 	}
-
-	public String getTopic() {
+	
+	public String getTopic(){
 		return topic;
 	}
-
-	public void setTopic(String topic) {
+	
+	public void setTopic(String topic){
 		this.topic = topic;
 	}
-
+	
 	public Map<String, String> getProperties(){
 		return properties;
 	}
-
+	
 	public void setProperties(Map<String, String> properties){
 		this.properties = properties;
 	}
+	
+	/**
+	 * Create an ElexisEvent
+	 * 
+	 * @param topic
+	 *            the topic of this event
+	 * @param objectId
+	 *            the referenced object id
+	 * @param objectClass
+	 *            the referenced object class, in store-to-string format
+	 * @return
+	 * @see ElexisEventTopics
+	 */
+	@Transient
+	public static ElexisEvent of(String topic, String objectId, String objectClass){
+		ElexisEvent ee = new ElexisEvent();
+		ee.setTopic(topic);
+		ee.getProperties().put(ElexisEventTopics.PROPKEY_ID, objectId);
+		ee.getProperties().put(ElexisEventTopics.PROPKEY_CLASS, objectClass);
+		return ee;
+	}
+	
 }
