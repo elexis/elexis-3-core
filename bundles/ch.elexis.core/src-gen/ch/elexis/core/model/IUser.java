e3c3b47149d96a8a396dfc3a0c69ec8e9314d46d
Marco Descher <marco@descher.at>
2018-10-15 10:28:08 +0200

    docu update

--- bundles/ch.elexis.core/src-gen/ch/elexis/core/model/IUser.java
+++ bundles/ch.elexis.core/src-gen/ch/elexis/core/model/IUser.java
@@ -12,24 +12,22 @@ package ch.elexis.core.model;
 
 import java.util.List;
 
-
 /**
- * <!-- begin-user-doc -->
- * A representation of the model object '<em><b>IUser</b></em>'.
- * <!-- end-user-doc -->
+ * <!-- begin-user-doc --> A representation of the model object '<em><b>IUser</b></em>'. <!--
+ * end-user-doc -->
  *
  * <p>
  * The following features are supported:
  * </p>
  * <ul>
- *   <li>{@link ch.elexis.core.model.IUser#getUsername <em>Username</em>}</li>
- *   <li>{@link ch.elexis.core.model.IUser#getHashedPassword <em>Hashed Password</em>}</li>
- *   <li>{@link ch.elexis.core.model.IUser#getSalt <em>Salt</em>}</li>
- *   <li>{@link ch.elexis.core.model.IUser#getAssignedContact <em>Assigned Contact</em>}</li>
- *   <li>{@link ch.elexis.core.model.IUser#getRoles <em>Roles</em>}</li>
- *   <li>{@link ch.elexis.core.model.IUser#isActive <em>Active</em>}</li>
- *   <li>{@link ch.elexis.core.model.IUser#isAllowExternal <em>Allow External</em>}</li>
- *   <li>{@link ch.elexis.core.model.IUser#isAdministrator <em>Administrator</em>}</li>
+ * <li>{@link ch.elexis.core.model.IUser#getUsername <em>Username</em>}</li>
+ * <li>{@link ch.elexis.core.model.IUser#getHashedPassword <em>Hashed Password</em>}</li>
+ * <li>{@link ch.elexis.core.model.IUser#getSalt <em>Salt</em>}</li>
+ * <li>{@link ch.elexis.core.model.IUser#getAssignedContact <em>Assigned Contact</em>}</li>
+ * <li>{@link ch.elexis.core.model.IUser#getRoles <em>Roles</em>}</li>
+ * <li>{@link ch.elexis.core.model.IUser#isActive <em>Active</em>}</li>
+ * <li>{@link ch.elexis.core.model.IUser#isAllowExternal <em>Allow External</em>}</li>
+ * <li>{@link ch.elexis.core.model.IUser#isAdministrator <em>Administrator</em>}</li>
  * </ul>
  *
  * @see ch.elexis.core.model.ModelPackage#getIUser()
@@ -38,13 +36,13 @@ import java.util.List;
  */
 public interface IUser extends Deleteable, Identifiable {
 	/**
-	 * Returns the value of the '<em><b>Username</b></em>' attribute.
-	 * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Username</b></em>' attribute. <!-- begin-user-doc -->
 	 * <p>
-	 * If the meaning of the '<em>Username</em>' attribute isn't clear,
-	 * there really should be more of a description here...
+	 * If the meaning of the '<em>Username</em>' attribute isn't clear, there really should be more
+	 * of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
+	 * 
 	 * @return the value of the '<em>Username</em>' attribute.
 	 * @see #setUsername(String)
 	 * @see ch.elexis.core.model.ModelPackage#getIUser_Username()
@@ -52,25 +50,26 @@ public interface IUser extends Deleteable, Identifiable {
 	 * @generated
 	 */
 	String getUsername();
-
+	
 	/**
-	 * Sets the value of the '{@link ch.elexis.core.model.IUser#getUsername <em>Username</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @param value the new value of the '<em>Username</em>' attribute.
+	 * Sets the value of the '{@link ch.elexis.core.model.IUser#getUsername <em>Username</em>}'
+	 * attribute. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @param value
+	 *            the new value of the '<em>Username</em>' attribute.
 	 * @see #getUsername()
 	 * @generated
 	 */
 	void setUsername(String value);
-
+	
 	/**
-	 * Returns the value of the '<em><b>Hashed Password</b></em>' attribute.
-	 * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Hashed Password</b></em>' attribute. <!-- begin-user-doc -->
 	 * <p>
-	 * If the meaning of the '<em>Hashed Password</em>' attribute isn't clear,
-	 * there really should be more of a description here...
+	 * If the meaning of the '<em>Hashed Password</em>' attribute isn't clear, there really should
+	 * be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
+	 * 
 	 * @return the value of the '<em>Hashed Password</em>' attribute.
 	 * @see #setHashedPassword(String)
 	 * @see ch.elexis.core.model.ModelPackage#getIUser_HashedPassword()
@@ -78,25 +77,26 @@ public interface IUser extends Deleteable, Identifiable {
 	 * @generated
 	 */
 	String getHashedPassword();
-
+	
 	/**
-	 * Sets the value of the '{@link ch.elexis.core.model.IUser#getHashedPassword <em>Hashed Password</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @param value the new value of the '<em>Hashed Password</em>' attribute.
+	 * Sets the value of the '{@link ch.elexis.core.model.IUser#getHashedPassword <em>Hashed
+	 * Password</em>}' attribute. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @param value
+	 *            the new value of the '<em>Hashed Password</em>' attribute.
 	 * @see #getHashedPassword()
 	 * @generated
 	 */
 	void setHashedPassword(String value);
-
+	
 	/**
-	 * Returns the value of the '<em><b>Salt</b></em>' attribute.
-	 * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Salt</b></em>' attribute. <!-- begin-user-doc -->
 	 * <p>
-	 * If the meaning of the '<em>Salt</em>' attribute isn't clear,
-	 * there really should be more of a description here...
+	 * If the meaning of the '<em>Salt</em>' attribute isn't clear, there really should be more of a
+	 * description here...
 	 * </p>
 	 * <!-- end-user-doc -->
+	 * 
 	 * @return the value of the '<em>Salt</em>' attribute.
 	 * @see #setSalt(String)
 	 * @see ch.elexis.core.model.ModelPackage#getIUser_Salt()
@@ -104,25 +104,27 @@ public interface IUser extends Deleteable, Identifiable {
 	 * @generated
 	 */
 	String getSalt();
-
+	
 	/**
 	 * Sets the value of the '{@link ch.elexis.core.model.IUser#getSalt <em>Salt</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @param value the new value of the '<em>Salt</em>' attribute.
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @param value
+	 *            the new value of the '<em>Salt</em>' attribute.
 	 * @see #getSalt()
 	 * @generated
 	 */
 	void setSalt(String value);
-
+	
 	/**
-	 * Returns the value of the '<em><b>Assigned Contact</b></em>' reference.
-	 * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Assigned Contact</b></em>' reference. <!-- begin-user-doc
+	 * -->
 	 * <p>
-	 * If the meaning of the '<em>Assigned Contact</em>' reference isn't clear,
-	 * there really should be more of a description here...
+	 * If the meaning of the '<em>Assigned Contact</em>' reference isn't clear, there really should
+	 * be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
+	 * 
 	 * @return the value of the '<em>Assigned Contact</em>' reference.
 	 * @see #setAssignedContact(IContact)
 	 * @see ch.elexis.core.model.ModelPackage#getIUser_AssignedContact()
@@ -130,41 +132,42 @@ public interface IUser extends Deleteable, Identifiable {
 	 * @generated
 	 */
 	IContact getAssignedContact();
-
+	
 	/**
-	 * Sets the value of the '{@link ch.elexis.core.model.IUser#getAssignedContact <em>Assigned Contact</em>}' reference.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @param value the new value of the '<em>Assigned Contact</em>' reference.
+	 * Sets the value of the '{@link ch.elexis.core.model.IUser#getAssignedContact <em>Assigned
+	 * Contact</em>}' reference. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @param value
+	 *            the new value of the '<em>Assigned Contact</em>' reference.
 	 * @see #getAssignedContact()
 	 * @generated
 	 */
 	void setAssignedContact(IContact value);
-
+	
 	/**
-	 * Returns the value of the '<em><b>Roles</b></em>' reference list.
-	 * The list contents are of type {@link ch.elexis.core.model.IRole}.
-	 * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Roles</b></em>' reference list. The list contents are of
+	 * type {@link ch.elexis.core.model.IRole}. <!-- begin-user-doc -->
 	 * <p>
-	 * If the meaning of the '<em>Roles</em>' reference list isn't clear,
-	 * there really should be more of a description here...
+	 * This list is read only. Changes to it will not be stored. Use {@link #addRole(IRole)} to add
+	 * a role to this user.
 	 * </p>
 	 * <!-- end-user-doc -->
+	 * 
 	 * @return the value of the '<em>Roles</em>' reference list.
 	 * @see ch.elexis.core.model.ModelPackage#getIUser_Roles()
 	 * @model
 	 * @generated
 	 */
 	List<IRole> getRoles();
-
+	
 	/**
-	 * Returns the value of the '<em><b>Active</b></em>' attribute.
-	 * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Active</b></em>' attribute. <!-- begin-user-doc -->
 	 * <p>
-	 * If the meaning of the '<em>Active</em>' attribute isn't clear,
-	 * there really should be more of a description here...
+	 * If the meaning of the '<em>Active</em>' attribute isn't clear, there really should be more of
+	 * a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
+	 * 
 	 * @return the value of the '<em>Active</em>' attribute.
 	 * @see #setActive(boolean)
 	 * @see ch.elexis.core.model.ModelPackage#getIUser_Active()
@@ -172,25 +175,26 @@ public interface IUser extends Deleteable, Identifiable {
 	 * @generated
 	 */
 	boolean isActive();
-
+	
 	/**
-	 * Sets the value of the '{@link ch.elexis.core.model.IUser#isActive <em>Active</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @param value the new value of the '<em>Active</em>' attribute.
+	 * Sets the value of the '{@link ch.elexis.core.model.IUser#isActive <em>Active</em>}'
+	 * attribute. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @param value
+	 *            the new value of the '<em>Active</em>' attribute.
 	 * @see #isActive()
 	 * @generated
 	 */
 	void setActive(boolean value);
-
+	
 	/**
-	 * Returns the value of the '<em><b>Allow External</b></em>' attribute.
-	 * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Allow External</b></em>' attribute. <!-- begin-user-doc -->
 	 * <p>
-	 * If the meaning of the '<em>Allow External</em>' attribute isn't clear,
-	 * there really should be more of a description here...
+	 * If the meaning of the '<em>Allow External</em>' attribute isn't clear, there really should be
+	 * more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
+	 * 
 	 * @return the value of the '<em>Allow External</em>' attribute.
 	 * @see #setAllowExternal(boolean)
 	 * @see ch.elexis.core.model.ModelPackage#getIUser_AllowExternal()
@@ -198,25 +202,26 @@ public interface IUser extends Deleteable, Identifiable {
 	 * @generated
 	 */
 	boolean isAllowExternal();
-
+	
 	/**
-	 * Sets the value of the '{@link ch.elexis.core.model.IUser#isAllowExternal <em>Allow External</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @param value the new value of the '<em>Allow External</em>' attribute.
+	 * Sets the value of the '{@link ch.elexis.core.model.IUser#isAllowExternal <em>Allow
+	 * External</em>}' attribute. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @param value
+	 *            the new value of the '<em>Allow External</em>' attribute.
 	 * @see #isAllowExternal()
 	 * @generated
 	 */
 	void setAllowExternal(boolean value);
-
+	
 	/**
-	 * Returns the value of the '<em><b>Administrator</b></em>' attribute.
-	 * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Administrator</b></em>' attribute. <!-- begin-user-doc -->
 	 * <p>
-	 * If the meaning of the '<em>Administrator</em>' attribute isn't clear,
-	 * there really should be more of a description here...
+	 * If the meaning of the '<em>Administrator</em>' attribute isn't clear, there really should be
+	 * more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
+	 * 
 	 * @return the value of the '<em>Administrator</em>' attribute.
 	 * @see #setAdministrator(boolean)
 	 * @see ch.elexis.core.model.ModelPackage#getIUser_Administrator()
@@ -224,31 +229,33 @@ public interface IUser extends Deleteable, Identifiable {
 	 * @generated
 	 */
 	boolean isAdministrator();
-
+	
 	/**
-	 * Sets the value of the '{@link ch.elexis.core.model.IUser#isAdministrator <em>Administrator</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @param value the new value of the '<em>Administrator</em>' attribute.
+	 * Sets the value of the '{@link ch.elexis.core.model.IUser#isAdministrator
+	 * <em>Administrator</em>}' attribute. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @param value
+	 *            the new value of the '<em>Administrator</em>' attribute.
 	 * @see #isAdministrator()
 	 * @generated
 	 */
 	void setAdministrator(boolean value);
-
+	
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> Add a role to the user. Do not use
+	 * {@link #getRoles()#addRole(IRole)}, as it will not be stored. <!-- end-user-doc -->
+	 * 
 	 * @model roleRequired="true"
 	 * @generated
 	 */
 	IRole addRole(IRole role);
-
+	
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @model roleRequired="true"
 	 * @generated
 	 */
 	void removeRole(IRole role);
-
+	
 } // IUser
