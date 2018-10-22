974149347965f78265c5d777884a21f5c891086e
Marco Descher <marco@descher.at>
2018-10-22 15:25:10 +0200

    [14501] TarmedDefinitionen#NamedQueries

--- bundles/ch.elexis.core.jpa.entities/src/ch/elexis/core/jpa/entities/TarmedDefinitionen.java
+++ bundles/ch.elexis.core.jpa.entities/src/ch/elexis/core/jpa/entities/TarmedDefinitionen.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 import ch.elexis.core.jpa.entities.converter.BooleanCharacterConverterSafe;
@@ -15,9 +17,13 @@ import ch.elexis.core.jpa.entities.listener.EntityWithIdListener;
 @Entity
 @Table(name = "TARMED_DEFINITIONEN")
 @EntityListeners(EntityWithIdListener.class)
+@NamedQueries({
+	@NamedQuery(name = "TarmedDefinitionen.spalte.kuerzel", query = "SELECT td FROM TarmedDefinitionen td WHERE td.spalte = :spalte AND td.kuerzel = :kuerzel"),
+	@NamedQuery(name = "TarmedDefinitionen.spalte.titel", query = "SELECT td FROM TarmedDefinitionen td WHERE td.spalte = :spalte AND td.titel = :titel")
+})
 public class TarmedDefinitionen extends AbstractEntityWithId
 		implements EntityWithId, EntityWithDeleted {
-
+	
 	// Transparently updated by the EntityListener
 	protected Long lastupdate;
 	
@@ -32,45 +38,45 @@ public class TarmedDefinitionen extends AbstractEntityWithId
 	
 	@Column(length = 20)
 	private String spalte;
-
+	
 	@Column(length = 5)
 	private String kuerzel;
-
+	
 	@Column(length = 255)
 	private String titel;
-
+	
 	@Column(length = 3)
 	private String law;
-
-	public String getSpalte() {
+	
+	public String getSpalte(){
 		return spalte;
 	}
-
-	public void setSpalte(String spalte) {
+	
+	public void setSpalte(String spalte){
 		this.spalte = spalte;
 	}
-
-	public String getKuerzel() {
+	
+	public String getKuerzel(){
 		return kuerzel;
 	}
-
-	public void setKuerzel(String kuerzel) {
+	
+	public void setKuerzel(String kuerzel){
 		this.kuerzel = kuerzel;
 	}
-
-	public String getTitel() {
+	
+	public String getTitel(){
 		return titel;
 	}
-
-	public void setTitel(String titel) {
+	
+	public void setTitel(String titel){
 		this.titel = titel;
 	}
-
-	public String getLaw() {
+	
+	public String getLaw(){
 		return law;
 	}
-
-	public void setLaw(String law) {
+	
+	public void setLaw(String law){
 		this.law = law;
 	}
 	
