6ce205d523540ebddca10e31831ec17ba0882a12
Marco Descher <marco@descher.at>
2018-10-24 14:10:08 +0200

    [14501] IEncounterBuilder#date default to now

--- bundles/ch.elexis.core/src/ch/elexis/core/model/builder/IEncounterBuilder.java
+++ bundles/ch.elexis.core/src/ch/elexis/core/model/builder/IEncounterBuilder.java
@@ -8,14 +8,22 @@ import ch.elexis.core.model.IMandator;
 import ch.elexis.core.services.IModelService;
 
 public class IEncounterBuilder extends AbstractBuilder<IEncounter> {
-
-
+	
+	/**
+	 * Create an {@link IEncounter} with the provided parameters, and {@link IEncounter#getDate()}
+	 * now.
+	 * 
+	 * @param modelService
+	 * @param coverage
+	 * @param mandator
+	 */
 	public IEncounterBuilder(IModelService modelService, ICoverage coverage, IMandator mandator){
 		super(modelService);
 		
 		object = modelService.create(IEncounter.class);
 		object.setCoverage(coverage);
 		object.setMandator(mandator);
+		object.setDate(LocalDate.now());
 	}
 	
 	public IEncounterBuilder date(LocalDate date){
