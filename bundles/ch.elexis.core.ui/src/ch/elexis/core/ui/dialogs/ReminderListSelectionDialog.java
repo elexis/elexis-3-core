3e6e9a472a3c708708ca308768b31edfef0c896f
Marco Descher <marco@descher.at>
2018-11-20 14:50:16 +0100

    [4859] Allow closing reminders on login, show reminder detail

--- bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/dialogs/ReminderListSelectionDialog.java
+++ bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/dialogs/ReminderListSelectionDialog.java
@@ -3,7 +3,16 @@ package ch.elexis.core.ui.dialogs;
 import java.util.List;
 
 import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.dialogs.ListSelectionDialog;
 
 import ch.elexis.core.l10n.Messages;
@@ -19,11 +28,48 @@ import ch.elexis.data.Reminder;
  */
 public class ReminderListSelectionDialog extends ListSelectionDialog {
 	
+	private Label detailLabel;
+	private Composite detail;
+	
 	public ReminderListSelectionDialog(List<Reminder> input, String message){
 		super(UiDesk.getTopShell(), input, ArrayContentProvider.getInstance(),
 			PersistentObjectLabelProvider.getInstance(),
 			Messages.ReminderListSelectionDialog_SelectToClose);
 		setTitle(message);
+	}
+	
+	@Override
+	protected Control createDialogArea(Composite parent){
+		Control control = super.createDialogArea(parent);
+		detail = new Composite(parent, SWT.None);
+		detail.setLayout(new FillLayout(SWT.HORIZONTAL));
+		
+		detailLabel = new Label(detail, SWT.None);
+		
+		getViewer().addSelectionChangedListener(new ISelectionChangedListener() {
+			
+			@Override
+			public void selectionChanged(SelectionChangedEvent arg0){
+				final StringBuilder sb = new StringBuilder();
+				
+				if (!arg0.getSelection().isEmpty()) {
+					Reminder reminder =
+						(Reminder) ((IStructuredSelection) arg0.getSelection()).getFirstElement();
+					sb.append("    "+reminder.getSubject() + "\n");
+					sb.append("    "+reminder.getMessage());
+				}
+				detailLabel.setText(sb.toString());
+				detail.getParent().layout();
+			}
+		});
+		
+		@SuppressWarnings("unchecked")
+		List<Reminder> reminders = (List<Reminder>) getViewer().getInput();
+		if (reminders.size() > 0) {
+			getViewer().setSelection(new StructuredSelection(reminders.get(0)));
+		}
+		
+		return control;
 	}
 	
 	@Override
@@ -39,7 +85,6 @@ public class ReminderListSelectionDialog extends ListSelectionDialog {
 	
 	@Override
 	protected Point getInitialSize(){
-		return new Point(600, 250);
+		return new Point(600, 300);
 	}
-	
 }
