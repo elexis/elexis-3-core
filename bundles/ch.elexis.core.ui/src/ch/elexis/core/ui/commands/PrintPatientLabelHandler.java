5ff6e6dca9da6175a159e486942394c2d7501067
Daniel Rösch <dr@vality.ch>
2018-12-11 09:18:30 +0100

    Add license to GlobalActions print handlers

--- bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/commands/PrintPatientLabelHandler.java
+++ bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/commands/PrintPatientLabelHandler.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2018 IT-Med AG <info@it-med-ag.ch>.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IT-Med AG <info@it-med-ag.ch> - initial implementation
+ ******************************************************************************/
+
 package ch.elexis.core.ui.commands;
 
 import static ch.elexis.core.ui.text.TextTemplateRequirement.TT_PATIENT_LABEL;
