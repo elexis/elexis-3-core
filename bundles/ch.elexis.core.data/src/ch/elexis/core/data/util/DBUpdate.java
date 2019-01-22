c3ad852126f1b64b64adcf460ab3886018655282
thomashu <huster@medevit.at>
2019-01-22 17:51:25 +0100

    [14850] updated 3.7.0 db update with optional mysql set query_cache_type

--- bundles/ch.elexis.core.data/src/ch/elexis/core/data/util/DBUpdate.java
+++ bundles/ch.elexis.core.data/src/ch/elexis/core/data/util/DBUpdate.java
@@ -442,8 +442,7 @@ public class DBUpdate {
 			// 3.6.1
 			FILE_LOCATED,
 			// 3.7.0
-			"ALTER TABLE BEHANDLUNGEN ADD billable CHAR(1) default '1';"+
-			"ALTER TABLE BEHANDLUNGEN ADD Zeit CHAR(6) default '000000';"
+			FILE_LOCATED
 			};
 	//@formatter:on
 	
