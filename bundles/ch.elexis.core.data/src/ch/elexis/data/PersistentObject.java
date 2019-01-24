ceb0447cfcfb5f987fe9f8fd3a095ba4af7266b9
Niklaus Giger <niklaus.giger@member.fsf.org>
2019-01-24 08:08:57 +0100

    [14835] Show dialog when dbupdate fails. User may continue (#294)

--- bundles/ch.elexis.core.data/src/ch/elexis/data/PersistentObject.java
+++ bundles/ch.elexis.core.data/src/ch/elexis/data/PersistentObject.java
@@ -485,7 +485,17 @@ public abstract class PersistentObject implements IPersistentObject {
 		log.info("Gefundene Datenbankversion: " + vi.version());
 		if (vi.isOlder(CoreHub.DBVersion)) {
 			log.warn("Ältere Version der Datenbank gefunden ");
-			DBUpdate.doUpdate();
+			if (!DBUpdate.doUpdate()) {
+				String msg = String.format(
+						"Datenbank '%1s':\nUpdate auf '%2s' von '%3s' schlug fehlt.\nWollen Sie trotzdem fortsetzen?",
+						connection.getDBConnectString(), vi.version().toString(), CoreHub.DBVersion);
+				log.error(msg);
+				if (!cod.openQuestion("Datenbank update failed ", msg)) {
+					System.exit(8);
+				} else {
+					log.error("User continues with failed Elexis database update");
+				}
+			}
 		}
 		vi = new VersionInfo(CoreHub.globalCfg.get("ElexisVersion", "0.0.0"));
 		log.info("Verlangte Elexis-Version: " + vi.version());
