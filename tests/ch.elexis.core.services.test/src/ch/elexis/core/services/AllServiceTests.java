25b8ec29bb28e4728c939300f83aa681d44355dc
Marco Descher <marco@descher.at>
2019-04-16 13:07:43 +0200

    [14358] Run IVirtualFilesystemService tests

--- tests/ch.elexis.core.services.test/src/ch/elexis/core/services/AllServiceTests.java
+++ tests/ch.elexis.core.services.test/src/ch/elexis/core/services/AllServiceTests.java
@@ -14,7 +14,8 @@ import ch.elexis.core.utils.OsgiServiceUtil;
 
 @RunWith(Suite.class)
 @SuiteClasses({ AccessControlServiceTest.class, IConfigServiceTest.class, ILabServiceTest.class,
-		IStoreToStringServiceTest.class, IUserServiceTest.class, IStickerServiceTest.class })
+		IStoreToStringServiceTest.class, IUserServiceTest.class, IStickerServiceTest.class,
+		IVirtualFilesystemServiceTest.class })
 public class AllServiceTests {
 
 	private static IModelService modelService;
