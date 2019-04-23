1ac9a4a240ac0b480876ca68a909f600f1690bbc
Marco Descher <marco@descher.at>
2019-04-23 14:14:45 +0200

    [14358] Desk check po/nopo jdbclink equality (syserr out)

--- bundles/ch.elexis.core.application/src/ch/elexis/core/application/Desk.java
+++ bundles/ch.elexis.core.application/src/ch/elexis/core/application/Desk.java
@@ -107,8 +107,10 @@ public class Desk implements IApplication {
 		
 		String poConnectString = PersistentObject.getConnection().getConnectString();
 		String noPoConnectString = connection.get().connectionString;
-		if(!poConnectString.equalsIgnoreCase(noPoConnectString)) {
+		if (!poConnectString.equalsIgnoreCase(noPoConnectString)) {
 			log.error("Connection string differ po [{}] nopo [{}]", poConnectString, noPoConnectString);
+			System.err
+					.println("Connection string differ po [" + poConnectString + "] nopo [" + noPoConnectString + "]");
 		}
 		
 		// check for initialization parameters
