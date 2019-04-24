e1b2182965710677f78adbb2a3ee758130b4265c
Marco Descher <marco@descher.at>
2019-04-24 14:59:05 +0200

    [14946] Add context/lock servic to c.e.core.test

---
+++ bundles/ch.elexis.core.test/src/ch/elexis/core/test/service/TestContext.java
@@ -0,0 +1,115 @@
+package ch.elexis.core.test.service;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import ch.elexis.core.model.IContact;
+import ch.elexis.core.model.IMandator;
+import ch.elexis.core.model.IPatient;
+import ch.elexis.core.model.IPerson;
+import ch.elexis.core.model.IUser;
+import ch.elexis.core.model.builder.IContactBuilder;
+import ch.elexis.core.model.builder.IUserBuilder;
+import ch.elexis.core.services.IContext;
+import ch.elexis.core.services.IModelService;
+import ch.elexis.core.types.Gender;
+
+public class TestContext implements IContext {
+	
+	private IModelService coreModelService;
+	
+	private IMandator mandator;
+	private IUser user;
+	
+	public TestContext(IModelService coreModelService){
+		this.coreModelService = coreModelService;
+		
+		IPerson _mandator = new IContactBuilder.PersonBuilder(coreModelService, "Elisa",
+			"Mandatore", LocalDate.of(2000, 12, 1), Gender.FEMALE).mandator().buildAndSave();
+		mandator = coreModelService.load(_mandator.getId(), IMandator.class).get();
+		
+		user = new IUserBuilder(coreModelService, "user", _mandator).buildAndSave();
+	}
+	
+	@Override
+	public Optional<IUser> getActiveUser(){
+		return Optional.of(user);
+	}
+	
+	@Override
+	public void setActiveUser(IUser user){
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public Optional<IContact> getActiveUserContact(){
+		return Optional.empty();
+	}
+	
+	@Override
+	public void setActiveUserContact(IContact user){
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public Optional<IPatient> getActivePatient(){
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public void setActivePatient(IPatient patient){
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public Optional<IMandator> getActiveMandator(){
+		return Optional.of(mandator);
+	}
+	
+	@Override
+	public void setActiveMandator(IMandator mandator){
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public <T> Optional<T> getTyped(Class<T> clazz){
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public void setTyped(Object object){
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void removeTyped(Class<?> clazz){
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public Optional<?> getNamed(String name){
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public void setNamed(String name, Object object){
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public String getStationIdentifier(){
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
---
+++ bundles/ch.elexis.core.test/src/ch/elexis/core/test/service/TestLocalLockService.java
@@ -0,0 +1,104 @@
+package ch.elexis.core.test.service;
+
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ch.elexis.core.lock.types.LockInfo;
+import ch.elexis.core.lock.types.LockRequest;
+import ch.elexis.core.lock.types.LockResponse;
+import ch.elexis.core.services.ILocalLockService;
+
+@Component
+public class TestLocalLockService implements ILocalLockService {
+	
+	@Activate
+	public void activate(){
+		Logger logger = LoggerFactory.getLogger(getClass());
+		logger.error("Initializing test lock service");
+	}
+	
+	@Override
+	public LockResponse acquireOrReleaseLocks(LockRequest request){
+		return LockResponse.OK;
+	}
+	
+	@Override
+	public boolean isLocked(LockRequest request){
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public LockInfo getLockInfo(String storeToString){
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public LockResponse acquireLock(Object object){
+		return LockResponse.OK;
+	}
+	
+	@Override
+	public LockResponse releaseLock(Object object){
+		return LockResponse.OK;
+	}
+	
+	@Override
+	public LockResponse releaseLock(LockInfo lockInfo){
+		return LockResponse.OK;
+	}
+	
+	@Override
+	public boolean isLocked(Object object){
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public boolean isLockedLocal(Object po){
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public LockResponse releaseAllLocks(){
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public List<LockInfo> getCopyOfAllHeldLocks(){
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public String getSystemUuid(){
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public LockResponse acquireLockBlocking(Object po, int msTimeout, IProgressMonitor monitor){
+		return LockResponse.OK;
+	}
+	
+	@Override
+	public Status getStatus(){
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public void shutdown(){
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
