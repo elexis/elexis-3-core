a077882e648d640b81c83b9b0c2284d0ab4997d0
thomashu <huster@medevit.at>
2019-08-27 13:54:34 +0200

    [15779] updated EntityChangeEventListener with map for adapter lookup

--- bundles/ch.elexis.core.jpa/src/ch/elexis/core/jpa/model/adapter/event/EntityChangeEventListener.java
+++ bundles/ch.elexis.core.jpa/src/ch/elexis/core/jpa/model/adapter/event/EntityChangeEventListener.java
@@ -2,7 +2,9 @@ package ch.elexis.core.jpa.model.adapter.event;
 
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.WeakHashMap;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.event.Event;
@@ -22,34 +24,49 @@ public class EntityChangeEventListener implements EventHandler {
 	
 	private static Logger logger = LoggerFactory.getLogger(EntityChangeEventListener.class);
 	
-	private List<WeakReference<AbstractIdModelAdapter<?>>> listeners;
+	private WeakHashMap<EntityWithId, List<WeakReference<AbstractIdModelAdapter<?>>>> listenerMap;
 	
 	public EntityChangeEventListener(){
-		listeners = new ArrayList<>();
+		listenerMap =
+			new WeakHashMap<EntityWithId, List<WeakReference<AbstractIdModelAdapter<?>>>>();
 	}
 	
 	public void add(AbstractIdModelAdapter<?> adapter){
-		listeners.add(new WeakReference<AbstractIdModelAdapter<?>>(adapter));
+		synchronized (listenerMap) {
+			EntityWithId entity = adapter.getEntity();
+			List<WeakReference<AbstractIdModelAdapter<?>>> listeners = getListenersFor(entity);
+			
+			listeners.add(new WeakReference<AbstractIdModelAdapter<?>>(adapter));
+			listenerMap.put(entity, listeners);
+		}
+	}
+	
+	private List<WeakReference<AbstractIdModelAdapter<?>>> getListenersFor(EntityWithId entity){
+		List<WeakReference<AbstractIdModelAdapter<?>>> listeners = listenerMap.get(entity);
+		if (listeners == null) {
+			listeners = new ArrayList<WeakReference<AbstractIdModelAdapter<?>>>();
+		}
+		return listeners;
 	}
 	
 	@Override
 	public void handleEvent(Event event){
 		EntityWithId entity = (EntityWithId) event.getProperty(EntityWithId.class.getName());
-		List<WeakReference<AbstractIdModelAdapter<?>>> copyListeners = new ArrayList<>(listeners);
-		
-		logger.info("Refesh entity [" + entity + "] in " + copyListeners.size() + " listeners");
-		copyListeners.forEach(reference -> {
-			if (reference != null) {
-				AbstractIdModelAdapter<?> adapter = reference.get();
-				if (adapter != null) {
-					if (adapter.getEntity().equals(entity)) {
+		synchronized (listenerMap) {
+			List<WeakReference<AbstractIdModelAdapter<?>>> listeners = getListenersFor(entity);
+			
+			Iterator<WeakReference<AbstractIdModelAdapter<?>>> iter = listeners.iterator();
+			while (iter.hasNext()) {
+				WeakReference<AbstractIdModelAdapter<?>> reference = iter.next();
+				if (reference != null) {
+					AbstractIdModelAdapter<?> adapter = reference.get();
+					if (adapter != null) {
 						adapter.setEntity(entity);
+					} else {
+						iter.remove();
 					}
-				} else {
-					listeners.remove(reference);
 				}
 			}
-		});
-		logger.info("Refesh done ...");
+		}
 	}
 }
