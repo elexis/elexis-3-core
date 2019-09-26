5866c4d6f9d280d49918b0ddf7260c25013183b6
Marco Descher <marco@descher.at>
2019-09-26 15:48:17 +0200

    [15819] Fix IntegrationPostHandlerTest

--- bundles/ch.elexis.core.logback.rocketchat/src/ch/elexis/core/logback/rocketchat/test/IntegrationPostHandlerTest.java
+++ bundles/ch.elexis.core.logback.rocketchat/src/ch/elexis/core/logback/rocketchat/test/IntegrationPostHandlerTest.java
@@ -3,7 +3,7 @@ package ch.elexis.core.logback.rocketchat.test;
 import java.io.IOException;
 import java.net.MalformedURLException;
 
-import ch.elexis.core.logback.rocketchat.IntegrationPostHandler;
+import ch.elexis.core.logback.rocketchat.internal.IntegrationPostHandler;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
@@ -14,10 +14,11 @@ public class IntegrationPostHandlerTest {
 	
 	private static Logger testLogger = new LoggerContext().getLogger(IntegrationPostHandler.class);
 	
-	public static final String WEBHOOK_URL = "INSERT_URL_HERE";
+	public static final String WEBHOOK_URL = "https://rocketchat.medelexis.ch/hooks/4qyKfbsaGP4Ruwqqg/vXmDS5fW6aGkFwrLuAzZ7yPThjHGnhiG6e9y6hyBxaQ6GMt4";
+	
 	
 	public static void main(String[] args) throws MalformedURLException, IOException{
-		
+	
 		ILoggingEvent ile = new IntegrationPostHandlerTest().createNestedLoggingEvent(Level.DEBUG);
 		System.out.println(new IntegrationPostHandler(ile, null, true).post(WEBHOOK_URL));
 		
