4e67f8f20218935f12a8374c89acc7ec9f722a01
thomashu <huster@medevit.at>
2019-11-12 17:52:15 +0100

    [16917] added show all reminders option for current patient

--- bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/views/ReminderListsView.java
+++ bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/views/ReminderListsView.java
@@ -93,6 +93,8 @@ public class ReminderListsView extends ViewPart implements HeartListener, ISelec
 	private int filterDueDateDays = CoreHub.userCfg.get(Preferences.USR_REMINDER_FILTER_DUE_DAYS, -1);
 	private boolean autoSelectPatient = CoreHub.userCfg.get(Preferences.USR_REMINDER_AUTO_SELECT_PATIENT, false);
 	private boolean showOnlyDueReminders = CoreHub.userCfg.get(Preferences.USR_REMINDERSOPEN, false);
+	private boolean showAllReminders = (CoreHub.userCfg.get(Preferences.USR_REMINDEROTHERS, false)
+			&& CoreHub.acl.request(AccessControlDefaults.ADMIN_VIEW_ALL_REMINDERS));
 	private boolean showSelfCreatedReminders = CoreHub.userCfg.get(Preferences.USR_REMINDEROWN, false);
 
 	private Composite viewParent;
@@ -197,6 +199,21 @@ public class ReminderListsView extends ViewPart implements HeartListener, ISelec
 		public void run() {
 			autoSelectPatient = toggleAutoSelectPatientAction.isChecked();
 			CoreHub.userCfg.set(Preferences.USR_REMINDER_AUTO_SELECT_PATIENT, autoSelectPatient);
+		}
+	};
+
+	private Action showOthersRemindersAction = new RestrictedAction(AccessControlDefaults.ADMIN_VIEW_ALL_REMINDERS,
+			Messages.ReminderView_foreignAction, Action.AS_CHECK_BOX) {
+		{
+			setToolTipText(Messages.ReminderView_foreignTooltip);
+			setImageDescriptor(Images.IMG_ACHTUNG.getImageDescriptor());
+		}
+
+		@Override
+		public void doRun() {
+			showAllReminders = showOthersRemindersAction.isChecked();
+			CoreHub.userCfg.set(Preferences.USR_REMINDEROTHERS, showAllReminders);
+			refresh();
 		}
 	};
 
@@ -443,6 +460,7 @@ public class ReminderListsView extends ViewPart implements HeartListener, ISelec
 		menuManager.add(timeFilterSubMenu);
 		menuManager.add(showOnlyOwnDueReminderToggleAction);
 		menuManager.add(showSelfCreatedReminderAction);
+		menuManager.add(showOthersRemindersAction);
 
 		currentPatientViewer.getTable().setMenu(menuManager.createContextMenu(currentPatientViewer.getTable()));
 		generalPatientViewer.getTable().setMenu(menuManager.createContextMenu(generalPatientViewer.getTable()));
@@ -548,26 +566,36 @@ public class ReminderListsView extends ViewPart implements HeartListener, ISelec
 
 	private void refreshCurrentPatientInput() {
 		if (actPatient != null) {
-			List<Reminder> reminders = Reminder.findOpenRemindersResponsibleFor(CoreHub.actUser, showOnlyDueReminders,
-					filterDueDateDays, actPatient, false);
-
 			Query<Reminder> query = new Query<>(Reminder.class, null, null, Reminder.TABLENAME,
-					new String[] { Reminder.FLD_DUE, Reminder.FLD_PRIORITY, Reminder.FLD_ACTION_TYPE,
-							Reminder.FLD_CREATOR, Reminder.FLD_KONTAKT_ID });
-			if (showSelfCreatedReminders) {
-				query.add(Reminder.FLD_CREATOR, Query.EQUALS, CoreHub.actUser.getId());
+					new String[] { Reminder.FLD_DUE,
+					Reminder.FLD_PRIORITY, Reminder.FLD_ACTION_TYPE, Reminder.FLD_CREATOR, Reminder.FLD_KONTAKT_ID });
+			List<Reminder> reminders = Collections.emptyList();
+			if (showAllReminders && CoreHub.acl.request(AccessControlDefaults.ADMIN_VIEW_ALL_REMINDERS)) {
 				query.add(Reminder.FLD_KONTAKT_ID, Query.EQUALS, actPatient.getId());
 				if (filterDueDateDays != -1) {
 					applyDueDateFilter(query);
 				}
-				reminders.addAll(query.execute());
+				reminders = query.execute();
+			} else {
+				reminders = Reminder.findOpenRemindersResponsibleFor(CoreHub.actUser,
+						showOnlyDueReminders, filterDueDateDays, actPatient, false);
+
+				if (showSelfCreatedReminders) {
+					query.add(Reminder.FLD_CREATOR, Query.EQUALS, CoreHub.actUser.getId());
+					query.add(Reminder.FLD_KONTAKT_ID, Query.EQUALS, actPatient.getId());
+					if (filterDueDateDays != -1) {
+						applyDueDateFilter(query);
+					}
+					reminders.addAll(query.execute());
+				}
 			}
+			List<Reminder> input = reminders;
 			Display.getDefault().asyncExec(() -> {
 				if (currentPatientViewer != null && !currentPatientViewer.getTable().isDisposed()) {
-					currentPatientViewer.setInput(reminders);
+					currentPatientViewer.setInput(input);
 					viewerSelectionComposite.setCount(SELECTIONCOMP_CURRENTPATIENT_ID,
 							currentPatientViewer.getTable().getItemCount());
-					if (reminders.size() < 5) {
+					if (input.size() < 5) {
 						if (((GridData) currentPatientViewer.getTable().getLayoutData()).heightHint != 125) {
 							((GridData) currentPatientViewer.getTable().getLayoutData()).heightHint = 125;
 							currentPatientViewer.getTable().getParent().layout(true, true);
