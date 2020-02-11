44f592edc63b9f64e822535e2059ec7d16646cd8
thomashu <huster@medevit.at>
2020-02-11 19:15:02 +0100

    [18130] added active mandator block filter to BlockSelector

--- bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/views/codesystems/BlockSelector.java
+++ bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/views/codesystems/BlockSelector.java
@@ -18,6 +18,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
@@ -67,8 +68,9 @@ import ch.elexis.data.Query;
 
 public class BlockSelector extends CodeSelectorFactory {
 	protected static final String BLOCK_ONLY_FILTER_ENABLED = "blockselector/blockonlyfilter";
+	protected static final String BLOCK_FILTER_ONLY_MANDATOR = "blockselector/blockfilteronlymandator";
 	
-	private IAction deleteAction, createAction, exportAction, copyAction, searchBlocksOnly;
+	private IAction deleteAction, createAction, exportAction, copyAction, searchBlocksOnly, searchFilterMandator;
 	private CommonViewer cv;
 	private MenuManager mgr;
 	static SelectorPanelProvider slp;
@@ -130,7 +132,7 @@ public class BlockSelector extends CodeSelectorFactory {
 		}
 		
 		slp = new SelectorPanelProvider(lbName, true);
-		slp.addActions(createAction, exportAction, searchBlocksOnly);
+		slp.addActions(createAction, exportAction, searchBlocksOnly, searchFilterMandator);
 		ViewerConfigurer vc =
 			new ViewerConfigurer(new BlockContentProvider(this, cv),
 				new BlockTreeViewerItem.ColorizedLabelProvider(), slp,
@@ -256,6 +258,23 @@ public class BlockSelector extends CodeSelectorFactory {
 				CoreHub.userCfg.set(BLOCK_ONLY_FILTER_ENABLED, isChecked());
 			};
 		};
+		searchFilterMandator = new Action("Nur Blöcke des aktiven Mandanten", Action.AS_CHECK_BOX) {
+			{
+				setImageDescriptor(Images.IMG_PERSON.getImageDescriptor());
+				setToolTipText("Nur Blöcke des aktiven Mandanten");
+				setChecked(CoreHub.userCfg.get(BLOCK_FILTER_ONLY_MANDATOR, false));
+			}
+
+			public void run() {
+				CoreHub.userCfg.set(BLOCK_FILTER_ONLY_MANDATOR, isChecked());
+
+				if (cv.getConfigurer().getContentProvider() instanceof BlockContentProvider) {
+					BlockContentProvider blockContentProvider = (BlockContentProvider) cv.getConfigurer()
+							.getContentProvider();
+					blockContentProvider.refreshViewer();
+				}
+			}
+		};
 	}
 	
 	public static class BlockContentProvider implements
@@ -317,7 +336,7 @@ public class BlockSelector extends CodeSelectorFactory {
 			}
 			qbe.orderBy(false, Leistungsblock.FLD_NAME);
 			blockItemMap = new HashMap<>();
-			List<BlockTreeViewerItem> list = qbe.execute().stream().map(b -> {
+			List<BlockTreeViewerItem> list = qbe.execute().stream().filter(b -> applyMandatorFilter(b)).map(b -> {
 				BlockTreeViewerItem item = BlockTreeViewerItem.of(b);
 				blockItemMap.put(b, item);
 				return item;
@@ -325,6 +344,17 @@ public class BlockSelector extends CodeSelectorFactory {
 			return list.toArray();
 		}
 		
+		private boolean applyMandatorFilter(Leistungsblock b) {
+			if (selector.searchFilterMandator.isChecked()) {
+				Mandant mandator = ElexisEventDispatcher.getSelectedMandator();
+				String blockMandantId = b.get(Leistungsblock.FLD_MANDANT_ID);
+				if (StringUtils.isNotBlank(blockMandantId) && mandator != null) {
+					return blockMandantId.equals(mandator.getId());
+				}
+			}
+			return true;
+		}
+
 		public void dispose(){
 			stopListening();
 		}
