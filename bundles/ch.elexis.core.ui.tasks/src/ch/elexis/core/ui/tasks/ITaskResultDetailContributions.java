80ab7178f0ceba75565ece6c377f740a67a89e70
Marco Descher <marco@descher.at>
2020-02-19 13:35:59 +0100

    [18243] JavaDoc

--- bundles/ch.elexis.core.ui.tasks/src/ch/elexis/core/ui/tasks/ITaskResultDetailContributions.java
+++ bundles/ch.elexis.core.ui.tasks/src/ch/elexis/core/ui/tasks/ITaskResultDetailContributions.java
@@ -5,8 +5,17 @@ import java.util.Map;
 import org.eclipse.swt.widgets.Composite;
 
 import ch.elexis.core.tasks.model.ITask;
+import ch.elexis.core.tasks.model.TaskState;
 
 public interface ITaskResultDetailContributions {
 	
-	public void createDetailCompositeForTask(Composite parent, ITask task, Map<String, Object> services);
+	/**
+	 * @param parent
+	 * @param task
+	 *            in either {@link TaskState#COMPLETED} or {@link TaskState#COMPLETED_WARN} other
+	 *            states are covered by a generic composite
+	 * @param e4Services
+	 */
+	public void createDetailCompositeForTask(Composite parent, ITask task,
+		Map<String, Object> e4Services);
 }
