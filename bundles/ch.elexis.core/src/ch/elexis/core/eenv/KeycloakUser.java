d8b8847fba35db99e4426fc77e7910b3a6f4c16d
Marco Descher <marco@descher.at>
2020-03-10 14:47:31 +0100

    [18457] ch.elexis.core.eenv.KeycloakUser

---
+++ bundles/ch.elexis.core/src/ch/elexis/core/eenv/KeycloakUser.java
@@ -0,0 +1,182 @@
+package ch.elexis.core.eenv;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.List;
+
+import ch.elexis.core.model.IContact;
+import ch.elexis.core.model.IRole;
+import ch.elexis.core.model.IUser;
+import ch.elexis.core.model.IXid;
+import ch.elexis.core.services.IModelService;
+
+public class KeycloakUser implements IUser {
+	
+	private final ZoneOffset zone;
+	private final IModelService coreModelService;
+	
+	private final String preferredUsername;
+	private final String name;
+	private final String familyName;
+	private final String assignedContactId;
+	private final long issueTime;
+	private final long expirationTime;
+	
+	/**
+	 * Keycloak Represented User. This users data is populated via incoming JTW token. It maps to
+	 * the Elexis user world employing the core model service.
+	 * 
+	 * @param coreModelService
+	 * @param preferredUsername
+	 * @param name
+	 * @param familyName
+	 * @param issueTime
+	 *            jwt Issued At time (in seconds since Unix epoch)
+	 * @param expirationTime
+	 */
+	private KeycloakUser(IModelService coreModelService, String preferredUsername, String name,
+		String familyName, long issueTime, long expirationTime, String assignedContactId){
+		
+		this.preferredUsername = preferredUsername;
+		this.name = name;
+		this.familyName = familyName;
+		this.assignedContactId = assignedContactId;
+		this.issueTime = issueTime;
+		this.expirationTime = expirationTime;
+		
+		zone = ZoneOffset.of("Europe/Zurich");
+		this.coreModelService = coreModelService;
+	}
+	
+	@Override
+	public boolean isDeleted(){
+		return false;
+	}
+	
+	@Override
+	public void setDeleted(boolean value){}
+	
+	@Override
+	public String getId(){
+		return preferredUsername;
+	}
+	
+	@Override
+	public String getLabel(){
+		return name + " " + familyName;
+	}
+	
+	@Override
+	public boolean addXid(String domain, String id, boolean updateIfExists){
+		return false;
+	}
+	
+	@Override
+	public IXid getXid(String domain){
+		return null;
+	}
+	
+	@Override
+	public Long getLastupdate(){
+		return issueTime * 1000;
+	}
+	
+	@Override
+	public String getUsername(){
+		return preferredUsername;
+	}
+	
+	@Override
+	public void setUsername(String value){
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public String getHashedPassword(){
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public void setHashedPassword(String value){
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public String getSalt(){
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public void setSalt(String value){
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public IContact getAssignedContact(){
+		return coreModelService.load(assignedContactId, IContact.class).orElse(null);
+	}
+	
+	@Override
+	public void setAssignedContact(IContact value){
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public List<IRole> getRoles(){
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public boolean isActive(){
+		long nowEpochSecond = LocalDateTime.now().toEpochSecond(zone);
+		return expirationTime > nowEpochSecond;
+	}
+	
+	@Override
+	public void setActive(boolean value){
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public boolean isAllowExternal(){
+		return true;
+	}
+	
+	@Override
+	public void setAllowExternal(boolean value){
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public boolean isAdministrator(){
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public void setAdministrator(boolean value){
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public IRole addRole(IRole role){
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public void removeRole(IRole role){
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public IUser login(String username, char[] password){
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public boolean isInternal(){
+		return false;
+	}
+	
+}
