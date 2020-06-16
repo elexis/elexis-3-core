502b15a9a0715cab5bafe1c3a4ede3dd70224ec7
Marco Descher <marco@descher.at>
2020-06-16 10:37:47 +0200

    [20292] Provide command and contribution in c.e.c.ui.stock

---
+++ bundles/ch.elexis.core.ui.stock/src/ch/elexis/core/ui/stock/handler/StockSCSToggleArticleOutlayState.java
@@ -0,0 +1,21 @@
+package ch.elexis.core.ui.stock.handler;
+
+import org.eclipse.core.commands.State;
+
+import ch.elexis.core.constants.Preferences;
+import ch.elexis.core.data.service.ConfigServiceHolder;
+
+public class StockSCSToggleArticleOutlayState extends State {
+	
+	@Override
+	public Object getValue(){
+		return ConfigServiceHolder.get().getLocal(Preferences.INVENTORY_MACHINE_SUSPEND_OUTLAY,
+			Preferences.INVENTORY_MACHINE_SUSPEND_OUTLAY_DEFAULT);
+	}
+	
+	@Override
+	public void setValue(Object value){
+		ConfigServiceHolder.get().setLocal(Preferences.INVENTORY_MACHINE_SUSPEND_OUTLAY,
+			(Boolean) value);
+	}
+}
