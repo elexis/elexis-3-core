f610c74190557dee23889a86f3999de4135a5837
Marco Descher <marco@descher.at>
2020-06-16 10:37:47 +0200

    [20292] Provide command and contribution in c.e.c.ui.stock

---
+++ bundles/ch.elexis.core.ui.stock/src/ch/elexis/core/ui/stock/propertytester/PropertyTester.java
@@ -0,0 +1,29 @@
+package ch.elexis.core.ui.stock.propertytester;
+
+import java.util.List;
+import java.util.Optional;
+
+import ch.elexis.core.data.service.StockServiceHolder;
+import ch.elexis.core.model.IStock;
+
+public class PropertyTester extends org.eclipse.core.expressions.PropertyTester {
+	
+	// cache only
+	private Boolean isScsAvailable;
+	
+	@Override
+	public boolean test(Object receiver, String property, Object[] args, Object expectedValue){
+		if ("isSCSAvailable".equals(property)) {
+			if (isScsAvailable == null) {
+				List<IStock> allStocks = StockServiceHolder.get().getAllStocks(true);
+				Optional<IStock> scs =
+					allStocks.stream().filter(s -> s.isCommissioningSystem()).findFirst();
+				isScsAvailable = scs.isPresent();
+			}
+			return isScsAvailable;
+			
+		}
+		return false;
+	}
+	
+}
