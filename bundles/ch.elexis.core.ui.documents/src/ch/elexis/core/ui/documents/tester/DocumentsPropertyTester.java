77440b077da9ca4b546364eb6f2cfc198a4122ea
thomashu <huster@medevit.at>
2020-07-23 12:35:57 +0200

    [20706] add IDocumentConverter service interface, update doc prop tester

--- bundles/ch.elexis.core.ui.documents/src/ch/elexis/core/ui/documents/tester/DocumentsPropertyTester.java
+++ bundles/ch.elexis.core.ui.documents/src/ch/elexis/core/ui/documents/tester/DocumentsPropertyTester.java
@@ -1,6 +1,7 @@
 package ch.elexis.core.ui.documents.tester;
 
 import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.jface.viewers.TreeSelection;
 
 import ch.elexis.core.model.IDocument;
 
@@ -10,9 +11,12 @@ public class DocumentsPropertyTester extends PropertyTester {
 	public boolean test(Object receiver, String property, Object[] args, Object expectedValue){
 		if ("isMimeType".equals(property) && args != null && args.length == 1 //$NON-NLS-1$
 			&& args[0] instanceof String) {
+			if (receiver instanceof TreeSelection) {
+				receiver = ((TreeSelection) receiver).getFirstElement();
+			}
 			if (receiver instanceof IDocument) {
 				IDocument document = (IDocument) receiver;
-						return document.getMimeType().toLowerCase().endsWith((String) args[0]);
+				return document.getMimeType().toLowerCase().endsWith((String) args[0]);
 			}
 		}
 		return false;
