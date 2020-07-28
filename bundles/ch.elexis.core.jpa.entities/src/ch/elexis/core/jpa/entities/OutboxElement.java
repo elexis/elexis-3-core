a150f8597a1f56bbdd8175241c14e2a02ffe3953
thomashu <huster@medevit.at>
2020-07-28 12:51:31 +0200

    [20758] added outbox jpa entity

---
+++ bundles/ch.elexis.core.jpa.entities/src/ch/elexis/core/jpa/entities/OutboxElement.java
@@ -0,0 +1,112 @@
+package ch.elexis.core.jpa.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.eclipse.persistence.annotations.Cache;
+
+import ch.elexis.core.jpa.entities.converter.BooleanCharacterConverterSafe;
+import ch.elexis.core.jpa.entities.id.ElexisIdGenerator;
+import ch.elexis.core.jpa.entities.listener.EntityWithIdListener;
+
+@Entity
+@Table(name = "AT_MEDEVIT_ELEXIS_OUTBOX")
+@Cache(expiry = 15000)
+@EntityListeners(EntityWithIdListener.class)
+public class OutboxElement extends AbstractEntityWithId implements EntityWithId, EntityWithDeleted {
+	
+	// Transparently updated by the EntityListener
+	protected Long lastupdate;
+	
+	@Id
+	@GeneratedValue(generator = "system-uuid")
+	@Column(unique = true, nullable = false, length = 25)
+	private String id = ElexisIdGenerator.generateId();
+	
+	@Column
+	@Convert(converter = BooleanCharacterConverterSafe.class)
+	protected boolean deleted = false;
+	
+	@Override
+	public boolean isDeleted(){
+		return deleted;
+	}
+	
+	@Override
+	public void setDeleted(boolean deleted){
+		this.deleted = deleted;
+	}
+	
+	@Override
+	public String getId(){
+		return id;
+	}
+	
+	@Override
+	public void setId(String id){
+		this.id = id;
+	}
+	
+	@Override
+	public Long getLastupdate(){
+		return lastupdate;
+	}
+	
+	@Override
+	public void setLastupdate(Long lastupdate){
+		this.lastupdate = lastupdate;
+	}
+	
+	@ManyToOne
+	@JoinColumn(name = "patient")
+	private Kontakt patient;
+	
+	@ManyToOne
+	@JoinColumn(name = "mandant")
+	private Kontakt mandant;
+	
+	@Column(length = 1)
+	private String state;
+	
+	@Column(length = 255)
+	private String uri;
+	
+	public Kontakt getPatient(){
+		return patient;
+	}
+	
+	public void setPatient(Kontakt patient){
+		this.patient = patient;
+	}
+	
+	public Kontakt getMandant(){
+		return mandant;
+	}
+	
+	public void setMandant(Kontakt mandant){
+		this.mandant = mandant;
+	}
+	
+	public String getState(){
+		return state;
+	}
+	
+	public void setState(String state){
+		this.state = state;
+	}
+	
+	public String getUri(){
+		return uri;
+	}
+	
+	public void setUri(String uri){
+		this.uri = uri;
+	}
+}
