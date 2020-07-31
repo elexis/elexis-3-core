b53845383eb9cd9b51673dabd4a959676ac67474
Niklaus Giger <niklaus.giger@member.fsf.org>
2020-07-31 11:20:26 +0200

    [20669] Show correct elexis version when starting from Eclipse-IDE (#399)

--- bundles/ch.elexis.core/src/ch/elexis/core/constants/Elexis.java
+++ bundles/ch.elexis.core/src/ch/elexis/core/constants/Elexis.java
@@ -5,30 +5,31 @@ import java.io.InputStream;
 import java.net.URL;
 import java.util.Properties;
 
+import org.osgi.framework.FrameworkUtil;
 import org.slf4j.LoggerFactory;
 
 public final class Elexis {
-	
+
 	public static final String VERSION;
 	public static final String APPLICATION_NAME = "Elexis Core"; //$NON-NLS-1$
-	
+
 	static {
 		VERSION = initElexisBuildVersion();
 	}
-	
-	private static String initElexisBuildVersion(){
-		Properties prop = new Properties();
+
+	private static String initElexisBuildVersion() {
+		String version = FrameworkUtil.getBundle(Elexis.class).getVersion().toString();
 		String url_name = "platform:/plugin/ch.elexis.core.data/rsc/version.properties";
 		try (InputStream inputStream = new URL(url_name).openConnection().getInputStream()) {
 			if (inputStream != null) {
+				Properties prop = new Properties();
 				prop.load(inputStream);
-				return prop.getProperty("elexis.version").replace("-SNAPSHOT", "");
+				version = prop.getProperty("elexis.version");
+				LoggerFactory.getLogger(Elexis.class).trace("set version from " + url_name + " to " + version);
 			}
 		} catch (IOException e) {
-			LoggerFactory.getLogger(Elexis.class).error("Init error", e);
-			e.printStackTrace();
-			return "error";
-		}
-		return "unknown";
+			LoggerFactory.getLogger(Elexis.class).info(url_name + " not found", e);
+		};
+		return version;
 	}
 }
