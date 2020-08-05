f759efbb7c34d785d800b832e29dbf698b0055d4
thomashu <huster@medevit.at>
2020-08-05 15:37:52 +0200

    [20774] added default text template configuration and use on mail dialog

---
+++ bundles/ch.elexis.core.mail/src/ch/elexis/core/mail/PreferenceConstants.java
@@ -0,0 +1,6 @@
+package ch.elexis.core.mail;
+
+public interface PreferenceConstants {
+	public static final String PREF_COMMON_BASE = "mail/";
+	public static final String PREF_DEFAULT_TEMPLATE = PREF_COMMON_BASE + "template/default";
+}
