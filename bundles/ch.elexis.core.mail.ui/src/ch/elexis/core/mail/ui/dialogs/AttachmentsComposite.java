81135f614890b3c82e3a6896d69e79a11ebc23a1
thomashu <huster@medevit.at>
2020-08-11 10:45:27 +0200

    [20706] attachment open double click and remove separate, text layout

--- bundles/ch.elexis.core.mail.ui/src/ch/elexis/core/mail/ui/dialogs/AttachmentsComposite.java
+++ bundles/ch.elexis.core.mail.ui/src/ch/elexis/core/mail/ui/dialogs/AttachmentsComposite.java
@@ -1,15 +1,24 @@
 package ch.elexis.core.mail.ui.dialogs;
 
+import java.io.File;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.eclipse.core.commands.Command;
 import org.eclipse.jface.action.ToolBarManager;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.program.Program;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.ui.PlatformUI;
@@ -17,15 +26,16 @@ import org.eclipse.ui.commands.ICommandService;
 
 import ch.elexis.core.mail.AttachmentsUtil;
 import ch.elexis.core.model.IDocument;
+import ch.elexis.core.ui.icons.Images;
 
 public class AttachmentsComposite extends Composite {
 	
-	private Label attachmentsLabel;
-	private String attachmentsString = "";
 	private String attachments;
 	private String documents;
 	private Command createRocheLaborCommand;
 	private String postfix;
+	
+	private Composite attachmentsParent;
 	
 	public AttachmentsComposite(Composite parent, int style){
 		super(parent, style);
@@ -42,12 +52,12 @@ public class AttachmentsComposite extends Composite {
 		Label lbl = new Label(this, SWT.NONE);
 		lbl.setText("Anhang");
 		lbl.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, false, false));
-		attachmentsLabel = new Label(this, SWT.NONE);
-		attachmentsLabel.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
+		attachmentsParent = new Composite(this, SWT.NONE);
+		attachmentsParent.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		attachmentsParent.setLayout(new GridLayout(2, false));
 		
 		ToolBarManager mgr = new ToolBarManager();
 		mgr.add(new AddAttachmentAction(this));
-		mgr.add(new RemoveAttachmentAction(this));
 		if (createRocheLaborCommand != null && createRocheLaborCommand.isEnabled()) {
 			mgr.add(new LaborAttachmentAction(this, createRocheLaborCommand));
 		}
@@ -76,25 +86,76 @@ public class AttachmentsComposite extends Composite {
 	 */
 	public void setAttachments(String attachments){
 		this.attachments = attachments;
-		updateAttachmentsLabel();
+		updateAttachments();
 	}
 	
-	private void updateAttachmentsLabel(){
-		if (StringUtils.isNotBlank(attachments)) {
-			attachmentsString = attachments.replaceAll(":::", ",\n");
+	private void updateAttachments(){
+		attachmentsParent.setRedraw(false);
+		// clear all labels and rebuild
+		for (Control control : attachmentsParent.getChildren()) {
+			control.dispose();
 		}
-		if (StringUtils.isNotBlank(documents)) {
-			// replace if no attachments, else append
-			if (StringUtils.isNotBlank(attachments)) {
-				attachmentsString +=
-					AttachmentsUtil.toAttachments(documents).replaceAll(":::", ",\n");
-			} else {
-				attachmentsString =
-					AttachmentsUtil.toAttachments(documents).replaceAll(":::", ",\n");
+		if (StringUtils.isNotBlank(attachments)) {
+			String[] attachmentsParts = attachments.split(":::");
+			for (String string : attachmentsParts) {
+				Label label = new Label(attachmentsParent, SWT.NONE);
+				label.setText(FilenameUtils.getName(string));
+				label.setData(string);
+				label.setToolTipText("Mit Doppelklick öffnen (keine Änderungen)");
+				label.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseDoubleClick(MouseEvent e){
+						File file = new File((String) label.getData());
+						file.setReadOnly();
+						Program.launch((String) label.getData());
+					}
+				});
+				Label remove = new Label(attachmentsParent, SWT.NONE);
+				remove.setImage(Images.IMG_DELETE.getImage());
+				remove.setData(string);
+				remove.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseUp(MouseEvent e){
+						List<String> removeParts = Arrays.asList(getAttachments().split(":::"));
+						String removedString =
+							removeParts.stream().filter(part -> !part.equals(remove.getData()))
+							.collect(Collectors.joining(":::"));
+						setAttachments(removedString);
+					}
+				});
 			}
 		}
-		attachmentsLabel.setText(getAttachmentNames(attachmentsString));
-		attachmentsLabel.setToolTipText(attachmentsString);
+		if (StringUtils.isNotBlank(documents)) {
+			String[] documentsParts = documents.split(":::");
+			for (String string : documentsParts) {
+				Label label = new Label(attachmentsParent, SWT.NONE);
+				String tmpFile = AttachmentsUtil.toAttachment(string);
+				label.setText(FilenameUtils.getName(tmpFile));
+				label.setData(string);
+				label.setToolTipText("Mit Doppelklick öffnen (keine Änderungen)");
+				label.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseDoubleClick(MouseEvent e){
+						File file = new File(tmpFile);
+						file.setReadOnly();
+						Program.launch(tmpFile);
+					}
+				});
+				Label remove = new Label(attachmentsParent, SWT.NONE);
+				remove.setImage(Images.IMG_DELETE.getImage());
+				remove.setData(string);
+				remove.addMouseListener(new MouseAdapter() {
+					public void mouseUp(MouseEvent e){
+						List<String> removeParts = Arrays.asList(getDocuments().split(":::"));
+						String removedString =
+							removeParts.stream().filter(part -> !part.equals(remove.getData()))
+								.collect(Collectors.joining(":::"));
+						setDocuments(removedString);
+					};
+				});
+			}
+		}
+		attachmentsParent.setRedraw(true);
 		getParent().layout(true, true);
 	}
 	
@@ -108,7 +169,7 @@ public class AttachmentsComposite extends Composite {
 					":::" + AttachmentsUtil.getDocumentsString(Collections.singletonList(document));
 			}
 		}
-		updateAttachmentsLabel();
+		updateAttachments();
 	}
 	
 	/**
@@ -118,7 +179,7 @@ public class AttachmentsComposite extends Composite {
 	 */
 	public void setDocuments(String documents){
 		this.documents = documents;
-		updateAttachmentsLabel();
+		updateAttachments();
 	}
 	
 	public String getAttachments(){
