422dc7bd880ca4067f7efdd43d86eabcd1012ead
Marco Descher <marco@descher.at>
2020-09-10 10:11:55 +0200

    [17453] Provide ZeroDefaultStringInteger converters for databinding

---
+++ bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/util/ZeroDefaultIntegerStringConverter.java
@@ -0,0 +1,30 @@
+package ch.elexis.core.ui.util;
+
+import org.eclipse.core.databinding.conversion.Converter;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Safely convert a string to an int value. null or empty values are handled as value 0.
+ * 
+ * @since 3.8
+ */
+public class ZeroDefaultIntegerStringConverter extends Converter<String, Integer> {
+	
+	public ZeroDefaultIntegerStringConverter(){
+		super(String.class, Integer.class);
+	}
+	
+	@Override
+	public Integer convert(String fromObject){
+		if (fromObject != null && fromObject.length() > 0) {
+			try {
+				int value = Integer.valueOf(fromObject);
+				return value;
+			} catch (NumberFormatException nfe) {
+				LoggerFactory.getLogger(getClass()).warn("Conversion error, returning 0", nfe);
+			}
+		}
+		return 0;
+	}
+	
+}
