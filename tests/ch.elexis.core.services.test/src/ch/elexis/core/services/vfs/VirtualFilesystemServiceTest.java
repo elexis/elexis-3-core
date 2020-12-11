5f8241ff518608e8a512140d5ee8deb7dc5de931
Marco Descher <marco@descher.at>
2020-12-11 07:47:05 +0100

    [21604] VFSService parses UNC shares wrong

--- tests/ch.elexis.core.services.test/src/ch/elexis/core/services/vfs/VirtualFilesystemServiceTest.java
+++ tests/ch.elexis.core.services.test/src/ch/elexis/core/services/vfs/VirtualFilesystemServiceTest.java
@@ -4,10 +4,12 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import org.junit.Test;
 
 import ch.elexis.core.services.IVirtualFilesystemService;
+import ch.elexis.core.services.IVirtualFilesystemService.IVirtualFilesystemHandle;
 import ch.elexis.core.utils.OsgiServiceUtil;
 
 public class VirtualFilesystemServiceTest {
@@ -17,17 +19,16 @@ public class VirtualFilesystemServiceTest {
 	
 	@Test
 	public void of_unixoid() throws IOException{
-		assertEquals(new File("/tmp/test.txt"), service.of("/tmp/test.txt").toFile().get());
-		service.of("//tmp/test.txt");
-		service.of("file://tmp/test.txt");
-		service.of("file:///tmp/test.txt");
-		
+		File file = new File("/tmp/test.txt");
+		assertEquals(file, service.of("/tmp/test.txt").toFile().get());
+		assertEquals(file, service.of("//tmp/test.txt").toFile().get());
+		assertEquals(file, service.of("file:///tmp/test.txt").toFile().get());
 	}
 	
-//	@Test
-//	public void of_windows() throws IOException{
-//		assertEquals(new File("/tmp/test.txt"), service.of("C:/tmp/test.txt").toFile().get());
-//		service.of("\\server\test.txt");
-//	}
+	@Test
+	public void of_windows_share_UNC_Notation() throws IOException{
+		IVirtualFilesystemHandle unc_handle = service.of("\\\\medeserv\\elexisdata\\folder_name");
+		assertEquals(new URL("smb://medeserv/elexisdata/folder_name"), unc_handle.toURL());
+	}
 	
 }
