23dba8b540405f8300cdaaa68947587cbad7fe5a
Marco Descher <marco@descher.at>
2021-02-10 11:53:35 +0100

    [21972] IMAP support for c.e.c.mail

---
+++ bundles/ch.elexis.core.mail/src/ch/elexis/core/mail/IMAPMailMessage.java
@@ -0,0 +1,146 @@
+package ch.elexis.core.mail;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Part;
+
+import org.apache.commons.io.IOUtils;
+
+import com.sun.mail.imap.IMAPMessage;
+
+/**
+ * A self-sustainable representation of an IMAP message. It does not need a live IMAP connection, as
+ * all its properties have been loaded. The {@link #message} object is the original IMAP connection
+ * object.
+ */
+public class IMAPMailMessage {
+	
+	private final IMAPMessage message;
+	
+	private String sender;
+	private String subject;
+	private String text;
+	private List<Attachment> attachments;
+	private Date sentDate;
+	
+	public IMAPMailMessage(IMAPMessage message){
+		this.message = message;
+		this.attachments = new ArrayList<IMAPMailMessage.Attachment>();
+	}
+	
+	public static IMAPMailMessage of(IMAPMessage message) throws MessagingException{
+		IMAPMailMessage imapMailMessage = new IMAPMailMessage(message);
+		imapMailMessage.extractContent();
+		return imapMailMessage;
+	}
+	
+	private void extractContent() throws MessagingException{
+		sender = message.getSender().toString();
+		sentDate = message.getSentDate();
+		subject = message.getSubject();
+		
+		String contentType = message.getContentType();
+		
+		try {
+			Object content = message.getContent();
+			if (content instanceof Multipart) {
+				extractMultipartContent((Multipart) content);
+			} else if (content instanceof BodyPart) {
+				extractBodyPartContent((BodyPart) content);
+			} else {
+				extractOtherContent(contentType, content);
+			}
+			
+		} catch (IOException e) {
+			throw new MessagingException("Error reading attachments", e);
+		}
+	}
+	
+	private void extractOtherContent(String contentType, Object content){
+		if (contentType.toLowerCase().contains("text/plain")) {
+			text = (String) content;
+		}
+	}
+	
+	private void extractMultipartContent(Multipart multiPart)
+		throws MessagingException, IOException{
+		int numberOfParts = multiPart.getCount();
+		for (int partCount = 0; partCount < numberOfParts; partCount++) {
+			BodyPart part = multiPart.getBodyPart(partCount);
+			if (part.getContentType().contains("multipart")) {
+				extractMultipartContent((Multipart) part.getContent());
+			} else {
+				extractBodyPartContent(part);
+			}
+		}
+	}
+	
+	private void extractBodyPartContent(BodyPart part) throws MessagingException, IOException{
+		if (Part.ATTACHMENT.equalsIgnoreCase(part.getDisposition())) {
+			// this part is attachment
+			String fileName = part.getFileName();
+			byte[] content = IOUtils.toByteArray(part.getInputStream());
+			Attachment attachment = new Attachment(fileName, part.getSize(), content);
+			attachments.add(attachment);
+		} else {
+			extractOtherContent(part.getContentType(), part.getContent());
+		}
+	}
+	
+	public Message toIMAPMessage(){
+		return message;
+	}
+	
+	public String getSubject(){
+		return subject;
+	}
+	
+	public Date getSentDate(){
+		return sentDate;
+	}
+	
+	public String getSender(){
+		return sender;
+	}
+	
+	public String getText(){
+		return text;
+	}
+	
+	public List<Attachment> getAttachments(){
+		return attachments;
+	}
+	
+	public static class Attachment {
+		
+		private String filename;
+		private int size;
+		private byte[] content;
+		
+		public Attachment(String filename, int size, byte[] content){
+			this.filename = filename;
+			this.size = size;
+			this.content = content;
+		}
+		
+		public String getFilename(){
+			return filename;
+		}
+		
+		public byte[] getContent(){
+			return content;
+		}
+		
+		public int getSize(){
+			return size;
+		}
+	}
+	
+}
--- bundles/ch.elexis.core.mail/src/ch/elexis/core/mail/internal/MailClientProperties.java
+++ bundles/ch.elexis.core.mail/src/ch/elexis/core/mail/internal/MailClientProperties.java
@@ -28,10 +28,25 @@ public class MailClientProperties {
 			props.put("mail.smtp.port", account.getPort());
 			props.put("mail.smtp.starttls.enable", Boolean.toString(account.isStarttls()));
 		} else if (account.getType() == TYPE.IMAP) {
-			props.put("mail.transport.protocol", "imaps");
+			// IMAP properties
+			// https://javaee.github.io/javamail/docs/api/com/sun/mail/imap/package-summary.html
+			props.put("mail.store.protocol", "imap");
+			props.put("mail.imap.host", account.getHost());
+			props.put("mail.imap.port", account.getPort());
 			
+			if (account.getUsername() != null && account.getPassword() != null) {
+				props.put("mail.imap.user", account.getUsername());
+			}
+
+		} else if (account.getType() == TYPE.IMAPS) {
+			props.put("mail.store.protocol", "imaps");
 			props.put("mail.imaps.host", account.getHost());
 			props.put("mail.imaps.port", account.getPort());
+			
+			if (account.getUsername() != null && account.getPassword() != null) {
+				props.put("mail.imap.user", account.getUsername());
+			}
+
 		}
 		props.put("mail.debug", "true");
 		
