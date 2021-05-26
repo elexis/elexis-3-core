c855813321705740a9cad5c0e322a6b9b63ad210
thomas <thomas@thomas-hpelitebook840g5>
2021-05-26 17:56:33 +0200

    [22647] use encounter from context for block and favorites billing makro

--- bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/text/FavoritenKonsMakro.java
+++ bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/text/FavoritenKonsMakro.java
@@ -3,7 +3,6 @@ package ch.elexis.core.ui.text;
 import java.util.Optional;
 
 import ch.elexis.core.constants.StringConstants;
-import ch.elexis.core.data.events.ElexisEventDispatcher;
 import ch.elexis.core.data.interfaces.events.MessageEvent;
 import ch.elexis.core.model.IBillable;
 import ch.elexis.core.model.IBilled;
@@ -11,9 +10,8 @@ import ch.elexis.core.model.ICodeElementBlock;
 import ch.elexis.core.model.IEncounter;
 import ch.elexis.core.model.Identifiable;
 import ch.elexis.core.services.holder.BillingServiceHolder;
-import ch.elexis.core.services.holder.CoreModelServiceHolder;
+import ch.elexis.core.services.holder.ContextServiceHolder;
 import ch.elexis.core.ui.util.IKonsMakro;
-import ch.elexis.data.Konsultation;
 import ch.elexis.data.VerrechenbarFavorites;
 import ch.elexis.data.VerrechenbarFavorites.Favorite;
 import ch.rgw.tools.Result;
@@ -22,23 +20,24 @@ public class FavoritenKonsMakro implements IKonsMakro {
 	
 	@Override
 	public String executeMakro(String macro){
-		Konsultation actKons = (Konsultation) ElexisEventDispatcher.getSelected(Konsultation.class);
-		Favorite fav = VerrechenbarFavorites.findFavoritByMacroForCurrentUser(macro);
-		if (fav != null) {
-			IEncounter encounter = CoreModelServiceHolder.get().load(actKons.getId(), IEncounter.class).orElse(null);
-			Optional<Identifiable> favObj = fav.getObject();
-			if (favObj.isPresent()) {
-				if (favObj.get() instanceof ICodeElementBlock) {
-					BlockMakro blockMacro = new BlockMakro();
-					blockMacro.addBlock(actKons, (ICodeElementBlock) favObj.get());
-					return StringConstants.EMPTY;
-				} else if (favObj.get() instanceof IBillable) {
-					Result<IBilled> res =
-						BillingServiceHolder.get().bill((IBillable) favObj.get(), encounter, 1.0);
-					if (!res.isOK()) {
-						MessageEvent.fireError("Error", res.toString());
-					} else {
+		Optional<IEncounter> actEncounter = ContextServiceHolder.get().getTyped(IEncounter.class);
+		if (actEncounter.isPresent()) {
+			Favorite fav = VerrechenbarFavorites.findFavoritByMacroForCurrentUser(macro);
+			if (fav != null) {
+				Optional<Identifiable> favObj = fav.getObject();
+				if (favObj.isPresent()) {
+					if (favObj.get() instanceof ICodeElementBlock) {
+						BlockMakro blockMacro = new BlockMakro();
+						blockMacro.addBlock(actEncounter.get(), (ICodeElementBlock) favObj.get());
 						return StringConstants.EMPTY;
+					} else if (favObj.get() instanceof IBillable) {
+						Result<IBilled> res = BillingServiceHolder.get()
+							.bill((IBillable) favObj.get(), actEncounter.get(), 1.0);
+						if (!res.isOK()) {
+							MessageEvent.fireError("Error", res.toString());
+						} else {
+							return StringConstants.EMPTY;
+						}
 					}
 				}
 			}
