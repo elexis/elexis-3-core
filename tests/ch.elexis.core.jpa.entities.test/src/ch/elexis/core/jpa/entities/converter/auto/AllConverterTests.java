0fbab79582e1dfc7afa9a1aa9db35adaa43c7e90
Marco Descher <marco@descher.at>
2021-06-01 13:11:36 +0200

    [22666] Fix some tests

--- tests/ch.elexis.core.jpa.entities.test/src/ch/elexis/core/jpa/entities/converter/auto/AllConverterTests.java
+++ tests/ch.elexis.core.jpa.entities.test/src/ch/elexis/core/jpa/entities/converter/auto/AllConverterTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ LocalDateConverterTest.class, LocalDateTimeConverterTest.class})
+@SuiteClasses({ LocalDateConverterTest.class, LocalDateTimeConverterTest.class })
 public class AllConverterTests {
 
 }
