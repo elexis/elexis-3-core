54aa5c77a415d47af69033ac0785285f19046524
Marco Descher <marco@descher.at>
2021-08-30 09:03:51 +0200

    [15923] IElexisDataSource#getCurrentConnectionStatus

--- bundles/ch.elexis.core/src/ch/elexis/core/services/IElexisDataSource.java
+++ bundles/ch.elexis.core/src/ch/elexis/core/services/IElexisDataSource.java
@@ -3,8 +3,21 @@ package ch.elexis.core.services;
 import org.eclipse.core.runtime.IStatus;
 
 import ch.elexis.core.common.DBConnection;
+import ch.elexis.core.jdt.Nullable;
+import ch.elexis.core.status.ObjectStatus;
 
 public interface IElexisDataSource {
 	
 	public IStatus setDBConnection(DBConnection dbConnection);
+	
+	/**
+	 * Get the current connection status
+	 * 
+	 * @return <code>null</code> if no connection set, {@link ObjectStatus} containing a
+	 *         {@link DBConnection} if status is ok, or the resp. error message. The status code
+	 *         value is as follows:<br>
+	 *         1 testdb connection, 2 environment provided connection, 3 configuration provided
+	 *         connection
+	 */
+	public @Nullable ObjectStatus getCurrentConnectionStatus();
 }
