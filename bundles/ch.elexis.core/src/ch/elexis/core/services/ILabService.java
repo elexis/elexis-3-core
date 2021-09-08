5272ea203065db35546c463102b6355577afb7a4
Marco Descher <marco@descher.at>
2021-09-08 09:46:08 +0200

    [22949] ILabService#getLabOrdersInSameOrderIdGroup

--- bundles/ch.elexis.core/src/ch/elexis/core/services/ILabService.java
+++ bundles/ch.elexis.core/src/ch/elexis/core/services/ILabService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import ch.elexis.core.model.IContact;
 import ch.elexis.core.model.ILabItem;
 import ch.elexis.core.model.ILabMapping;
+import ch.elexis.core.model.ILabOrder;
 import ch.elexis.core.model.ILabResult;
 import ch.elexis.core.model.IPatient;
 import ch.elexis.core.types.LabItemTyp;
@@ -39,4 +40,15 @@ public interface ILabService {
 	 */
 	public List<ILabResult> getLabResultsForPatientWithItemType(IPatient patient, LabItemTyp type,
 		boolean includeDeleted);
+	
+	/**
+	 * Return all {@link ILabOrder} that are equal to the provided {@link LabOrder#getOrderid()} and
+	 * {@link LabOrder#getPatient()}
+	 * 
+	 * @param labOrder
+	 * @param nonEmptyResultsOnly return only {@link ILabResult} with a non empty result
+	 * @return
+	 * @since 3.8
+	 */
+	public List<ILabOrder> getLabOrdersInSameOrderIdGroup(ILabOrder labOrder, boolean nonEmptyResultsOnly);
 }
