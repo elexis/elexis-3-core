97dd8d617312e447d347e8216a12ffd5b6fdbcb8
Marco Descher <marco@descher.at>
2021-10-21 10:36:22 +0200

    [23301] SpotBugs - scariest - ch.elexis.core.ui.actions.HistoryLoader

--- bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/actions/HistoryLoader.java
+++ bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/actions/HistoryLoader.java
@@ -38,10 +38,11 @@ import ch.rgw.tools.VersionedResource;
  * 
  */
 public class HistoryLoader extends BackgroundJob {
-	StringBuilder sb;
+	
+	private StringBuilder sb;
 	private List<IEncounter> lKons;
-	KonsFilter filter;
-	IFilter globalFilter;
+	private KonsFilter filter;
+	private IFilter globalFilter;
 	private final int currentPage;
 	private final int pageSize;
 	boolean multiline = false;
@@ -73,120 +74,119 @@ public class HistoryLoader extends BackgroundJob {
 		this.currentPage = currentPage;
 		this.pageSize = pageSize;
 	}
+	
 	@Override
-	public IStatus execute(final IProgressMonitor monitor){
-		synchronized (lKons) {
-			monitor.beginTask(Messages.HistoryLoader_LoadKonsMessage, lKons.size() + 100); //$NON-NLS-1$
-			monitor.subTask(Messages.HistoryLoader_Sorting); //$NON-NLS-1$
-			if (lKons.isEmpty()) {
+	public synchronized IStatus execute(final IProgressMonitor monitor){
+		monitor.beginTask(Messages.HistoryLoader_LoadKonsMessage, lKons.size() + 100); //$NON-NLS-1$
+		monitor.subTask(Messages.HistoryLoader_Sorting); //$NON-NLS-1$
+		if (lKons.isEmpty()) {
+			return Status.OK_STATUS;
+		}
+		
+		Collections.sort(lKons, new Comparator<IEncounter>() {
+			@Override
+			public int compare(final IEncounter o1, final IEncounter o2){
+				if ((o1 == null) || (o2 == null)) {
+					return 0;
+				}
+				
+				int sort = o2.getDate().compareTo(o1.getDate());
+				if (sort != 0) {
+					return sort;
+				}
+				return Long.compare(o2.getLastupdate(), o1.getLastupdate());
+			}
+		});
+		
+		if (currentPage > 0 && pageSize > 0) {
+			// lazy loading via pagination
+			int fromIdx = (currentPage - 1) * pageSize;
+			int toIdx = currentPage * pageSize;
+			
+			// upper limit corrections
+			if (toIdx > lKons.size()) {
+				toIdx = lKons.size();
+				fromIdx = toIdx - pageSize;
+			}
+			// lower limit corrections
+			if (fromIdx < 0) {
+				fromIdx = 0;
+			}
+			lKons =
+				new ArrayList<IEncounter>(fromIdx < toIdx ? lKons.subList(fromIdx, toIdx) : lKons);
+		} else {
+			lKons = new ArrayList<IEncounter>(lKons);
+		}
+		monitor.worked(50);
+		
+		ICoverage selectedFall = ContextServiceHolder.get().getActiveCoverage().orElse(null);
+		Iterator<IEncounter> it = lKons.iterator();
+		sb.append("<form>"); //$NON-NLS-1$
+		//@TODO convert to jpa
+		globalFilter = ObjectFilterRegistry.getInstance().getFilterFor(Konsultation.class);
+		while (!monitor.isCanceled()) {
+			if (!it.hasNext()) {
+				sb.append("</form>"); //$NON-NLS-1$
+				result = sb.toString();
+				monitor.worked(1);
+				monitor.done();
 				return Status.OK_STATUS;
 			}
-			
-			Collections.sort(lKons, new Comparator<IEncounter>() {
-				public int compare(final IEncounter o1, final IEncounter o2){
-					if ((o1 == null) || (o2 == null)) {
-						return 0;
+			IEncounter k = it.next();
+			if (filter != null) {
+				if (filter.pass(k) == false) {
+					continue;
+				}
+			}
+			if (globalFilter != null) {
+				if (globalFilter.select(k) == false) {
+					continue;
+				}
+			}
+			if (k != null) {
+				VersionedResource vr = k.getVersionedEntry();
+				String s = vr.getHead();
+				if (s != null) {
+					if (s.startsWith("<")) { //$NON-NLS-1$
+						Samdas samdas = new Samdas(s);
+						s = samdas.getRecordText();
+					}
+					s = maskHTML(s);
+					if (multiline) {
+						// TODO use system line separator
+						// replace Windows line separator
+						s = s.replaceAll("\r\n", "<br/>"); //$NON-NLS-1$ //$NON-NLS-2$
+						// replace remaining "manual" line separators
+						s = s.replaceAll("\n", "<br/>"); //$NON-NLS-1$ //$NON-NLS-2$
 					}
 					
-					int sort = o2.getDate().compareTo(o1.getDate());
-					if (sort != 0) {
-						return sort;
-					}
-					return Long.compare(o2.getLastupdate(), o1.getLastupdate());
+				} else {
+					s = ""; //$NON-NLS-1$
 				}
-			});
-
-			if (currentPage > 0 && pageSize > 0)
-			{
-				// lazy loading via pagination
-				int fromIdx = (currentPage - 1) * pageSize;
-				int toIdx = currentPage * pageSize;
-				
-				// upper limit corrections
-				if (toIdx > lKons.size()) {
-					toIdx = lKons.size();
-					fromIdx = toIdx - pageSize;
-				}
-				// lower limit corrections
-				if (fromIdx < 0) {
-					fromIdx = 0;
-				}
-				lKons = new ArrayList<IEncounter>(
-					fromIdx < toIdx ? lKons.subList(fromIdx, toIdx) : lKons);
-			} else {
-				lKons = new ArrayList<IEncounter>(lKons);
-			}
-			monitor.worked(50);
-			
-			ICoverage selectedFall = ContextServiceHolder.get().getActiveCoverage().orElse(null);
-			Iterator<IEncounter> it = lKons.iterator();
-			sb.append("<form>"); //$NON-NLS-1$
-			//@TODO convert to jpa
-			globalFilter = ObjectFilterRegistry.getInstance().getFilterFor(Konsultation.class);
-			while (!monitor.isCanceled()) {
-				if (!it.hasNext()) {
-					sb.append("</form>"); //$NON-NLS-1$
-					result = sb.toString();
-					monitor.worked(1);
-					monitor.done();
-					return Status.OK_STATUS;
-				}
-				IEncounter k = it.next();
-				if (filter != null) {
-					if (filter.pass(k) == false) {
-						continue;
-					}
-				}
-				if (globalFilter != null) {
-					if (globalFilter.select(k) == false) {
-						continue;
-					}
-				}
-				if (k != null) {
-					VersionedResource vr = k.getVersionedEntry();
-					String s = vr.getHead();
-					if (s != null) {
-						if (s.startsWith("<")) { //$NON-NLS-1$
-							Samdas samdas = new Samdas(s);
-							s = samdas.getRecordText();
-						}
-						s = maskHTML(s);
-						if (multiline) {
-							// TODO use system line separator
-							// replace Windows line separator
-							s = s.replaceAll("\r\n", "<br/>"); //$NON-NLS-1$ //$NON-NLS-2$
-							// replace remaining "manual" line separators
-							s = s.replaceAll("\n", "<br/>"); //$NON-NLS-1$ //$NON-NLS-2$
-						}
-						
-					} else {
-						s = ""; //$NON-NLS-1$
-					}
-					String label = maskHTML(k.getLabel());
-					// make kons text grey if kons Fall is not the selected Fall
-					if (selectedFall != null && !selectedFall.equals(k.getCoverage())) {
-						sb.append("<p><a href=\"") //$NON-NLS-1$
-							.append(maskHTML(k.getId())).append("\">") //$NON-NLS-1$
-							.append(label).append("</a><br/>") //$NON-NLS-1$
-							.append("<span color=\"gruen\">") //$NON-NLS-1$
-							.append(maskHTML(k.getCoverage().getLabel()))
-							.append("</span><br/><span color=\"gruen\">") //$NON-NLS-1$
-							.append(s).append("</span></p>"); //$NON-NLS-1$
-					} else {
-						sb.append("<p><a href=\"") //$NON-NLS-1$
+				String label = maskHTML(k.getLabel());
+				// make kons text grey if kons Fall is not the selected Fall
+				if (selectedFall != null && !selectedFall.equals(k.getCoverage())) {
+					sb.append("<p><a href=\"") //$NON-NLS-1$
 						.append(maskHTML(k.getId())).append("\">") //$NON-NLS-1$
 						.append(label).append("</a><br/>") //$NON-NLS-1$
 						.append("<span color=\"gruen\">") //$NON-NLS-1$
-							.append(maskHTML(k.getCoverage().getLabel())).append("</span><br/>") //$NON-NLS-1$
-							.append(s).append("</p>"); //$NON-NLS-1$
-					}
+						.append(maskHTML(k.getCoverage().getLabel()))
+						.append("</span><br/><span color=\"gruen\">") //$NON-NLS-1$
+						.append(s).append("</span></p>"); //$NON-NLS-1$
+				} else {
+					sb.append("<p><a href=\"") //$NON-NLS-1$
+						.append(maskHTML(k.getId())).append("\">") //$NON-NLS-1$
+						.append(label).append("</a><br/>") //$NON-NLS-1$
+						.append("<span color=\"gruen\">") //$NON-NLS-1$
+						.append(maskHTML(k.getCoverage().getLabel())).append("</span><br/>") //$NON-NLS-1$
+						.append(s).append("</p>"); //$NON-NLS-1$
 				}
-				monitor.worked(1);
-				
 			}
-			sb.setLength(0);
-			monitor.done();
+			monitor.worked(1);
+			
 		}
+		sb.setLength(0);
+		monitor.done();
 		return Status.CANCEL_STATUS;
 	}
 	
