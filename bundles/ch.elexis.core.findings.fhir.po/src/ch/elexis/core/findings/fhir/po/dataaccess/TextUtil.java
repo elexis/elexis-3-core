315ec4fb8c7b6432093f320398c2ef25ca6e55f2
thomas <huster@medevit.at>
2021-12-13 11:25:43 +0100

    [23511] remove condition status from text for documents

--- bundles/ch.elexis.core.findings.fhir.po/src/ch/elexis/core/findings/fhir/po/dataaccess/TextUtil.java
+++ bundles/ch.elexis.core.findings.fhir.po/src/ch/elexis/core/findings/fhir/po/dataaccess/TextUtil.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import ch.elexis.core.findings.IAllergyIntolerance;
 import ch.elexis.core.findings.ICoding;
 import ch.elexis.core.findings.ICondition;
-import ch.elexis.core.findings.ICondition.ConditionStatus;
 import ch.elexis.core.findings.IFamilyMemberHistory;
 import ch.elexis.core.findings.IObservation;
 import ch.elexis.core.findings.IObservation.ObservationCategory;
@@ -59,8 +58,7 @@ public class TextUtil {
 	 */
 	public static String getText(ICondition condition, ICodingService codingService){
 		StringBuilder sb = new StringBuilder();
-		ConditionStatus status = condition.getStatus();
-		sb.append(status.getLocalized()).append(" (");
+		sb.append("(");
 		Optional<String> start = condition.getStart();
 		sb.append(start.orElse("")).append(" - ");
 		Optional<String> end = condition.getEnd();
