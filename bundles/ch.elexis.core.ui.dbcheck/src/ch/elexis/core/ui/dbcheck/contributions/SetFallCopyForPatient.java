54ad465921855fa4d7ed826df67537aa2a1c6a0f
thomas <huster@medevit.at>
2021-12-22 16:28:00 +0100

    [23542] add dbcheck tool to set copy for patient in open TP coverages

---
+++ bundles/ch.elexis.core.ui.dbcheck/src/ch/elexis/core/ui/dbcheck/contributions/SetFallCopyForPatient.java
@@ -0,0 +1,46 @@
+package ch.elexis.core.ui.dbcheck.contributions;
+
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import ch.elexis.core.ui.dbcheck.external.ExternalMaintenance;
+import ch.elexis.data.Fall;
+import ch.elexis.data.Fall.Tiers;
+import ch.elexis.data.Query;
+
+public class SetFallCopyForPatient extends ExternalMaintenance {
+	
+	@Override
+	public String executeMaintenance(IProgressMonitor pm, String DBVersion){
+		StringBuilder sb = new StringBuilder();
+		
+		Query<Fall> query = new Query<Fall>(Fall.class);
+		List<Fall> allFaelle = query.execute();
+		sb.append(allFaelle.size() + " Fälle insgesamt.\n");
+		pm.beginTask("Set copy to patient...", allFaelle.size());
+		boolean setValue = true;
+		int tpFaelle = 0;
+		int changedFaelle = 0;
+		for (Fall fall : allFaelle) {
+			if (fall.isOpen() && fall.getTiersType() == Tiers.PAYANT) {
+				tpFaelle++;
+				if (setValue != fall.getCopyForPatient()) {
+					changedFaelle++;
+				}
+				fall.setCopyForPatient(setValue);
+				
+			}
+			pm.worked(1);
+		}
+		sb.append("Kopie an Patienten, in " + changedFaelle + " von " + tpFaelle
+			+ " TP Fällen angepasst.");
+		return sb.toString();
+	}
+	
+	@Override
+	public String getMaintenanceDescription(){
+		return "Kopie an Patienten, aller editierbaren TP Fälle setzen";
+	}
+	
+}
