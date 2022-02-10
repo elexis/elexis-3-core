791e7b3856f309562be8833a79feb4bbab7079bf
thomas <huster@medevit.at>
2022-02-10 13:33:43 +0100

    [23656] add GsonAutoDiscoverable to register jersey reader and writer

---
+++ bundles/ch.elexis.core.services/src/ch/elexis/core/services/es/GsonAutoDiscoverable.java
@@ -0,0 +1,32 @@
+package ch.elexis.core.services.es;
+
+import javax.inject.Singleton;
+import javax.ws.rs.core.FeatureContext;
+import javax.ws.rs.ext.MessageBodyReader;
+import javax.ws.rs.ext.MessageBodyWriter;
+
+import org.glassfish.jersey.internal.inject.AbstractBinder;
+import org.glassfish.jersey.internal.spi.ForcedAutoDiscoverable;
+
+/**
+ * JAXB {@link ForcedAutoDiscoverable} that registers all necessary JAXB features into the injection
+ * manager directly.
+ *
+ * @author Jakub Podlesak
+ */
+public final class GsonAutoDiscoverable implements ForcedAutoDiscoverable {
+	
+	@Override
+	public void configure(final FeatureContext context){
+		context.register(new GsonMessagingBinder());
+	}
+	
+	private class GsonMessagingBinder extends AbstractBinder {
+		
+		@Override
+		protected void configure(){
+			bind(GsonProvider.class).to(MessageBodyReader.class).in(Singleton.class);
+			bind(GsonProvider.class).to(MessageBodyWriter.class).in(Singleton.class);
+		}
+	}
+}
