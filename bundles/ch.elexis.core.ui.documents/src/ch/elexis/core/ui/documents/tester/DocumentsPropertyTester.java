0cc024632c8ae8306ab99a484df62d7e81c790f9
thomas <huster@medevit.at>
2022-02-14 17:25:29 +0100

    [23665] hide context menu local editing if document is extern file

--- bundles/ch.elexis.core.ui.documents/src/ch/elexis/core/ui/documents/tester/DocumentsPropertyTester.java
+++ bundles/ch.elexis.core.ui.documents/src/ch/elexis/core/ui/documents/tester/DocumentsPropertyTester.java
@@ -1,9 +1,13 @@
 package ch.elexis.core.ui.documents.tester;
 
+import java.util.List;
+
 import org.eclipse.core.expressions.PropertyTester;
 import org.eclipse.jface.viewers.TreeSelection;
 
+import ch.elexis.core.constants.Preferences;
 import ch.elexis.core.model.IDocument;
+import ch.elexis.core.services.holder.ConfigServiceHolder;
 
 public class DocumentsPropertyTester extends PropertyTester {
 
@@ -14,11 +18,53 @@ public class DocumentsPropertyTester extends PropertyTester {
 			if (receiver instanceof TreeSelection) {
 				receiver = ((TreeSelection) receiver).getFirstElement();
 			}
-			if (receiver instanceof IDocument) {
-				IDocument document = (IDocument) receiver;
-				return document.getMimeType().toLowerCase().endsWith((String) args[0]);
+			if (receiver instanceof List) {
+				for (Object object : (List<?>) receiver) {
+					return testIsMimeType(object, (String) args[0]);
+				}
+			} else {
+				return testIsMimeType(receiver, (String) args[0]);
+			}
+		} else if ("isExternFile".equals(property)) {
+			if (receiver instanceof TreeSelection) {
+				receiver = ((TreeSelection) receiver).getFirstElement();
+			}
+			if (receiver instanceof List) {
+				for (Object object : (List<?>) receiver) {
+					return testIsExternFile(object);
+				}
+			} else {
+				return testIsExternFile(receiver);
 			}
 		}
 		return false;
+	}
+	
+	private boolean testIsMimeType(Object receiver, String mime){
+		if (receiver instanceof IDocument) {
+			IDocument document = (IDocument) receiver;
+			return document.getMimeType().toLowerCase().endsWith(mime);
+		}
+		return false;
+	}
+	
+	private boolean testIsExternFile(Object receiver){
+		if (receiver instanceof IDocument) {
+			IDocument document = (IDocument) receiver;
+			if ("ch.elexis.data.store.brief".equals(document.getStoreId())) {
+				return ConfigServiceHolder.getGlobal(Preferences.P_TEXT_EXTERN_FILE, false);
+			} else if ("ch.elexis.data.store.omnivore".equals(document.getStoreId())) {
+				return isOmnivoreStoreInFilesystem();
+			}
+		}
+		return false;
+	}
+	
+	private boolean isOmnivoreStoreInFilesystem(){
+		if (ConfigServiceHolder.getGlobal("ch.elexis.omnivore/store_in_fs_global", false)) {
+			return ConfigServiceHolder.getGlobal("ch.elexis.omnivore/store_in_fs", false);
+		} else {
+			return ConfigServiceHolder.getLocal("ch.elexis.omnivore/store_in_fs", false);
+		}
 	}
 }
