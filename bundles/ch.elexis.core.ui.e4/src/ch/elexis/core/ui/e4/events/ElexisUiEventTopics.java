011bf81cc7a1e5fadad24e9c76b0f14e2aa39e09
Marco Descher <marco@descher.at>
2022-02-28 10:06:50 +0100

    [23776] EVENT_PREVIEW_MIMETYPE_PDF use IDocument instead of InputStream

--- bundles/ch.elexis.core.ui.e4/src/ch/elexis/core/ui/e4/events/ElexisUiEventTopics.java
+++ bundles/ch.elexis.core.ui.e4/src/ch/elexis/core/ui/e4/events/ElexisUiEventTopics.java
@@ -1,15 +1,16 @@
 package ch.elexis.core.ui.e4.events;
 
-import java.io.InputStream;
+import ch.elexis.core.model.IDocument;
 
 public class ElexisUiEventTopics {
 	
 	public static final String EVENT_BASE_UI = "ui/";
-
-	public static final String EVENT_PREVIEW_MIMETYPE = EVENT_BASE_UI+"preview/mimetype/";
+	
+	public static final String EVENT_PREVIEW_MIMETYPE = EVENT_BASE_UI + "preview/mimetype/";
 	/**
-	 * Expects {@link InputStream}
+	 * Expects {@link IDocument}, will call {@link IDocument#getContent()}
 	 */
-	public static final String EVENT_PREVIEW_MIMETYPE_PDF = EVENT_PREVIEW_MIMETYPE+"application/pdf";
-
+	public static final String EVENT_PREVIEW_MIMETYPE_PDF =
+		EVENT_PREVIEW_MIMETYPE + "application/pdf";
+	
 }
