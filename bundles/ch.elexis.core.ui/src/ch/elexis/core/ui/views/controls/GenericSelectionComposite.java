d13f25e615a8e2b9bee761d278d996c75962d8e5
pdenzler <patrick.denzlers@gmail.com>
2022-04-27 13:56:42 +0200

    [23892] add generic dialog to view Rechnungs-Vorschlag

--- bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/views/controls/GenericSelectionComposite.java
+++ bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/views/controls/GenericSelectionComposite.java
@@ -10,10 +10,7 @@
  ******************************************************************************/
 package ch.elexis.core.ui.views.controls;
 
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 import org.eclipse.core.runtime.ListenerList;
 import org.eclipse.jface.dialogs.Dialog;
@@ -24,18 +21,13 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Shell;
-
-import ch.elexis.data.PersistentObject;
 
 /**
  * A {@link Composite} to manage the selection from a list of objects. A
@@ -47,16 +39,25 @@ import ch.elexis.data.PersistentObject;
  */
 public class GenericSelectionComposite extends Composite implements ISelectionProvider {
 
-	private ListenerList selectionListeners = new ListenerList();
+	private ListenerList<ISelectionChangedListener> selectionListeners = new ListenerList<ISelectionChangedListener>();
 
 	private List<?> input;
 	private IStructuredSelection selection;
+	private String shellTitle, title, message;
 
 	private Label selectLabel;
 	private Button selectButton;
 
 	public GenericSelectionComposite(Composite parent, int style) {
 		super(parent, style);
+		createContent();
+	}
+
+	public GenericSelectionComposite(Composite parent, int style, String shellTitle, String title, String message) {
+		super(parent, style);
+		this.shellTitle = shellTitle;
+		this.title = title;
+		this.message = message;
 		createContent();
 	}
 
@@ -75,7 +76,8 @@ public class GenericSelectionComposite extends Composite implements ISelectionPr
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				if (input != null && !input.isEmpty()) {
-					GenericSelectionDialog dialog = new GenericSelectionDialog(getShell(), input);
+					GenericSearchSelectionDialog dialog = new GenericSearchSelectionDialog(getShell(), input,
+							shellTitle, title, message, null, SWT.CHECK);
 					if (selection != null) {
 						dialog.setSelection(selection.toList());
 					}
@@ -93,7 +95,7 @@ public class GenericSelectionComposite extends Composite implements ISelectionPr
 		StringBuilder sb = new StringBuilder();
 		if (selection != null && !selection.isEmpty()) {
 			for (Object object : selection.toList()) {
-				String label = GenericSelectionDialog.getLabel(object);
+				String label = GenericSearchSelectionDialog.getLabel(object);
 				if (label != null && !label.isEmpty()) {
 					if (sb.length() > 0) {
 						sb.append(", ").append(label);
@@ -145,92 +147,6 @@ public class GenericSelectionComposite extends Composite implements ISelectionPr
 		if (selection instanceof IStructuredSelection) {
 			this.selection = (IStructuredSelection) selection;
 			updateLabel();
-		}
-	}
-
-	/**
-	 * Dialog for managing the selection from a list of objects. For each Object a
-	 * checkbox with text is displayed. For {@link PersistentObject} instances the
-	 * Label property is displayed, else toString.
-	 *
-	 * @author thomas
-	 *
-	 */
-	public static class GenericSelectionDialog extends Dialog {
-
-		private List<?> input;
-		private Map<Object, Button> buttonMap = new HashMap<>();
-		private List<Object> selection = new LinkedList<>();
-
-		public GenericSelectionDialog(Shell parentShell, List<?> input) {
-			super(parentShell);
-			this.input = input;
-		}
-
-		public void setSelection(List<Object> selection) {
-			this.selection = new LinkedList<>(selection);
-		}
-
-		public IStructuredSelection getSelection() {
-			return new StructuredSelection(selection);
-		}
-
-		@Override
-		protected Control createDialogArea(Composite parent) {
-			Composite ret = (Composite) super.createDialogArea(parent);
-			ScrolledComposite sc = new ScrolledComposite(ret, SWT.H_SCROLL | SWT.V_SCROLL);
-
-			Composite child = new Composite(sc, SWT.NONE);
-			child.setLayout(new GridLayout());
-
-			GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
-			data.heightHint = 400;
-			sc.setLayoutData(data);
-
-			Label title = new Label(child, SWT.NONE);
-			title.setText("Auswahl:");
-			// create the UI
-			for (Object object : input) {
-				Button button = new Button(child, SWT.CHECK);
-				button.setText(getLabel(object));
-				button.addSelectionListener(new SelectionAdapter() {
-					@Override
-					public void widgetSelected(SelectionEvent e) {
-						if (button.getSelection()) {
-							selection.add(object);
-						} else {
-							selection.remove(object);
-						}
-					}
-				});
-				buttonMap.put(object, button);
-			}
-			sc.setMinSize(child.computeSize(SWT.DEFAULT, SWT.DEFAULT));
-			sc.setExpandHorizontal(true);
-			sc.setExpandVertical(true);
-			sc.setContent(child);
-
-			updateSelectionUi();
-
-			return ret;
-		}
-
-		private void updateSelectionUi() {
-			if (selection != null && !selection.isEmpty() && !buttonMap.isEmpty()) {
-				for (Object object : selection) {
-					buttonMap.get(object).setSelection(true);
-				}
-			}
-		}
-
-		protected static String getLabel(Object object) {
-			if (object instanceof PersistentObject) {
-				return ((PersistentObject) object).getLabel();
-			} else if (object != null) {
-				return object.toString();
-			} else {
-				return "";
-			}
 		}
 	}
 }
