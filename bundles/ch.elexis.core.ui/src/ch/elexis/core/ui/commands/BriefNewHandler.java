e86d40ff4a1372977a602b0d05bfe8ae1c539b05
pdenzler <patrick.denzlers@gmail.com>
2022-04-27 13:58:22 +0200

    [23892] add dialog to view Briefauswahl

--- bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/commands/BriefNewHandler.java
+++ bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/commands/BriefNewHandler.java
@@ -1,28 +1,35 @@
 package ch.elexis.core.ui.commands;
 
+import java.util.List;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.SWT;
 import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.PlatformUI;
 
 import ch.elexis.core.data.activator.CoreHub;
 import ch.elexis.core.data.events.ElexisEventDispatcher;
 import ch.elexis.core.ui.UiDesk;
-import ch.elexis.core.ui.dialogs.DocumentSelectDialog;
 import ch.elexis.core.ui.dialogs.SelectFallDialog;
 import ch.elexis.core.ui.util.viewers.CommonViewer;
 import ch.elexis.core.ui.views.BriefAuswahl;
 import ch.elexis.core.ui.views.Messages;
 import ch.elexis.core.ui.views.TextView;
+import ch.elexis.core.ui.views.controls.GenericSearchSelectionDialog;
+import ch.elexis.data.Brief;
 import ch.elexis.data.Fall;
 import ch.elexis.data.Konsultation;
 import ch.elexis.data.Kontakt;
 import ch.elexis.data.Patient;
+import ch.elexis.data.Query;
 import ch.rgw.tools.ExHandler;
+import ch.rgw.tools.StringTool;
 
 public class BriefNewHandler extends AbstractHandler implements IHandler {
 	public static final String CMD_ID = "ch.elexis.core.ui.commands.BriefNew";
@@ -61,25 +68,53 @@ public class BriefNewHandler extends AbstractHandler implements IHandler {
 
 		TextView tv = null;
 		try {
-			DocumentSelectDialog bs = new DocumentSelectDialog(
-					PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), CoreHub.actMandant,
-					DocumentSelectDialog.TYPE_CREATE_DOC_WITH_TEMPLATE);
-			if (bs.open() == Dialog.OK) {
+			Query<Brief> qbe = new Query<Brief>(Brief.class);
+			qbe.add(Brief.FLD_TYPE, Query.EQUALS, Brief.TEMPLATE);
+			qbe.add(Brief.FLD_KONSULTATION_ID, Query.NOT_EQUAL, "SYS");
+			qbe.startGroup();
+			qbe.add(Brief.FLD_DESTINATION_ID, Query.EQUALS, ElexisEventDispatcher.getSelectedMandator().getId());
+			qbe.or();
+			qbe.add(Brief.FLD_DESTINATION_ID, Query.EQUALS, StringTool.leer);
+			qbe.endGroup();
+			List<Brief> lBrief = qbe.execute();
+
+			GenericSearchSelectionDialog dialog = new GenericSearchSelectionDialog(
+					PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), lBrief,
+					ch.elexis.core.l10n.Messages.DocumentSelectDialog_schooseTemplateForLetter,
+					ch.elexis.core.ui.dialogs.Messages.DocumentSelectDialog_createLetterWithTemplate,
+					ch.elexis.core.ui.dialogs.Messages.DocumentSelectDialog_pleaseSelectTemplateFromList, null,
+					SWT.SINGLE);
+
+			Brief result;
+			String subject = null;
+			Kontakt address = null;
+
+			if (dialog.open() == Dialog.OK) {
 				tv = (TextView) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
 						.showView(TextView.ID);
-				// trick: just supply a dummy address for creating the doc
-				Kontakt address = null;
-				if (DocumentSelectDialog.getDontAskForAddresseeForThisTemplate(bs.getSelectedDocument()))
-					address = Kontakt.load("-1"); //$NON-NLS-1$
-				tv.createDocument(bs.getSelectedDocument(), bs.getBetreff(), address);
-				tv.setName();
 
-				IViewPart viewPart = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
-						.findView(BriefAuswahl.ID);
-				if (viewPart instanceof BriefAuswahl) {
-					((BriefAuswahl) viewPart).refreshCV(CommonViewer.Message.update_keeplabels);
+				IStructuredSelection sel = (IStructuredSelection) dialog.getSelection();
+				if ((sel != null) && (!sel.isEmpty())) {
+					result = (Brief) sel.getFirstElement();
+					subject = result.getBetreff();
+
+					if (StringTool.isNothing(subject)) {
+						subject = result.getBetreff();
+					}
+				}
+
+				if (dialog.getSelection() != null) {
+					tv.createDocument((Brief) dialog.getSelection().getFirstElement(), subject, address);
+					tv.setName();
+
+					IViewPart viewPart = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
+							.findView(BriefAuswahl.ID);
+					if (viewPart instanceof BriefAuswahl) {
+						((BriefAuswahl) viewPart).refreshCV(CommonViewer.Message.update_keeplabels);
+					}
 				}
 			}
+
 		} catch (Exception ex) {
 			ExHandler.handle(ex);
 		}
