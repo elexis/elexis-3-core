1d32aad61a33326f73a3acbfe7ba2629a2ed0a5d
Marco Descher <marco@descher.at>
2022-09-09 10:00:05 +0200

    Revert "[24485] hl7 template taskDescriptor (#472)"
    This reverts commit eb13a3a1766f46a8e72c60d9e22661ee7fd913f6.

--- bundles/ch.elexis.core.tasks/src/ch/elexis/core/tasks/internal/service/IdentifiedRunnableFactoryImpl.java
+++ bundles/ch.elexis.core.tasks/src/ch/elexis/core/tasks/internal/service/IdentifiedRunnableFactoryImpl.java
@@ -3,22 +3,17 @@ package ch.elexis.core.tasks.internal.service;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.osgi.service.component.ComponentException;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
-import org.slf4j.LoggerFactory;
 
 import ch.elexis.core.model.tasks.IIdentifiedRunnable;
 import ch.elexis.core.model.tasks.IIdentifiedRunnableFactory;
-import ch.elexis.core.model.tasks.TaskException;
 import ch.elexis.core.services.IModelService;
 import ch.elexis.core.services.IVirtualFilesystemService;
 import ch.elexis.core.tasks.internal.runnables.DeleteFileIdentifiedRunnable;
 import ch.elexis.core.tasks.internal.runnables.LogResultContextIdentifiedRunnable;
 import ch.elexis.core.tasks.internal.runnables.RemoveTaskLogEntriesRunnable;
 import ch.elexis.core.tasks.internal.runnables.TriggerTaskForEveryFileInDirectoryRunnable;
-import ch.elexis.core.tasks.internal.runnables.TriggerTaskForEveryFileInDirectoryTemplateTaskDescriptor;
-import ch.elexis.core.tasks.model.ITaskService;
 
 @Component
 public class IdentifiedRunnableFactoryImpl implements IIdentifiedRunnableFactory {
@@ -41,16 +36,6 @@ public class IdentifiedRunnableFactoryImpl implements IIdentifiedRunnableFactory
 		ret.add(new TriggerTaskForEveryFileInDirectoryRunnable(virtualFilsystemService));
 		ret.add(new RemoveTaskLogEntriesRunnable(taskModelService));
 		return ret;
-	}
-
-	@Override
-	public void initialize(Object taskService) {
-		try {
-			TriggerTaskForEveryFileInDirectoryTemplateTaskDescriptor.assertTemplate((ITaskService) taskService);
-		} catch (TaskException e) {
-			LoggerFactory.getLogger(getClass()).error("initialize", e);
-			throw new ComponentException(e);
-		}
 	}
 
 }
