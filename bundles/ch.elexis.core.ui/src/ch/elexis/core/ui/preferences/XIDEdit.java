46800b766a240bc8009dcbcdd5863e840c784302
Jeremy Colameo <jeremy.colameo@gmail.com>
2022-10-17 08:37:03 +0200

    [24559] Added EditDialog

--- bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/preferences/XIDEdit.java
+++ bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/preferences/XIDEdit.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.ColumnLabelProvider;
@@ -16,12 +17,20 @@ import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerComparator;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
@@ -146,6 +155,30 @@ public class XIDEdit extends PreferencePage implements IWorkbenchPreferencePage 
 			}
 		});
 
+		table.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseDoubleClick(MouseEvent e) {
+				TableItem[] sel = table.getSelection();
+				if (sel != null && sel.length > 0) {
+					new XidEditDialog(getShell(), sel[0].getText(1)).open();
+					for (TableItem it : table.getItems()) {
+						XIDDomain xd = Xid.getDomain(it.getText(1));
+						StringBuilder sb = new StringBuilder();
+						if (xd.isDisplayedFor(Person.class)) {
+							sb.append("P"); //$NON-NLS-1$
+						}
+						if (xd.isDisplayedFor(Organisation.class)) {
+							sb.append("O"); //$NON-NLS-1$
+						}
+						it.setText(2, sb.toString());
+					}
+					table.redraw();
+				}
+			}
+
+		});
+
 		tv.setComparator(comparator);
 		tv.refresh();
 		return table;
@@ -165,6 +198,58 @@ public class XIDEdit extends PreferencePage implements IWorkbenchPreferencePage 
 			}
 		};
 		return selectionAdapter;
+	}
+
+	static class XidEditDialog extends Dialog {
+		Text tShort;
+		Button bPerson, bOrg;
+		XIDDomain mine;
+
+		public XidEditDialog(Shell shell, String myDomain) {
+			super(shell);
+			mine = Xid.getDomain(myDomain);
+		}
+
+		@Override
+		public void create() {
+			super.create();
+			getShell().setText(Messages.XIDEdit_XidOpetions);
+		}
+
+		@Override
+		protected Control createDialogArea(Composite parent) {
+			Composite ret = (Composite) super.createDialogArea(parent);
+			ret.setLayout(new RowLayout(SWT.VERTICAL));
+			new Label(ret, SWT.NONE).setText(mine.getDomainName());
+			tShort = new Text(ret, SWT.BORDER);
+			tShort.setText(mine.getSimpleName());
+			new Label(ret, SWT.SEPARATOR | SWT.HORIZONTAL);
+			new Label(ret, SWT.NONE).setText(Messages.XIDEdit_ShowWith);
+			bPerson = new Button(ret, SWT.CHECK);
+			bOrg = new Button(ret, SWT.CHECK);
+			bPerson.setText(Messages.XIDEdit_Persons);
+			bOrg.setText(Messages.XIDEdit_Organizations);
+			if (mine.isDisplayedFor(Person.class)) {
+				bPerson.setSelection(true);
+			}
+			if (mine.isDisplayedFor(Organisation.class)) {
+				bOrg.setSelection(true);
+			}
+			return ret;
+		}
+
+		@Override
+		protected void okPressed() {
+			if (bPerson.getSelection()) {
+				mine.addDisplayOption(Person.class);
+			}
+			if (bOrg.getSelection()) {
+				mine.addDisplayOption(Organisation.class);
+			}
+			mine.setSimpleName(tShort.getText());
+			super.okPressed();
+		}
+
 	}
 
 	public class XIDComparator extends ViewerComparator {
