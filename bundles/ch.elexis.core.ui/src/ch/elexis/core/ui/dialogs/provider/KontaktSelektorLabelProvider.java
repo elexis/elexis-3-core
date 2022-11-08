4c7b2809a82814b5bb4618ecd19e4e753c745997
Thomas Huster <huster@medevit.at>
2022-11-08 13:56:21 +0100

    [24736] add sticker image, background and foreground to kontakt selector

--- bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/dialogs/provider/KontaktSelektorLabelProvider.java
+++ bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/dialogs/provider/KontaktSelektorLabelProvider.java
@@ -1,12 +1,22 @@
 package ch.elexis.core.ui.dialogs.provider;
 
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.jface.viewers.ITableColorProvider;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Image;
+
 import ch.elexis.core.constants.StringConstants;
+import ch.elexis.core.model.IContact;
+import ch.elexis.core.model.ISticker;
+import ch.elexis.core.model.OrganizationConstants;
+import ch.elexis.core.services.holder.StickerServiceHolder;
+import ch.elexis.core.ui.util.CoreUiUtil;
 import ch.elexis.core.ui.util.viewers.DefaultLabelProvider;
 import ch.elexis.data.Kontakt;
 import ch.elexis.data.PersistentObject;
 import ch.elexis.data.Person;
 
-public class KontaktSelektorLabelProvider extends DefaultLabelProvider {
+public class KontaktSelektorLabelProvider extends DefaultLabelProvider implements ITableColorProvider {
 	@Override
 	public String getText(Object element) {
 		if (element instanceof Kontakt) {
@@ -18,6 +28,12 @@ public class KontaktSelektorLabelProvider extends DefaultLabelProvider {
 			}
 			if (StringConstants.ONE.equals(k.get(Kontakt.FLD_IS_USER))) {
 				label = k.get(Kontakt.FLD_NAME1) + " " + k.get(Kontakt.FLD_NAME2) + " - " + label;
+			}
+			if (k.istOrganisation() && StringUtils.isNotBlank(
+					(String) k.getExtInfoStoredObjectByKey(OrganizationConstants.FLD_EXT_ALLOWED_BILLINGLAW))) {
+				label = label + " (" //$NON-NLS-1$
+						+ (String) k.getExtInfoStoredObjectByKey(OrganizationConstants.FLD_EXT_ALLOWED_BILLINGLAW)
+						+ ")"; //$NON-NLS-1$
 			}
 			return label;
 		} else if (element instanceof PersistentObject) {
@@ -31,4 +47,39 @@ public class KontaktSelektorLabelProvider extends DefaultLabelProvider {
 		return getText(element);
 	}
 
+	@Override
+	public Image getColumnImage(final Object element, final int columnIndex) {
+		if (element instanceof Kontakt) {
+			IContact pat = ((Kontakt) element).toIContact();
+			ISticker sticker = StickerServiceHolder.get().getSticker(pat).orElse(null);
+			if (sticker != null && sticker.getImage() != null) {
+				return CoreUiUtil.getImageAsIcon(sticker.getImage());
+			}
+		}
+		return super.getColumnImage(element, columnIndex);
+	}
+
+	@Override
+	public Color getForeground(Object element, int columnIndex) {
+		if (element instanceof Kontakt) {
+			IContact pat = ((Kontakt) element).toIContact();
+			ISticker sticker = StickerServiceHolder.get().getSticker(pat).orElse(null);
+			if (sticker != null && StringUtils.isNotBlank(sticker.getForeground())) {
+				return CoreUiUtil.getColorForString(sticker.getForeground());
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public Color getBackground(final Object element, final int columnIndex) {
+		if (element instanceof Kontakt) {
+			IContact pat = ((Kontakt) element).toIContact();
+			ISticker sticker = StickerServiceHolder.get().getSticker(pat).orElse(null);
+			if (sticker != null && StringUtils.isNotBlank(sticker.getBackground())) {
+				return CoreUiUtil.getColorForString(sticker.getBackground());
+			}
+		}
+		return null;
+	}
 }
