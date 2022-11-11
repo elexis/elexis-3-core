f461d0767746ad5522dae4f01f15e72318f5e2de
Marco Descher <marco@descher.at>
2022-11-11 09:01:07 +0100

    [23028] ESCreateEventForwarderEventHandler#log err in post event

--- bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/events/ElexisServerCreateEventForwarderEventHandler.java
+++ bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/events/ElexisServerCreateEventForwarderEventHandler.java
@@ -1,14 +1,17 @@
 package ch.elexis.core.ui.events;
 
+import org.eclipse.core.runtime.IStatus;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventConstants;
 import org.osgi.service.event.EventHandler;
+import org.slf4j.LoggerFactory;
 
 import ch.elexis.core.common.ElexisEvent;
 import ch.elexis.core.common.ElexisEventTopics;
 import ch.elexis.core.services.IElexisServerService;
+import ch.elexis.core.status.StatusUtil;
 
 /**
  * Forward create events to the server (currently used by rockethealth to pick
@@ -25,7 +28,10 @@ public class ElexisServerCreateEventForwarderEventHandler implements EventHandle
 		if (elexisServerService.deliversRemoteEvents()) {
 			ch.elexis.core.common.ElexisEvent mapEvent = mapEvent(event);
 			if (mapEvent != null) {
-				elexisServerService.postEvent(mapEvent);
+				IStatus status = elexisServerService.postEvent(mapEvent);
+				if (!status.isOK()) {
+					StatusUtil.printStatus(LoggerFactory.getLogger(getClass()), status);
+				}
 			}
 		}
 	}
