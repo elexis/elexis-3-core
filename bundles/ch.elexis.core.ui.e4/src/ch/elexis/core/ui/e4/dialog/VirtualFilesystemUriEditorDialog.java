e5095963357667d0dbf174d1f13e7d84c41fb220
Marco Descher <marco@descher.at>
2022-11-16 16:37:10 +0100

    [21613] multiple fixes for VirtualFilesystemUriEditorDialog

--- bundles/ch.elexis.core.ui.e4/src/ch/elexis/core/ui/e4/dialog/VirtualFilesystemUriEditorDialog.java
+++ bundles/ch.elexis.core.ui.e4/src/ch/elexis/core/ui/e4/dialog/VirtualFilesystemUriEditorDialog.java
@@ -7,6 +7,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.http.client.utils.URIBuilder;
 import org.eclipse.core.databinding.DataBindingContext;
 import org.eclipse.core.databinding.UpdateValueStrategy;
 import org.eclipse.core.databinding.beans.typed.BeanProperties;
@@ -38,6 +39,7 @@ public class VirtualFilesystemUriEditorDialog extends TitleAreaDialog {
 	private MyURI uri;
 
 	private Text txtHost;
+	private Text txtPort;
 	private Text txtUser;
 	private Text txtPath;
 	private Text txtPassword;
@@ -88,6 +90,13 @@ public class VirtualFilesystemUriEditorDialog extends TitleAreaDialog {
 
 		txtHost = new Text(container, SWT.BORDER);
 		txtHost.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+
+		Label lblPort = new Label(container, SWT.NONE);
+		lblPort.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblPort.setText("Port");
+
+		txtPort = new Text(container, SWT.BORDER);
+		txtPort.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 
 		Label lblPath = new Label(container, SWT.NONE);
 		lblPath.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
@@ -176,6 +185,11 @@ public class VirtualFilesystemUriEditorDialog extends TitleAreaDialog {
 		bindingContext.bindValue(widget, model, null, null);
 
 		//
+		IObservableValue<String> widgetPort = WidgetProperties.text(SWT.Modify).observe(txtPort);
+		IObservableValue<Integer> modelPort = BeanProperties.value(MyURI.class, "port", Integer.class).observe(uri);
+		bindingContext.bindValue(widgetPort, modelPort, null, null);
+
+		//
 		IObservableValue<String> widgetPath = WidgetProperties.text(SWT.Modify).observe(txtPath);
 		IObservableValue<String> modelPath = BeanProperties.value(MyURI.class, "path", String.class).observe(uri);
 		bindingContext.bindValue(widgetPath, modelPath, null, null);
@@ -203,6 +217,7 @@ public class VirtualFilesystemUriEditorDialog extends TitleAreaDialog {
 		return bindingContext;
 	}
 
+	@SuppressWarnings("unused")
 	private class MyURI {
 
 		private String scheme;
@@ -210,13 +225,13 @@ public class VirtualFilesystemUriEditorDialog extends TitleAreaDialog {
 		private String path;
 		private String user;
 		private String pass;
-		private int port;
+		private Integer port;
 
-		public int getPort() {
+		public Integer getPort() {
 			return port;
 		}
 
-		public void setPort(int port) {
+		public void setPort(Integer port) {
 			firePropertyChange("port", this.port, this.port = port);
 			firePropertyChange("uri", null, getUri());
 		}
@@ -268,7 +283,24 @@ public class VirtualFilesystemUriEditorDialog extends TitleAreaDialog {
 
 		public URI getUri() {
 			try {
-				return new URI(scheme, user + ":" + pass, host, port, path, null, null);
+				URIBuilder uriBuilder = new URIBuilder();
+				if (StringUtils.isNotBlank(scheme)) {
+					uriBuilder.setScheme(scheme);
+				}
+				if (StringUtils.isNotBlank(host)) {
+					uriBuilder.setHost(host);
+				}
+				if (StringUtils.isNotBlank(path)) {
+					uriBuilder.setPath(path);
+				}
+				if (StringUtils.isNotBlank(user)) {
+					uriBuilder.setUserInfo(user, pass);
+				}
+				if (port != null && port > 0) {
+					uriBuilder.setPort(port);
+				}
+				return uriBuilder.build();
+
 			} catch (URISyntaxException e) {
 
 			}
