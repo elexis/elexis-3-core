de069d5bc6a63ae2ac7b29e13c3c746612d10950
Marco Descher <marco@descher.at>
2022-11-29 13:58:48 +0100

    [24831] IModelService#touch, StickerService send update event

--- tests/ch.elexis.core.model.test/src/ch/elexis/core/model/AllModelTests.java
+++ tests/ch.elexis.core.model.test/src/ch/elexis/core/model/AllModelTests.java
@@ -5,11 +5,11 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AccountTransactionTest.class, AppointmentTest.class, ConfigTest.class, ContactTest.class,
-		CoverageTest.class, DbImageTest.class, DocumentLetterTest.class, EncounterTest.class, LabItemTest.class,
-		LabMappingTest.class, LaboratoryTest.class, LabOrderTest.class, LabResultTest.class, OrderEntryTest.class,
-		PatientTest.class, RoleTest.class, StockEntryTest.class, UserTest.class, ArticleTest.class,
-		UserConfigTest.class, XidTest.class, BillingSystemFactorTest.class, LocalServiceTest.class,
+@SuiteClasses({ IdentifiableTest.class, AccountTransactionTest.class, AppointmentTest.class, ConfigTest.class,
+		ContactTest.class, CoverageTest.class, DbImageTest.class, DocumentLetterTest.class, EncounterTest.class,
+		LabItemTest.class, LabMappingTest.class, LaboratoryTest.class, LabOrderTest.class, LabResultTest.class,
+		OrderEntryTest.class, PatientTest.class, RoleTest.class, StockEntryTest.class, UserTest.class,
+		ArticleTest.class, UserConfigTest.class, XidTest.class, BillingSystemFactorTest.class, LocalServiceTest.class,
 		DiagnosisReferenceTest.class, BilledTest.class, PrescriptionTest.class, FreeTextDiagnosisTest.class,
 		RecipeTest.class, DefaultSignatureTest.class, PersonTest.class, MessageTest.class, MandatorTest.class,
 		TextTemplateTest.class, SickCertificateTest.class, InvoiceTest.class, DayMessageTest.class, BlobTest.class,
