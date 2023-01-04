f29274f9f734abb8fb622fcd15493b839d59d383
Thomas Huster <huster@medevit.at>
2023-01-04 18:24:06 +0100

    [24915] add debug logging for event fallback

--- bundles/ch.elexis.core.data/src/ch/elexis/core/data/events/ElexisEventDispatcher.java
+++ bundles/ch.elexis.core.data/src/ch/elexis/core/data/events/ElexisEventDispatcher.java
@@ -66,7 +66,7 @@ import ch.elexis.data.PersistentObject;
  * @since 3.8 must explicitly {@link #start()} queue execution
  */
 public final class ElexisEventDispatcher implements Runnable {
-	private Logger log = LoggerFactory.getLogger(ElexisEventDispatcher.class);
+	private static Logger log = LoggerFactory.getLogger(ElexisEventDispatcher.class);
 
 	private final ListenerList<ElexisEventListener> listeners;
 	private static ElexisEventDispatcher theInstance;
@@ -270,6 +270,7 @@ public final class ElexisEventDispatcher implements Runnable {
 		if (po != null) {
 			getInstance().fire(new ElexisEvent(po, po.getClass(), ElexisEvent.EVENT_SELECTED));
 		} else {
+			log.info("Could not get PersistentObject for [" + identifiable + "]");
 			ContextServiceHolder.get().getRootContext().setTyped(identifiable);
 		}
 	}
@@ -481,12 +482,13 @@ public final class ElexisEventDispatcher implements Runnable {
 		contextService.getRootContext().setNamed(ch.elexis.core.services.holder.ContextServiceHolder.SELECTIONFALLBACK,
 				new Consumer<Identifiable>() { // $NON-NLS-1$
 
-			@Override
-			public void accept(Identifiable identifiable) {
-				if (identifiable != null) {
-					fireSelectionEvent(identifiable);
-				}
-			}
-		});
+					@Override
+					public void accept(Identifiable identifiable) {
+						log.info("Fallback Event consumer for [" + identifiable + "]");
+						if (identifiable != null) {
+							fireSelectionEvent(identifiable);
+						}
+					}
+				});
 	}
 }
