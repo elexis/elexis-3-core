9e67d602b81c735a9a09728a312513138bcc6217
Marco Descher <marco@descher.at>
2023-02-27 11:58:42 +0100

    [24876] IContactService#findPersonDuplicates (fuzzy name)

---
+++ tests/ch.elexis.core.services.test/src/ch/elexis/core/services/IContactServiceTest.java
@@ -0,0 +1,31 @@
+package ch.elexis.core.services;
+
+import static org.junit.Assert.assertEquals;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.junit.Test;
+
+import ch.elexis.core.model.IPerson;
+import ch.elexis.core.model.builder.IContactBuilder;
+import ch.elexis.core.types.Gender;
+import ch.elexis.core.utils.OsgiServiceUtil;
+
+public class IContactServiceTest extends AbstractServiceTest {
+
+	private IContactService contactService = OsgiServiceUtil.getService(IContactService.class).get();
+
+	@Test
+	public void findPersonDuplicate() {
+		IPerson testPerson = new IContactBuilder.PersonBuilder(coreModelService, "Arnold", "Schwoarzenegger",
+				LocalDate.of(1947, 7, 30), Gender.MALE).buildAndSave();
+
+		List<IPerson> findPersonDuplicates = contactService.findPersonDuplicates(LocalDate.of(1947, 7, 30), Gender.MALE,
+				"Schwarzenegger", null, false);
+		assertEquals(1, findPersonDuplicates.size());
+
+		coreModelService.remove(testPerson);
+	}
+
+}
