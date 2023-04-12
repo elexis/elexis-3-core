7413bafa70dff20805491a5ae24b494b539f94cd
thuster <huster@medevit.at>
2023-04-12 14:43:26 +0200

    [25206] add EsrRecord entity to core jpa

---
+++ bundles/ch.elexis.core/src/ch/elexis/core/model/esr/ESRCode.java
@@ -0,0 +1,41 @@
+package ch.elexis.core.model.esr;
+
+import java.util.HashMap;
+
+import ch.elexis.core.interfaces.INumericEnum;
+
+public enum ESRCode implements INumericEnum {
+	//@formatter:off
+	Gutschrift_edv(0), 
+	Storno_edv(1), 
+	Korrektur_edv(2), 
+	Gutschrift_Schalter(3), 
+	Storno_Schalter(4), 
+	Korrektur_Schalter(5),
+	Summenrecord(6), 
+	Unbekannt(7);
+	//@formatter:on
+	
+	private int numeric;
+	
+	private static HashMap<Integer, ESRCode> numericMap = new HashMap<>();
+
+	ESRCode(int numeric) {
+		this.numeric = numeric;
+	}
+
+	@Override
+	public int numericValue() {
+		return numeric;
+	}
+
+	public static ESRCode byNumeric(int numeric) {
+		if (numericMap.isEmpty()) {
+			ESRCode[] entries = values();
+			for (int i = 0; i < entries.length; i++) {
+				numericMap.put(entries[i].numericValue(), entries[i]);
+			}
+		}
+		return numericMap.getOrDefault(numeric, Unbekannt);
+	}
+}
