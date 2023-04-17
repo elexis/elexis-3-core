d9b2c43a34a7757631fe97c645e1fd22606fc93f
pdenzler <patrick.denzlers@gmail.com>
2023-04-17 08:40:38 +0200

    [25067] Export multiple files and folders

--- bundles/ch.elexis.core.ui.documents/src/ch/elexis/core/ui/documents/handler/DocumentExportHandler.java
+++ bundles/ch.elexis.core.ui.documents/src/ch/elexis/core/ui/documents/handler/DocumentExportHandler.java
@@ -1,14 +1,21 @@
 package ch.elexis.core.ui.documents.handler;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import org.apache.commons.io.IOUtils;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
@@ -17,8 +24,11 @@ import org.eclipse.ui.handlers.HandlerUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import ch.elexis.core.data.service.ContextServiceHolder;
+import ch.elexis.core.documents.FilterCategory;
 import ch.elexis.core.exceptions.ElexisException;
 import ch.elexis.core.model.IDocument;
+import ch.elexis.core.model.IPatient;
 import ch.elexis.core.ui.documents.Messages;
 import ch.elexis.core.ui.documents.service.DocumentStoreServiceHolder;
 import ch.elexis.core.ui.util.SWTHelper;
@@ -29,13 +39,35 @@ public class DocumentExportHandler extends AbstractHandler implements IHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		Shell shell = PlatformUI.getWorkbench().getDisplay().getActiveShell();
-		ISelection selection = HandlerUtil.getCurrentSelection(event);
-		if (selection instanceof StructuredSelection && !((StructuredSelection) selection).isEmpty()) {
-			List<?> iDocuments = ((StructuredSelection) selection).toList();
 
-			for (Object documentToExport : iDocuments) {
-				if (documentToExport instanceof IDocument) {
-					openExportDialog(shell, (IDocument) documentToExport);
+		IStructuredSelection selection = HandlerUtil.getCurrentStructuredSelection(event);
+		List<?> selections = selection.toList();
+		Set<Object> types = new HashSet<>();
+
+		for (Object first : selections) {
+			if (!types.contains(first.getClass())) {
+				if (first instanceof IDocument) {
+					if (types.stream().noneMatch(t -> t instanceof IDocument)) {
+						types.add(first);
+					}
+				} else {
+					types.add(first.getClass());
+				}
+			}
+		}
+
+		if (types.size() > 1) {
+			MessageDialog.openInformation(shell, "Export Information",
+					"Es können nicht Ordner und Dokumente gleichzeitig exportiert werden.");
+		} else {
+			Object selectionType = selection.getFirstElement();
+			if (selectionType != null) {
+				if (selectionType instanceof IDocument) {
+					for (Object documentToExport : selections) {
+						openExportDialog(shell, (IDocument) documentToExport);
+					}
+				} else if (selectionType instanceof FilterCategory) {
+					openExportFilterCategoryDialog(shell, (FilterCategory) selectionType, selection);
 				}
 			}
 		}
@@ -44,17 +76,63 @@ public class DocumentExportHandler extends AbstractHandler implements IHandler {
 
 	private void openExportDialog(Shell shell, IDocument document) {
 		FileDialog fd = new FileDialog(shell, SWT.SAVE);
+		fd.setFileName(document.getTitle());
 		fd.setFilterExtensions(createExtensionFilter(document));
 		String fname = fd.open();
 		if (fname != null) {
 			try {
-				if (DocumentStoreServiceHolder.getService().saveContentToFile(document, fname) == null) {
+				if (DocumentStoreServiceHolder.getService().saveContentToFile(document, fname + "." + document.getExtension()) == null) {
 					SWTHelper.showError(Messages.DocumentView_exportErrorCaption,
 							Messages.DocumentView_exportErrorEmptyText);
 				}
 			} catch (ElexisException e) {
 				logger.error("cannot export file", e);
 				SWTHelper.showError(Messages.DocumentView_exportErrorCaption, Messages.DocumentView_exportErrorText);
+			}
+		}
+	}
+
+	private void openExportFilterCategoryDialog(Shell shell, FilterCategory filterCategory,
+			IStructuredSelection selection) {
+		FileDialog fileDialog = new FileDialog(shell, SWT.SAVE | SWT.MULTI);
+		fileDialog.setFileName(filterCategory.getName());
+		String filterCategoryName = fileDialog.open();
+
+		if (filterCategoryName != null) {
+			for (int i = 0; i < selection.size(); i++) {
+				FilterCategory fc = (FilterCategory) selection.toList().get(i);
+				File folder = new File(fileDialog.getFilterPath() + File.separator + fc.getName());
+				folder.mkdir();
+
+				IPatient activePatient = ContextServiceHolder.get().getActivePatient().orElse(null);
+				List<IDocument> documentList = DocumentStoreServiceHolder.getService()
+						.getDocumentsByCategory(activePatient.getId(), fc);
+
+				Set<String> titles = new HashSet<String>();
+				for (int a = 0; a < documentList.size(); a++) {
+					IDocument document = (IDocument) documentList.get(a);
+
+					String _titel = document.getTitle();
+					if (titles.contains(document.getTitle())) {
+						_titel = document.getTitle() + "(" + a + ")";
+					} else {
+						titles.add(document.getTitle());
+					}
+
+					if (document.getContent() != null) {
+						File newFile = new File(
+								folder.getAbsoluteFile() + File.separator + _titel + "." + document.getExtension());
+
+						try (InputStream inputStream = documentList.get(a).getContent();
+								FileOutputStream outputStream = new FileOutputStream(newFile)) {
+							if (inputStream != null) {
+								IOUtils.copy(inputStream, outputStream);
+							}
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+				}
 			}
 		}
 	}
