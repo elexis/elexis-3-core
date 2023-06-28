af2b0797d580aa93e1ce8696535e5adc06293bdd
pdenzler <patrick.denzlers@gmail.com>
2023-06-28 16:21:06 +0200

    [25354] MailTransporter - agenda reminder (#524)

--- bundles/ch.elexis.core.mail/src/ch/elexis/core/mail/PreferenceConstants.java
+++ bundles/ch.elexis.core.mail/src/ch/elexis/core/mail/PreferenceConstants.java
@@ -3,4 +3,5 @@ package ch.elexis.core.mail;
 public interface PreferenceConstants {
 	public static final String PREF_COMMON_BASE = "mail/";
 	public static final String PREF_DEFAULT_TEMPLATE = PREF_COMMON_BASE + "template/default";
+	public static final String PREF_DEFAULT_MAIL_ACCOUNT = "account";
 }
