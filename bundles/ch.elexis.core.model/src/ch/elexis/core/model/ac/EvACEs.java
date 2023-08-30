4542e44f6c445884b02a2382cef9a80096d6a5fc
thuster <huster@medevit.at>
2023-08-30 15:10:05 +0200

    [25312] add missing EvACEs definitions

--- bundles/ch.elexis.core.model/src/ch/elexis/core/model/ac/EvACEs.java
+++ bundles/ch.elexis.core.model/src/ch/elexis/core/model/ac/EvACEs.java
@@ -5,6 +5,7 @@ import ch.elexis.core.ac.EvaluatableACE;
 import ch.elexis.core.ac.Right;
 import ch.elexis.core.model.IBilled;
 import ch.elexis.core.model.ICoverage;
+import ch.elexis.core.model.IEncounter;
 import ch.elexis.core.model.IInvoice;
 import ch.elexis.core.model.ILabResult;
 
@@ -12,11 +13,8 @@ public class EvACEs {
 	
 	public static final EvaluatableACE ACCOUNTING_GLOBAL = EvACE.of(IInvoice.class, Right.READ).and(Right.VIEW);
 	public static final EvaluatableACE CASE_DEFINE_SPECIALS = EvACE.of(ICoverage.class, Right.UPDATE).and(Right.EXECUTE);
-	public static final EvaluatableACE ACCOUNTING_STATS = null;
-	public static final EvaluatableACE KONS_REASSIGN = null;
+	public static final EvaluatableACE ACCOUNTING_STATS = EvACE.of(IEncounter.class, Right.READ).and(Right.VIEW);
+	public static final EvaluatableACE KONS_REASSIGN = EvACE.of(IEncounter.class, Right.UPDATE).and(Right.READ);
 	public static final EvaluatableACE LSTG_VERRECHNEN = EvACE.of(IBilled.class, Right.CREATE).and(Right.UPDATE);
 	public static final EvaluatableACE LAB_SEEN = EvACE.of(ILabResult.class, Right.EXECUTE).and(Right.READ).and(Right.VIEW);
-	public static final EvaluatableACE LSTG_CHARGE_FOR_ALL = null;
-	public static final EvaluatableACE DISPLAY_ESR = null;
-
 }
