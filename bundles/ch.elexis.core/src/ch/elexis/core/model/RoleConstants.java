d0a5a23d0c1a1f9f02ad8419760c226f2b13a53b
thuster <huster@medevit.at>
2023-09-29 14:18:35 +0200

    [25312] update role acl definition, add medical-user and poweruser roles

--- bundles/ch.elexis.core/src/ch/elexis/core/model/RoleConstants.java
+++ bundles/ch.elexis.core/src/ch/elexis/core/model/RoleConstants.java
@@ -7,6 +7,8 @@ public class RoleConstants {
 	public static final String ACCESSCONTROLE_ROLE_MANDATOR = "mandator";
 	public static final String ACCESSCONTROLE_ROLE_MPK = "mpk";
 	public static final String ACCESSCONTROLE_ROLE_MPA = "mpa";
+	public static final String ACCESSCONTROLE_ROLE_MEDICAL_USER = "medical-user";
+	public static final String ACCESSCONTROLE_ROLE_POWERUSER = "poweruser";
 
 	public static final String ACCESSCONTROLE_ROLE_ICT_ADMINISTRATOR = "ict-administrator";
 	public static final String ACCESSCONTROLE_ROLE_BOT = "bot";
