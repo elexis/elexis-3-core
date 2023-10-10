34ff088e837db4adfcf6a8226fd95c79749b0f5e
pdenzler <patrick.denzlers@gmail.com>
2023-10-10 16:36:28 +0200

    [25723] add family anamnesis to Patientenblatt2

--- bundles/ch.elexis.core.l10n/src/ch/elexis/core/l10n/Messages.java
+++ bundles/ch.elexis.core.l10n/src/ch/elexis/core/l10n/Messages.java
@@ -3425,6 +3425,7 @@ public class Messages extends NLS {
 	public static String Patientenblatt2_kindOfRelation;
 	public static String Patientenblatt2_legalGuardian;
 	public static String Patientenblatt2_persAnamnesisLbl;
+	public static String Patientenblatt2_famAnamnesisLbl;
 	public static String Patientenblatt2_phone1;
 	public static String Patientenblatt2_phone2;
 	public static String Patientenblatt2_pleaseEnterKindOfRelationship;
