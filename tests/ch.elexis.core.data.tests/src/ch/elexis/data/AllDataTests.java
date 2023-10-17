02c0e2dcd3b8e097cf11f53b04f82d151f39a240
Marco Descher <marco@descher.at>
2023-10-17 21:52:42 +0200

    AllDataTests wait for POActivator

--- tests/ch.elexis.core.data.tests/src/ch/elexis/data/AllDataTests.java
+++ tests/ch.elexis.core.data.tests/src/ch/elexis/data/AllDataTests.java
@@ -1,17 +1,25 @@
 package ch.elexis.data;
 
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import ch.elexis.core.utils.OsgiServiceUtil;
+
 @RunWith(Suite.class)
 @SuiteClasses({ Test_DBInitialState.class, Test_PersistentObject.class, Test_Prescription.class, Test_Patient.class,
 		Test_LabItem.class, Test_DBImage.class, Test_Query.class, Test_Verrechnet.class, Test_Reminder.class,
-		Test_StockService.class, Test_OrderService.class, Test_Konsultation.class,
-		Test_VkPreise.class, Test_ZusatzAdresse.class, Test_Rechnung.class, Test_Trace.class, Test_User.class,
-		Test_LabResult.class, Test_BezugsKontakt.class })
+		Test_StockService.class, Test_OrderService.class, Test_Konsultation.class, Test_VkPreise.class,
+		Test_ZusatzAdresse.class, Test_Rechnung.class, Test_Trace.class, Test_User.class, Test_LabResult.class,
+		Test_BezugsKontakt.class })
 public class AllDataTests {
 
 	public static final boolean PERFORM_UPDATE_TESTS = false;
+
+	@BeforeClass
+	public static void beforeClass() {
+		OsgiServiceUtil.getServiceWait(PersistentObjectDataSourceActivator.class, 5000).orElseThrow();
+	}
 
 }
