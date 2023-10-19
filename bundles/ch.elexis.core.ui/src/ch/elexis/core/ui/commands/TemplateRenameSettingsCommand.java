2302920e8a56e3ea9613e83fbc24e3a925d5d655
Victoria <Victoria@LAPTOP-LMK2LJMB>
2023-10-19 09:15:58 +0200

    [24131] created Vorlagen umbenennen function

---
+++ bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/commands/TemplateRenameSettingsCommand.java
@@ -0,0 +1,48 @@
+package ch.elexis.core.ui.commands;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import ch.elexis.core.ui.views.textsystem.TextTemplateView;
+import ch.elexis.core.ui.views.textsystem.model.TextTemplate;
+import ch.elexis.data.Brief;
+
+public class TemplateRenameSettingsCommand extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		ISelection selection = HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().getSelection();
+		if (selection != null) {
+			IStructuredSelection strucSelection = (IStructuredSelection) selection;
+			Object firstElement = strucSelection.getFirstElement();
+				if (firstElement != null && firstElement instanceof TextTemplate) {
+					TextTemplate textTemplate = (TextTemplate) firstElement;
+					InputDialog inputDialog = new InputDialog(Display.getCurrent().getActiveShell(),
+							"Vorlage umbenennen", "Bitte geben Sie den neuen Namen für die Vorlage ein:",
+							textTemplate.getName(), null);
+
+					if (inputDialog.open() == InputDialog.OK) {
+						String newName = inputDialog.getValue();
+						Brief template = textTemplate.getTemplate();
+						if (template != null) {
+							template.set(Brief.FLD_SUBJECT, newName);
+							IWorkbenchPage page = HandlerUtil.getActiveWorkbenchWindow(event).getActivePage();
+							TextTemplateView view = (TextTemplateView) page.findView(TextTemplateView.ID);
+							if (view != null) {
+								view.refresh();
+							}
+						}
+					}
+				}
+			}
+
+		return null;
+	}
+}
