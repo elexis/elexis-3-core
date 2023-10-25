163aa70354d1cefc4cf0cead537a03db61828e22
Daksic28 <93644806+Daksic28@users.noreply.github.com>
2023-10-25 16:59:36 +0200

    [25763] Send mail handler customized to send the mail after edit in (#582)

--- bundles/ch.elexis.core.mail.ui/src/ch/elexis/core/mail/ui/handlers/SendMailHandler.java
+++ bundles/ch.elexis.core.mail.ui/src/ch/elexis/core/mail/ui/handlers/SendMailHandler.java
@@ -1,5 +1,7 @@
 package ch.elexis.core.mail.ui.handlers;
 
+import java.io.Serializable;
+import java.util.Map;
 import java.util.Optional;
 
 import org.eclipse.core.commands.AbstractHandler;
@@ -12,6 +14,7 @@ import org.eclipse.ui.handlers.HandlerUtil;
 import ch.elexis.core.mail.MailMessage;
 import ch.elexis.core.mail.TaskUtil;
 import ch.elexis.core.mail.ui.dialogs.SendMailDialog;
+import ch.elexis.core.services.holder.ContextServiceHolder;
 import ch.elexis.core.tasks.model.ITask;
 import ch.elexis.core.tasks.model.ITaskDescriptor;
 import ch.elexis.core.tasks.model.TaskState;
@@ -32,7 +35,9 @@ import ch.elexis.core.tasks.model.TaskState;
  *
  */
 public class SendMailHandler extends AbstractHandler implements IHandler {
-
+	public static Optional<ITaskDescriptor> taskDescriptor;
+	public static final String MESSAGE_KEY = "message";
+	public static final String TEXT_KEY = "text";
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		SendMailDialog sendMailDialog = new SendMailDialog(HandlerUtil.getActiveShell(event));
@@ -48,23 +53,48 @@ public class SendMailHandler extends AbstractHandler implements IHandler {
 		if (to != null) {
 			sendMailDialog.setTo(to);
 		}
+		String accountId = event.getParameter("ch.elexis.core.mail.ui.sendMail.accountid");
+		if (to != null) {
+			sendMailDialog.setAccountId(accountId);
+		}
 		String subject = event.getParameter("ch.elexis.core.mail.ui.sendMail.subject");
 		if (subject != null) {
 			sendMailDialog.setSubject(subject);
 		}
 		String text = event.getParameter("ch.elexis.core.mail.ui.sendMail.text");
+		Optional<?> descriptor = ContextServiceHolder.get().getNamed("sendMailDialog.taskDescriptor");
+		if (descriptor.isPresent() && descriptor.get() instanceof ITaskDescriptor) {
+			ITaskDescriptor taskDescriptor = (ITaskDescriptor) descriptor.get();
+			Map<String, Serializable> runContext = taskDescriptor.getRunContext();
+			if (runContext != null && runContext.containsKey(MESSAGE_KEY)) {
+				Object messageObject = runContext.get(MESSAGE_KEY);
+				if (messageObject instanceof Map) {
+					Map<?, ?> messageMap = (Map<?, ?>) messageObject;
+					Object messageText = messageMap.get(TEXT_KEY);
+					if (messageText != null) {
+						text = messageText.toString();
+					}
+				}
+			}
+		}
 		if (text != null) {
 			sendMailDialog.setText(text);
 		}
 
+		String doSendString = event.getParameter("ch.elexis.core.mail.ui.sendMail.doSend");
+		if (doSendString != null) {
+			Boolean doSend = Boolean.valueOf(doSendString);
+			sendMailDialog.doSend(doSend);
+		}
 		if (sendMailDialog.open() == Dialog.OK) {
 			MailMessage message = new MailMessage().to(sendMailDialog.getTo()).cc(sendMailDialog.getCc())
 					.subject(sendMailDialog.getSubject()).text(sendMailDialog.getText());
 			message.setAttachments(sendMailDialog.getAttachmentsString());
 			message.setDocuments(sendMailDialog.getDocumentsString());
-			Optional<ITaskDescriptor> taskDescriptor = TaskUtil
+			taskDescriptor = TaskUtil
 					.createSendMailTaskDescriptor(sendMailDialog.getAccount().getId(), message);
-			if (taskDescriptor.isPresent()) {
+			ContextServiceHolder.get().getRootContext().setNamed("sendMailDialog.taskDescriptor", taskDescriptor.get());
+			if (!Boolean.valueOf(doSendString) && taskDescriptor.isPresent()) {
 				ITask task = new SendMailTaskWithProgress().execute(HandlerUtil.getActiveShell(event),
 						taskDescriptor.get());
 				return task.getState() == TaskState.COMPLETED;
