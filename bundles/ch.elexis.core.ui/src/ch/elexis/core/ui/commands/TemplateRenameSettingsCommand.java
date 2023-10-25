cd5e619631b537b77ffa835645b4423eaeb532ab
Victoria <Victoria@LAPTOP-LMK2LJMB>
2023-10-25 15:13:07 +0200

    [24131] adjustet the dialog so the OK can't be clicked on until there is some input in the intpufield

--- bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/commands/TemplateRenameSettingsCommand.java
+++ bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/commands/TemplateRenameSettingsCommand.java
@@ -4,7 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.IInputValidator;
 import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
@@ -25,55 +25,57 @@ public class TemplateRenameSettingsCommand extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-	    ISelection selection = HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().getSelection();
-	    if (selection != null) {
-	        IStructuredSelection strucSelection = (IStructuredSelection) selection;
-	        Object firstElement = strucSelection.getFirstElement();
-	        if (firstElement != null && firstElement instanceof TextTemplate) {
-	            TextTemplate textTemplate = (TextTemplate) firstElement;
+		ISelection selection = HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().getSelection();
+		if (selection != null) {
+			IStructuredSelection strucSelection = (IStructuredSelection) selection;
+			Object firstElement = strucSelection.getFirstElement();
+			if (firstElement != null && firstElement instanceof TextTemplate) {
+				TextTemplate textTemplate = (TextTemplate) firstElement;
 
-	            
-	            if (textTemplate.isSystemTemplate()) {
-	                MessageDialog.openWarning(Display.getCurrent().getActiveShell(), Messages.Template_not_editable,
-	                        Messages.Template_not_editable_text);
-	                return null;
-	            }
+				if (textTemplate.isSystemTemplate()) {
+					MessageDialog.openWarning(Display.getCurrent().getActiveShell(), Messages.Template_not_editable,
+							Messages.Template_not_editable_text);
+					return null;
+				}
 
-	            boolean shouldShowInputDialog = true;
+				boolean shouldShowInputDialog = true;
 
-	            while (shouldShowInputDialog) {
-	                InputDialog inputDialog = new InputDialog(Display.getCurrent().getActiveShell(), Messages.Edit_Template,
-	                        Messages.Edit_Template_Description, textTemplate.getName(), null);
+				while (shouldShowInputDialog) {
+					InputDialog inputDialog = new InputDialog(Display.getCurrent().getActiveShell(),
+							Messages.Edit_Template, Messages.Edit_Template_Description, textTemplate.getName(),
+							new NonEmptyInputValidator());
 
 					int inputDialogResult = inputDialog.open();
 
 					if (inputDialogResult == InputDialog.OK) {
-	                    String newName = inputDialog.getValue();
-	                    if (StringUtils.isNotBlank(newName)) {
-	                        Brief template = textTemplate.getTemplate();
-	                        if (template != null) {
-	                            template.set(Brief.FLD_SUBJECT, newName);
-	                            IWorkbenchPage page = HandlerUtil.getActiveWorkbenchWindow(event).getActivePage();
-	                            ContextServiceHolder.get().postEvent(ElexisEventTopics.EVENT_RELOAD, IDocumentLetter.class);
+						String newName = inputDialog.getValue();
+						if (StringUtils.isNotBlank(newName)) {
+							Brief template = textTemplate.getTemplate();
+							if (template != null) {
+								template.set(Brief.FLD_SUBJECT, newName);
+								IWorkbenchPage page = HandlerUtil.getActiveWorkbenchWindow(event).getActivePage();
+								ContextServiceHolder.get().postEvent(ElexisEventTopics.EVENT_RELOAD,
+										IDocumentLetter.class);
 								shouldShowInputDialog = false;
-	                        }
-	                    } else {
-							MessageDialog messageDialog = new MessageDialog(Display.getCurrent().getActiveShell(),
-									Messages.Template_not_editable, null,
-									Messages.Template_not_editable_empty, MessageDialog.WARNING,
-									new String[] { IDialogConstants.OK_LABEL }, 0);
-							int messageDialogResult = messageDialog.open();
-							if (messageDialogResult != IDialogConstants.OK_ID) {
-								shouldShowInputDialog = false; 
 							}
-	                    }
-	                } else {
-						shouldShowInputDialog = false; 
-	                }
-	            }
-	        }
-	    }
+						}
+					} else {
+						shouldShowInputDialog = false;
+					}
+				}
+			}
+		}
 
-	    return null;
+		return null;
+	}
+
+	private static class NonEmptyInputValidator implements IInputValidator {
+		@Override
+		public String isValid(String newText) {
+			if (newText.trim().isEmpty()) {
+				return "Das Feld darf nicht leer sein.";
+			}
+			return null; // Gültig
+		}
 	}
 }
