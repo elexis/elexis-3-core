d32d7a07ecd689448946186dd2339399f143c1f7
Victoria <Victoria@LAPTOP-LMK2LJMB>
2023-11-24 14:16:31 +0100

    [25871] added sortfunction to Behandlungsdiagnose view

--- bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/views/DiagnosenDisplay.java
+++ bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/views/DiagnosenDisplay.java
@@ -35,6 +35,8 @@ import org.eclipse.jface.viewers.ColumnWeightData;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerComparator;
 import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.dnd.DropTargetEvent;
@@ -42,6 +44,8 @@ import org.eclipse.swt.events.ControlAdapter;
 import org.eclipse.swt.events.ControlEvent;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
@@ -82,6 +86,7 @@ import ch.rgw.tools.StringTool;
 public class DiagnosenDisplay extends Composite implements IUnlockable {
 	private Table table;
 	private TableViewer viewer;
+	private boolean isPriceSortedAscending = true;
 
 	private final GenericObjectDropTarget dropTarget;
 
@@ -97,7 +102,8 @@ public class DiagnosenDisplay extends Composite implements IUnlockable {
 	private IEncounter actEncounter;
 	private ToolBar toolBar;
 	private TableColumnLayout tableLayout;
-
+	protected boolean isDescriptionSortedAscending;
+	protected boolean isCodeSortedAscending;
 	@Override
 	public void setEnabled(boolean enabled) {
 		toolBar.setEnabled(enabled);
@@ -345,6 +351,46 @@ public class DiagnosenDisplay extends Composite implements IUnlockable {
 				return StringTool.leer;
 			}
 		});
+
+
+		ViewerComparator descriptionComparator = new ViewerComparator() {
+			@Override
+			public int compare(Viewer viewer, Object e1, Object e2) {
+				if (e1 instanceof IDiagnosisReference && e2 instanceof IDiagnosisReference) {
+					IDiagnosisReference b1 = (IDiagnosisReference) e1;
+					IDiagnosisReference b2 = (IDiagnosisReference) e2;
+					return b1.getText().compareTo(b2.getText());
+				}
+				return 0;
+			}
+		};
+		col.getColumn().addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if (viewer.getComparator() == descriptionComparator) {
+					isDescriptionSortedAscending = !isDescriptionSortedAscending;
+					viewer.setComparator(new ViewerComparator() {
+						private boolean isDescriptionSortedAscending;
+
+						@Override
+						public int compare(Viewer viewer, Object e1, Object e2) {
+							if (e1 instanceof IDiagnosisReference && e2 instanceof IDiagnosisReference) {
+								IDiagnosisReference b1 = (IDiagnosisReference) e1;
+								IDiagnosisReference b2 = (IDiagnosisReference) e2;
+								int result = b1.getText().compareTo(b2.getText());
+								return isDescriptionSortedAscending ? result : -result;
+							}
+							return 0;
+						}
+					});
+				} else {
+					viewer.setComparator(descriptionComparator);
+					isDescriptionSortedAscending = true; // Zurücksetzen auf aufsteigende Sortierung
+				}
+				viewer.refresh();
+			}
+		});
+
 
 		col = createTableViewerColumn(titles[3], weights[3], 3, SWT.NONE);
 		col.setLabelProvider(new ColumnLabelProvider() {
