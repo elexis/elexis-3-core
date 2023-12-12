3584e22427a6bc154635a33ccb7563648c940606
thuster <huster@medevit.at>
2023-12-12 09:53:49 +0100

    [26017] create modifiable copy of run context for trigger sync task

--- bundles/ch.elexis.core.tasks/src/ch/elexis/core/tasks/internal/service/TaskServiceImpl.java
+++ bundles/ch.elexis.core.tasks/src/ch/elexis/core/tasks/internal/service/TaskServiceImpl.java
@@ -488,6 +488,8 @@ public class TaskServiceImpl implements ITaskService {
 		}
 		
 		if(sync) {
+			// create modifiable copy
+			runContext = new HashMap<String, String>(runContext);
 			runContext.put("isTriggerSync", Boolean.TRUE.toString());
 		}
 
