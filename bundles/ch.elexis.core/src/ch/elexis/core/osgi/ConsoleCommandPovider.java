74a29ef578986839d724c164e8727a530a23d07b
Marco Descher <marco@descher.at>
2024-01-17 18:32:46 +0100

    [25915] Fix ds consolecommandprovider

--- bundles/ch.elexis.core/src/ch/elexis/core/osgi/ConsoleCommandPovider.java
+++ bundles/ch.elexis.core/src/ch/elexis/core/osgi/ConsoleCommandPovider.java
@@ -4,6 +4,7 @@ import org.eclipse.equinox.console.completion.common.Completer;
 import org.eclipse.osgi.framework.console.CommandInterpreter;
 import org.eclipse.osgi.framework.console.CommandProvider;
 import org.osgi.framework.FrameworkUtil;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.runtime.ServiceComponentRuntime;
@@ -16,18 +17,23 @@ import ch.elexis.core.console.CmdAdvisor;
  * available console components.
  */
 @Component(service = { CommandProvider.class, Completer.class }, immediate = true)
-public class ConsoleCommandPovider extends AbstractConsoleCommandProvider implements Completer {
+public class ConsoleCommandPovider extends AbstractConsoleCommandProvider {
 
 	@Reference
 	private ServiceComponentRuntime scr;
 
-	@CmdAdvisor(description = "ds")
+	@Activate
+	public void activate() {
+		register(this.getClass());
+	}
+
+	@CmdAdvisor(description = "declarative services")
 	public void _ds(CommandInterpreter ci) {
 		ci.print(getHelp((String[]) null));
 	}
 
 	@CmdAdvisor(description = "list unsatisfied osgi services")
-	public String __ds_unsatisfied(String appointmentId, String patientId, String mandatorId) {
+	public String __ds_unsatisfied() {
 		return UnsatisfiedComponentUtil.listUnsatisfiedComponents(scr, FrameworkUtil.getBundle(getClass()));
 	}
 }
