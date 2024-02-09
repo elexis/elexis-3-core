acd854372d0d61667c164a087a7805bd7f7ec664
Olivier Debenath <olivier@debenath.ch>
2024-02-09 11:11:16 +0100

    Added comment

--- ch.elexis.sdk/qualifier-bump.sh
+++ ch.elexis.sdk/qualifier-bump.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# Script to enforce tycho to build new qualifiers for bundles/features.
+# It creates iteratively dummy file(s) within the bundle root and it commits the
+# new file(s). Subsequently the script remove all dummy files
+# and commits the changes again. Due to the new commit tycho will 
+# create new qualifiers. 
+
+# Step1 (dummy file(s) creation/first commit)
 for dir in ../bundles/*/
 do
 dir=${dir%*/}
@@ -9,6 +17,8 @@ echo "Add string..." > $dir/bumpit
 git add $dir/bumpit
 done
 git commit -m "Added dummy file"
+
+# Step2 (dummy file(s) removal/second commit)
 for dir in ../bundles/*/
 do
 dir=${dir%*/}
