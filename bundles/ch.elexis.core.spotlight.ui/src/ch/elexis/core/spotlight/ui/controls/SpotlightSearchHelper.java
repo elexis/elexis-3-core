19506ad785603a1f8f8dc77f9cc7a59b108affdb
Dalibor <Dalibor@LAPTOP-QRU70GCF>
2024-02-28 13:34:01 +0100

    [26204] Spotlight Fokussierung und Zentrierung

--- bundles/ch.elexis.core.spotlight.ui/src/ch/elexis/core/spotlight/ui/controls/SpotlightSearchHelper.java
+++ bundles/ch.elexis.core.spotlight.ui/src/ch/elexis/core/spotlight/ui/controls/SpotlightSearchHelper.java
@@ -29,6 +29,7 @@ public class SpotlightSearchHelper {
 	public static void highlightSearchText(StyledText styledText, String searchText) {
 		String fullText = styledText.getText().toLowerCase();
 		String[] searchWords = searchText.toLowerCase().split("\\s+");
+		int firstIndex = -1;
 		styledText.setStyleRange(null);
 		for (String word : searchWords) {
 			if (word.isEmpty())
@@ -40,8 +41,34 @@ public class SpotlightSearchHelper {
 				styleRange.length = word.length();
 				styleRange.background = Display.getCurrent().getSystemColor(SWT.COLOR_YELLOW);
 				styledText.setStyleRange(styleRange);
+				if (firstIndex == -1)
+					firstIndex = index;
 				index += word.length();
 			}
 		}
+		if (firstIndex != -1) {
+			centerTextAtPosition(styledText, firstIndex);
+		}
+	}
+
+	/**
+	 * Centers the StyledText widget's view on the line containing the specified
+	 * position. This method calculates the optimal scroll position to make the line
+	 * visible in the center of the visible area if possible.
+	 * 
+	 * @param styledText The StyledText widget to be scrolled.
+	 * @param position   The character position in the text to be centered in the
+	 *                   view.
+	 */
+	public static void centerTextAtPosition(StyledText styledText, int position) {
+		Display.getDefault().asyncExec(() -> {
+			if (!styledText.isDisposed()) {
+				int line = styledText.getLineAtOffset(position);
+				int linesVisible = styledText.getClientArea().height / styledText.getLineHeight();
+				int topLine = line - (linesVisible / 6);
+				styledText.setTopIndex(Math.max(topLine, 0));
+				styledText.redraw();
+			}
+		});
 	}
 }
