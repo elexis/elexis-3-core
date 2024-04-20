192c3107c14e6f926e062c4561b80f7709f58f2a
Marco Descher <marco@descher.at>
2024-04-20 11:23:20 +0200

    [26314] ElexisEnvironmentService -> RefreshAccessTokenTimerTask

---
+++ bundles/ch.elexis.core.services/src/ch/elexis/core/services/eenv/RefreshAccessTokenTimerTask.java
@@ -0,0 +1,93 @@
+package ch.elexis.core.services.eenv;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.TimerTask;
+
+import org.apache.commons.lang3.StringUtils;
+import org.keycloak.adapters.KeycloakDeployment;
+import org.keycloak.adapters.ServerRequest;
+import org.keycloak.adapters.ServerRequest.HttpFailure;
+import org.keycloak.representations.AccessTokenResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ch.elexis.core.common.ElexisEventTopics;
+import ch.elexis.core.eenv.AccessToken;
+import ch.elexis.core.model.IUser;
+import ch.elexis.core.services.IContextService;
+import ch.elexis.core.time.TimeUtil;
+
+public class RefreshAccessTokenTimerTask extends TimerTask {
+
+	private final KeycloakDeployment keycloakDeployment;
+	private final IContextService contextService;
+	private final Logger logger;
+
+	public RefreshAccessTokenTimerTask(KeycloakDeployment keycloakDeployment, IContextService contextService) {
+		this.keycloakDeployment = keycloakDeployment;
+		this.contextService = contextService;
+		this.logger = LoggerFactory.getLogger(getClass());
+	}
+
+	@Override
+	public void run() {
+		contextService.getTyped(AccessToken.class).ifPresent(this::refreshAccessToken);
+	}
+
+	private void refreshAccessToken(AccessToken accessToken) {
+
+		Optional<IUser> activeUser = contextService.getActiveUser();
+		if (!activeUser.isPresent()) {
+			contextService.removeTyped(AccessToken.class);
+			logger.warn("Found access-token but no active user. Removed access-token.");
+			return;
+		}
+
+		String activeUserId = activeUser.get().getId();
+		if (!StringUtils.equals(activeUserId, accessToken.getUsername())) {
+			contextService.removeTyped(AccessToken.class);
+			logger.warn("Found access-token for user [{}] but active user id [{}]. Removed access-token.",
+					accessToken.getUsername(), activeUserId);
+			return;
+		}
+
+		// TODO check validity - what if EE restarted??
+
+		long accessTokenTimeLeft = accessToken.getAccessTokenExpiration().getTime() - new Date().getTime();
+		if (accessTokenTimeLeft <= 60 * 1000) {
+			// we need to refresh the access-token
+			// 1) try via refreshToken if available and not expired
+			Date refreshTokenExpiration = accessToken.getAccessTokenExpiration();
+			if (refreshTokenExpiration != null) {
+				if (refreshTokenExpiration.getTime() > new Date().getTime()) {
+					try {
+						AccessTokenResponse invokeRefresh = ServerRequest.invokeRefresh(keycloakDeployment,
+								accessToken.getRefreshToken());
+						AccessToken _accessToken = AccessTokenUtil.load(invokeRefresh);
+						contextService.setTyped(_accessToken);
+						logger.info("RT Refreshed access-token for [{}], valid until [{}], refresh until [{}]",
+								activeUserId, TimeUtil.toLocalDateTime(_accessToken.getAccessTokenExpiration()),
+								TimeUtil.toLocalDateTime(_accessToken.refreshTokenExpiration()));
+						return;
+					} catch (IOException | HttpFailure e) {
+						logger.warn("Failed to refresh access-token via refresh-token", e);
+					}
+				}
+			}
+
+			// 2) via re-entering username password - we ask for re-login
+			Map<String, Object> eventMap = new HashMap<>();
+			eventMap.put(ElexisEventTopics.NOTIFICATION_PROPKEY_TITLE, "EE Login expired");
+			eventMap.put(ElexisEventTopics.NOTIFICATION_PROPKEY_MESSAGE,
+					"Your EE login expired, please re-login to Elexis");
+			logger.warn("Re-Login required, user informed");
+			contextService.sendEvent(ElexisEventTopics.BASE_NOTIFICATION + "warn", null, eventMap);
+		}
+
+	}
+
+}
--- bundles/ch.elexis.core/src/ch/elexis/core/eenv/AccessToken.java
+++ bundles/ch.elexis.core/src/ch/elexis/core/eenv/AccessToken.java
@@ -2,28 +2,43 @@ package ch.elexis.core.eenv;
 
 import java.util.Date;
 
+import ch.elexis.core.jdt.Nullable;
+
 public class AccessToken {
 
 	private final String token;
-	private final Date expirationTime;
+	private final Date accessTokenExpiration;
 	private final String username;
+	private final String refreshToken;
+	private final Date refreshTokenExpirationDate;
 
-	public AccessToken(String token, Date expirationTime, String username) {
+	public AccessToken(String token, Date accessTokenExpiration, String username, String refreshToken,
+			Date refreshTokenExpiration) {
 		this.token = token;
-		this.expirationTime = expirationTime;
+		this.accessTokenExpiration = accessTokenExpiration;
 		this.username = username;
+		this.refreshToken = refreshToken;
+		this.refreshTokenExpirationDate = refreshTokenExpiration;
 	}
 
 	public String getToken() {
 		return token;
 	}
 
-	public Date getExpirationTime() {
-		return expirationTime;
+	public Date getAccessTokenExpiration() {
+		return accessTokenExpiration;
 	}
 
 	public String getUsername() {
 		return username;
+	}
+
+	public @Nullable String getRefreshToken() {
+		return refreshToken;
+	}
+
+	public @Nullable Date refreshTokenExpiration() {
+		return refreshTokenExpirationDate;
 	}
 
 }
