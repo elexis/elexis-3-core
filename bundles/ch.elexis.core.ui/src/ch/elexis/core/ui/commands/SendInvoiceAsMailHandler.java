41a0f4503b9900caaabfd175defeafcbbac9c0ad
thuster <huster@medevit.at>
2024-06-04 11:15:11 +0200

    [26291] add noui rnoutput prop, keep orig pdf on send copy as mail

--- bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/commands/SendInvoiceAsMailHandler.java
+++ bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/commands/SendInvoiceAsMailHandler.java
@@ -2,6 +2,8 @@ package ch.elexis.core.ui.commands;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -22,15 +24,18 @@ import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.ui.handlers.IHandlerService;
 import org.slf4j.LoggerFactory;
 
+import ch.elexis.core.common.ElexisEventTopics;
 import ch.elexis.core.data.constants.ExtensionPointConstantsData;
 import ch.elexis.core.data.interfaces.IRnOutputter;
 import ch.elexis.core.data.util.NoPoUtil;
 import ch.elexis.core.mail.AttachmentsUtil;
 import ch.elexis.core.model.IInvoice;
+import ch.elexis.core.model.InvoiceConstants;
 import ch.elexis.core.preferences.PreferencesUtil;
 import ch.elexis.core.services.LocalConfigService;
 import ch.elexis.core.services.holder.ConfigServiceHolder;
 import ch.elexis.core.services.holder.ContextServiceHolder;
+import ch.elexis.core.services.holder.CoreModelServiceHolder;
 import ch.elexis.core.services.holder.VirtualFilesystemServiceHolder;
 import ch.elexis.core.utils.Extensions;
 import ch.elexis.data.Rechnung;
@@ -39,6 +44,8 @@ public class SendInvoiceAsMailHandler extends AbstractHandler {
 
 	private IRnOutputter pdfOutputter;
 
+	private List<File> movedExisting;
+
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		Optional<IInvoice> selectedInvoice = ContextServiceHolder.get().getTyped(IInvoice.class);
@@ -46,23 +53,27 @@ public class SendInvoiceAsMailHandler extends AbstractHandler {
 			// generate a pdf to send
 			Optional<IRnOutputter> pdfOutputter = getPdfOutputter();
 			if (pdfOutputter.isPresent()) {
-				List<File> pdfs = getPdfs(selectedInvoice.get());
-				
-				if (pdfs.isEmpty()) {
-					Properties outputProps = new Properties();
-					outputProps.put(IRnOutputter.PROP_OUTPUT_MODIFY_INVOICESTATE, Boolean.toString(false));
-					outputProps.put(IRnOutputter.PROP_OUTPUT_WITH_ESR, Boolean.toString(true));
-					outputProps.put(IRnOutputter.PROP_OUTPUT_WITH_RECLAIM, Boolean.toString(true));
-					outputProps.put(IRnOutputter.PROP_OUTPUT_WITH_MAIL, Boolean.toString(false));
+				moveExistingPfds(selectedInvoice.get());
 
-					pdfOutputter.get().doOutput(IRnOutputter.TYPE.ORIG, Collections.singletonList(
-							(Rechnung) NoPoUtil.loadAsPersistentObject(selectedInvoice.get())), outputProps);
-					pdfs = getPdfs(selectedInvoice.get());
-					if (pdfs.isEmpty()) {
-						MessageDialog.openError(HandlerUtil.getActiveShell(event), "Fehler",
-								"Konnte keine Pdfs für die Rechnung erstellen.");
-						return Boolean.FALSE;
-					}
+				Properties outputProps = new Properties();
+				outputProps.put(IRnOutputter.PROP_OUTPUT_NOUI, Boolean.toString(true));
+				outputProps.put(IRnOutputter.PROP_OUTPUT_MODIFY_INVOICESTATE, Boolean.toString(false));
+				outputProps.put(IRnOutputter.PROP_OUTPUT_WITH_ESR, Boolean.toString(true));
+				outputProps.put(IRnOutputter.PROP_OUTPUT_WITH_RECLAIM, Boolean.toString(true));
+				outputProps.put(IRnOutputter.PROP_OUTPUT_WITH_MAIL, Boolean.toString(false));
+
+				pdfOutputter.get().doOutput(IRnOutputter.TYPE.COPY,
+						Collections.singletonList((Rechnung) NoPoUtil.loadAsPersistentObject(selectedInvoice.get())),
+						outputProps);
+				List<File> pdfs = getPdfs(selectedInvoice.get());
+				if (pdfs.isEmpty()) {
+					restoreExistingPfds();
+					MessageDialog.openError(HandlerUtil.getActiveShell(event), "Fehler",
+							"Konnte keine Rechnungskopie Pdfs erstellen.");
+					return Boolean.FALSE;
+				} else {
+					pdfs = moveCopyPdfs(pdfs, selectedInvoice.get());
+					restoreExistingPfds();
 				}
 
 				if (!pdfs.isEmpty()) {
@@ -81,12 +92,17 @@ public class SendInvoiceAsMailHandler extends AbstractHandler {
 						}
 
 						params.put("ch.elexis.core.mail.ui.sendMail.subject", //$NON-NLS-1$
-								"Rechnung " + selectedInvoice.get().getNumber()); //$NON-NLS-1$
+								"Rechnungskopie " + selectedInvoice.get().getNumber()); //$NON-NLS-1$
 
 						ParameterizedCommand parametrizedCommmand = ParameterizedCommand
 								.generateCommand(sendMailCommand, params);
-						PlatformUI.getWorkbench().getService(IHandlerService.class).executeCommand(parametrizedCommmand,
-								null);
+						Object result = PlatformUI.getWorkbench().getService(IHandlerService.class)
+								.executeCommand(parametrizedCommmand, null);
+						if (Boolean.TRUE.equals(result)) {
+							selectedInvoice.get().addTrace(InvoiceConstants.OUTPUT, "Rechnungskopie per Mail");
+							CoreModelServiceHolder.get().save(selectedInvoice.get());
+							ContextServiceHolder.get().postEvent(ElexisEventTopics.EVENT_UPDATE, selectedInvoice.get());
+						}
 					} catch (Exception ex) {
 						throw new RuntimeException("ch.elexis.core.mail.ui.sendMail not found", ex); //$NON-NLS-1$
 					}
@@ -97,6 +113,67 @@ public class SendInvoiceAsMailHandler extends AbstractHandler {
 			}
 		}
 		return Boolean.TRUE;
+	}
+
+	private List<File> moveCopyPdfs(List<File> pdfs, IInvoice iInvoice) {
+		List<File> movedFiles = new ArrayList<>();
+		for (File file : pdfs) {
+			try {
+				if (file.getName().endsWith("_esr.pdf")) {
+					Path destination = file.toPath()
+							.resolveSibling(iInvoice.getNumber() + "_Rechnungskopie_Einzahlungsschein.pdf");
+					if (destination.toFile().exists()) {
+						destination.toFile().delete();
+					}
+					Files.move(file.toPath(), destination);
+					movedFiles.add(destination.toFile());
+				} else if (file.getName().endsWith("_rf.pdf")) {
+					Path destination = file.toPath()
+							.resolveSibling(iInvoice.getNumber() + "_Rechnungskopie_Formular.pdf");
+					if (destination.toFile().exists()) {
+						destination.toFile().delete();
+					}
+					Files.move(file.toPath(), destination);
+					movedFiles.add(destination.toFile());
+				}
+			} catch (IOException e) {
+				LoggerFactory.getLogger(getClass()).error("Error moving existing pdfs", e);
+			}
+		}
+		return movedFiles;
+	}
+
+	private void moveExistingPfds(IInvoice iInvoice) {
+		movedExisting = new ArrayList<>();
+		List<File> pdfs = getPdfs(iInvoice);
+		for (File file : pdfs) {
+			try {
+				Path destination = file.toPath().resolveSibling("existing_" + file.getName());
+				if (destination.toFile().exists()) {
+					destination.toFile().delete();
+				}
+				Files.move(file.toPath(), destination);
+				movedExisting.add(destination.toFile());
+			} catch (IOException e) {
+				LoggerFactory.getLogger(getClass()).error("Error moving existing pdfs", e);
+			}
+		}
+	}
+
+	private void restoreExistingPfds() {
+		if (movedExisting != null) {
+			for (File file : movedExisting) {
+				try {
+					Path destination = file.toPath().resolveSibling(file.getName().replace("existing_", ""));
+					if (destination.toFile().exists()) {
+						destination.toFile().delete();
+					}
+					Files.move(file.toPath(), destination);
+				} catch (IOException e) {
+					LoggerFactory.getLogger(getClass()).error("Error restoring existing pdfs", e);
+				}
+			}
+		}
 	}
 
 	private List<File> getPdfs(IInvoice iInvoice) {
