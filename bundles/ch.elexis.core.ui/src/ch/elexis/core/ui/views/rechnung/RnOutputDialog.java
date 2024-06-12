3b0dece8ff36c098936b7f2bb9447e6ccee55b29
Dalibor <Dalibor@LAPTOP-QRU70GCF>
2024-06-12 13:41:20 +0200

    [26344] Rechnungsausgabe Button Groesse

--- bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/views/rechnung/RnOutputDialog.java
+++ bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/views/rechnung/RnOutputDialog.java
@@ -191,6 +191,13 @@ public class RnOutputDialog extends TitleAreaDialog {
 	    return ret;
 	}
 
+	public void redrawLayout() {
+	    if (ret != null && !ret.isDisposed()) {
+	        ret.layout(true, true);
+	        ret.getParent().layout(true, true);
+	    }
+	}
+
 	public void resetCustomButtons() {
 		for (Control control : additionalButtonParent.getChildren()) {
 			if (control instanceof Button) {
@@ -222,7 +229,7 @@ public class RnOutputDialog extends TitleAreaDialog {
 	}
 
 	public void updateSize() {
-		getShell().setSize(getShell().getSize().x, 430);
+		getShell().setSize(getShell().getSize().x, 360);
 	}
 
 	@Override
