970f090ce334c62e89d64c5f8fd78fc2d039c4b5
Marco Descher <marco@descher.at>
2024-06-27 12:20:15 +0200

    [26453] PatientDetailCompositeUtil skip patient balance if postgres

--- tests/ch.elexis.core.services.test/src/ch/elexis/core/services/test/AllTests.java
+++ tests/ch.elexis.core.services.test/src/ch/elexis/core/services/test/AllTests.java
@@ -4,13 +4,15 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import ch.elexis.core.CoreUtilTest;
 import ch.elexis.core.TimeUtilTest;
 import ch.elexis.core.java.AllJavaTests;
 import ch.elexis.core.model.builder.AllBuilderTests;
 import ch.elexis.core.services.AllServiceTests;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TimeUtilTest.class, AllJavaTests.class, AllBuilderTests.class, AllServiceTests.class })
+@SuiteClasses({ TimeUtilTest.class, CoreUtilTest.class, AllJavaTests.class, AllBuilderTests.class,
+		AllServiceTests.class })
 public class AllTests {
 
 }
