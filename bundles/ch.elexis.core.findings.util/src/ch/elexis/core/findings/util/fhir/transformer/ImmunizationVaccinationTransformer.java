a2a3ac7e75112242cf596f3cd63a9484f1f42ea2
thuster <huster@medevit.at>
2024-07-31 11:16:52 +0200

    [26456] add fhir immunization attribute mapper, rework transformer

--- bundles/ch.elexis.core.findings.util/src/ch/elexis/core/findings/util/fhir/transformer/ImmunizationVaccinationTransformer.java
+++ bundles/ch.elexis.core.findings.util/src/ch/elexis/core/findings/util/fhir/transformer/ImmunizationVaccinationTransformer.java
@@ -1,21 +1,14 @@
 package ch.elexis.core.findings.util.fhir.transformer;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
-import org.apache.commons.lang3.StringUtils;
 import org.hl7.fhir.r4.model.CodeableConcept;
 import org.hl7.fhir.r4.model.Coding;
-import org.hl7.fhir.r4.model.DateTimeType;
 import org.hl7.fhir.r4.model.Immunization;
-import org.hl7.fhir.r4.model.Immunization.ImmunizationStatus;
 import org.hl7.fhir.r4.model.Type;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.slf4j.LoggerFactory;
 
@@ -24,8 +17,8 @@ import ca.uhn.fhir.rest.api.SummaryEnum;
 import ch.elexis.core.findings.util.fhir.IFhirTransformer;
 import ch.elexis.core.findings.util.fhir.MedicamentCoding;
 import ch.elexis.core.findings.util.fhir.transformer.helper.FhirUtil;
+import ch.elexis.core.findings.util.fhir.transformer.mapper.IVaccinationImmunizationAttributeMapper;
 import ch.elexis.core.model.IArticle;
-import ch.elexis.core.model.IMandator;
 import ch.elexis.core.model.IPatient;
 import ch.elexis.core.model.IVaccination;
 import ch.elexis.core.model.ModelPackage;
@@ -35,7 +28,6 @@ import ch.elexis.core.services.IContextService;
 import ch.elexis.core.services.IModelService;
 import ch.elexis.core.services.IQuery;
 import ch.elexis.core.services.IQuery.COMPARATOR;
-import ch.elexis.core.services.holder.CoreModelServiceHolder;
 import ch.elexis.core.utils.CoreUtil;
 
 @Component
@@ -51,63 +43,18 @@ public class ImmunizationVaccinationTransformer implements IFhirTransformer<Immu
 	@org.osgi.service.component.annotations.Reference
 	private ICodeElementService codeElemetService;
 
+	private IVaccinationImmunizationAttributeMapper attributeMapper;
+
+	@Activate
+	public void activate() {
+		attributeMapper = new IVaccinationImmunizationAttributeMapper(modelService);
+	}
+
 	@Override
 	public Optional<Immunization> getFhirObject(IVaccination localObject, SummaryEnum summaryEnum,
 			Set<Include> includes) {
 		Immunization fhirObject = new Immunization();
-		FhirUtil.setVersionedIdPartLastUpdatedMeta(Immunization.class, fhirObject, localObject);
-
-		fhirObject.addIdentifier(getElexisObjectIdentifier(localObject));
-
-		fhirObject.setStatus(ImmunizationStatus.COMPLETED);
-
-		fhirObject.setPatient(FhirUtil.getReference(localObject.getPatient()));
-
-		if (localObject.getPerformer() != null && localObject.getPerformer().isMandator()) {
-			IMandator mandator = CoreModelServiceHolder.get()
-					.load(localObject.getPerformer().getId(), IMandator.class).get();
-			fhirObject.addPerformer().setActor(FhirUtil.getReference(mandator));
-		}
-
-		StringBuilder textBuilder = new StringBuilder();
-
-		CodeableConcept vaccine = new CodeableConcept();
-		String gtin = localObject.getArticleGtin();
-		String atc = localObject.getArticleAtc();
-		String articelLabel = localObject.getArticleName();
-		if (gtin != null) {
-			Coding coding = vaccine.addCoding();
-			coding.setSystem(MedicamentCoding.GTIN.getOid());
-			coding.setCode(gtin);
-			coding.setDisplay(articelLabel);
-		}
-		if (atc != null) {
-			Coding coding = vaccine.addCoding();
-			coding.setSystem(MedicamentCoding.ATC.getOid());
-			coding.setCode(atc);
-		}
-		vaccine.setText(articelLabel);
-		textBuilder.append(articelLabel);
-		vaccine.setText(textBuilder.toString());
-		fhirObject.setVaccineCode(vaccine);
-
-		fhirObject.setLotNumber(localObject.getLotNumber());
-
-		if (StringUtils.isNotBlank(localObject.getIngredientsAtc())) {
-			CodeableConcept reasonCode = fhirObject.addReasonCode();
-			String[] parts = localObject.getIngredientsAtc().split(",");
-			for (String ingredientAtc : parts) {
-				Coding coding = reasonCode.addCoding();
-				coding.setSystem(MedicamentCoding.ATC.getOid());
-				coding.setCode(ingredientAtc);
-			}
-		}
-
-		LocalDate dateOfAdministration = localObject.getDateOfAdministration();
-		if (dateOfAdministration != null) {
-			Date date = Date.from(dateOfAdministration.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
-			fhirObject.setOccurrence(new DateTimeType(date));
-		}
+		attributeMapper.elexisToFhir(localObject, fhirObject, summaryEnum, includes);
 		return Optional.of(fhirObject);
 	}
 
@@ -127,8 +74,9 @@ public class ImmunizationVaccinationTransformer implements IFhirTransformer<Immu
 
 	@Override
 	public Optional<IVaccination> updateLocalObject(Immunization fhirObject, IVaccination localObject) {
-
-		return Optional.empty();
+		attributeMapper.fhirToElexis(fhirObject, localObject);
+		modelService.save(localObject);
+		return Optional.of(localObject);
 	}
 
 	@Override
@@ -151,45 +99,16 @@ public class ImmunizationVaccinationTransformer implements IFhirTransformer<Immu
 		// lookup patient
 		Optional<IPatient> patient = modelService.load(FhirUtil.getId(fhirObject.getPatient()).orElse(null),
 				IPatient.class);
-		Optional<IMandator> mandator = Optional.empty();
-		if (fhirObject.hasPerformer() && fhirObject.getPerformerFirstRep().hasActor()) {
-			mandator = modelService
-					.load(FhirUtil.getId(fhirObject.getPerformerFirstRep().getActor()).orElse(null), IMandator.class);
-		}
 		if (patient.isPresent()) {
 			IVaccination localObject = null;
 			if (item.isPresent()) {
-				localObject = new IVaccinationBuilder(modelService, contextService, item.get(), patient.get())
-					.build();
+				localObject = new IVaccinationBuilder(modelService, contextService, item.get(), patient.get()).build();
 			} else {
 				localObject = new IVaccinationBuilder(modelService, contextService,
 						displayName.orElse("unknown vaccine"), gtin.orElse(null), atc.orElse(null), patient.get())
 						.build();
 			}
-			if (mandator.isPresent()) {
-				localObject.setPerformer(mandator.get());
-			}
-
-			if (fhirObject.hasOccurrenceDateTimeType()) {
-				LocalDate occurance = LocalDateTime
-						.ofInstant(fhirObject.getOccurrenceDateTimeType().getValue().toInstant(),
-								ZoneId.systemDefault())
-						.toLocalDate();
-				localObject.setDateOfAdministration(occurance);
-			}
-
-			if (fhirObject.hasLotNumber()) {
-				localObject.setLotNumber(fhirObject.getLotNumber());
-			}
-
-			if (fhirObject.hasReasonCode()) {
-				List<String> atcCodes = FhirUtil.getCodesFromConceptList(MedicamentCoding.ATC.getOid(),
-						fhirObject.getReasonCode());
-				if (!atcCodes.isEmpty()) {
-					localObject.setIngredientsAtc(atcCodes.stream().collect(Collectors.joining(",")));
-				}
-			}
-
+			attributeMapper.fhirToElexis(fhirObject, localObject);
 			modelService.save(localObject);
 			return Optional.of(localObject);
 		} else {
