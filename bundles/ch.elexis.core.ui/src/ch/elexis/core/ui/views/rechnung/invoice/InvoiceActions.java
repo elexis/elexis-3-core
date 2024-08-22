7b018ec72bd9d8b47f50b8816b09c9502e989977
marcel7 <144210616+marcel7ag@users.noreply.github.com>
2024-08-22 15:59:35 +0200

    [26548] rechnungsliste view use new icon (#702)

--- bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/views/rechnung/invoice/InvoiceActions.java
+++ bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/views/rechnung/invoice/InvoiceActions.java
@@ -304,7 +304,7 @@ public class InvoiceActions {
 		exportListAction = new Action(Messages.RnActions_exportListAction) {
 			{
 				setToolTipText(Messages.RnActions_exportListTooltip);
-				setImageDescriptor(Images.IMG_EXPORT.getImageDescriptor());
+				setImageDescriptor(Images.IMG_DOCUMENT_EXCEL.getImageDescriptor());
 			}
 
 			@Override
