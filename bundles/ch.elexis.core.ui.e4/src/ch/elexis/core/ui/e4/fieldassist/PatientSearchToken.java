a592da5ab31ff78d989b18da7a7762ba11fef287
pdenzler <pdenzler@DESKTOP-1LRDH28>
2024-11-26 09:57:11 +0100

    [26773] contentProposal search for patient with multiple names

--- bundles/ch.elexis.core.ui.e4/src/ch/elexis/core/ui/e4/fieldassist/PatientSearchToken.java
+++ bundles/ch.elexis.core.ui.e4/src/ch/elexis/core/ui/e4/fieldassist/PatientSearchToken.java
@@ -16,8 +16,6 @@ public class PatientSearchToken {
 
 	private boolean ignore;
 
-	private boolean lastname;
-	private boolean firstname;
 	private boolean name;
 
 	private String token;
@@ -30,14 +28,6 @@ public class PatientSearchToken {
 
 	public void setIsIgnore() {
 		ignore = true;
-	}
-
-	public void setIsFirstname() {
-		firstname = true;
-	}
-
-	public void setIsLastname() {
-		lastname = true;
 	}
 
 	public void setIsName() {
@@ -54,17 +44,21 @@ public class PatientSearchToken {
 			query.and("code", COMPARATOR.EQUALS, token.substring(1));
 		} else if (isDate()) {
 			query.and("dob", COMPARATOR.LIKE, AsyncContentProposalProvider.getElexisDateSearchString(token));
-		} else if (lastname) {
-			query.and("description1", COMPARATOR.LIKE, token + "%", true);
-		} else if (firstname) {
-			query.and("description2", COMPARATOR.LIKE, token + "%", true);
 		} else if (name) {
-			query.startGroup();
-			query.or("description1", COMPARATOR.LIKE, token + "%", true);
-			query.or("description2", COMPARATOR.LIKE, token + "%", true);
-			query.andJoinGroups();
+			String[] tokens = token.toLowerCase().split(StringUtils.SPACE);
+			for (String t : tokens) {
+				String pattern = "%" + t + "%";
+				
+				query.startGroup();
+				query.or("description1", COMPARATOR.LIKE, pattern, true);
+				query.or("description2", COMPARATOR.LIKE, pattern, true);
+				query.andJoinGroups();
+			}
 		}
 	}
+
+
+
 
 	public boolean test(IPatient patient) {
 		if (isPatientCode()) {
@@ -77,13 +71,9 @@ public class PatientSearchToken {
 			} else {
 				return timestampFormat.format(patient.getDateOfBirth()).startsWith(compareToken);
 			}
-		} else if (lastname) {
-			return patient.getDescription1().toLowerCase().startsWith(token.toLowerCase());
-		} else if (firstname) {
-			return patient.getDescription2().toLowerCase().startsWith(token.toLowerCase());
 		} else if (name) {
-			return patient.getDescription1().toLowerCase().startsWith(token.toLowerCase())
-					|| patient.getDescription2().toLowerCase().startsWith(token.toLowerCase());
+			String fullName = patient.getDescription1().toLowerCase() + " " + patient.getDescription2().toLowerCase();
+			return fullName.startsWith(token.toLowerCase());
 		}
 		return false;
 	}
@@ -111,22 +101,9 @@ public class PatientSearchToken {
 
 	public static List<PatientSearchToken> getPatientSearchTokens(String[] split) {
 		if (split != null && split.length > 0) {
-			List<PatientSearchToken> tokens = Arrays.asList(split).stream().map(s -> PatientSearchToken.of(s))
+			List<PatientSearchToken> tokens = Arrays.stream(split).map(PatientSearchToken::of)
 					.collect(Collectors.toList());
-			// update name tokens
-			List<PatientSearchToken> nameTokens = tokens.stream().filter(st -> st.isNameToken())
-					.collect(Collectors.toList());
-			for (int i = 0; i < nameTokens.size(); i++) {
-				if (i == 0 && nameTokens.size() == 1) {
-					nameTokens.get(i).setIsName();
-				} else if (i == 0) {
-					nameTokens.get(i).setIsLastname();
-				} else if (i == 1) {
-					nameTokens.get(i).setIsFirstname();
-				} else {
-					nameTokens.get(i).setIsIgnore();
-				}
-			}
+			tokens.forEach(PatientSearchToken::setIsName);
 			return tokens.stream().filter(st -> !st.ignore()).collect(Collectors.toList());
 		}
 		return Collections.emptyList();
