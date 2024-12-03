b18007dd6e78fc1aad2b1c108fd64804eb88c071
pdenzler <pdenzler@DESKTOP-1LRDH28>
2024-12-03 16:49:14 +0100

    [26782] remove IOrderService changes

--- bundles/ch.elexis.core.services/src/ch/elexis/core/services/OrderService.java
+++ bundles/ch.elexis.core.services/src/ch/elexis/core/services/OrderService.java
@@ -3,22 +3,18 @@ package ch.elexis.core.services;
 import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 import ch.elexis.core.model.IArticle;
 import ch.elexis.core.model.IOrder;
 import ch.elexis.core.model.IOrderEntry;
-import ch.elexis.core.model.IStock;
 import ch.elexis.core.model.IStockEntry;
 import ch.elexis.core.model.OrderEntryState;
 import ch.elexis.core.services.IQuery.COMPARATOR;
+import ch.elexis.core.services.holder.CoreModelServiceHolder;
 import ch.elexis.core.services.holder.StoreToStringServiceHolder;
 
 @Component
 public class OrderService implements IOrderService {
-
-	@Reference(target = "(" + IModelService.SERVICEMODELNAME + "=ch.elexis.core.model)")
-	private IModelService modelService;
 
 	public IOrderEntry findOpenOrderEntryForStockEntry(IStockEntry stockEntry) {
 		IArticle article = stockEntry.getArticle();
@@ -26,7 +22,7 @@ public class OrderService implements IOrderService {
 			String[] articleStoreToStringParts = StoreToStringServiceHolder.getStoreToString(article)
 					.split(IStoreToStringContribution.DOUBLECOLON);
 
-			IQuery<IOrderEntry> query = modelService.getQuery(IOrderEntry.class);
+			IQuery<IOrderEntry> query = CoreModelServiceHolder.get().getQuery(IOrderEntry.class);
 			query.and("stockid", COMPARATOR.EQUALS, stockEntry.getStock().getId());
 			query.and("articleType", COMPARATOR.EQUALS, articleStoreToStringParts[0]);
 			query.and("articleId", COMPARATOR.EQUALS, articleStoreToStringParts[1]);
@@ -39,12 +35,6 @@ public class OrderService implements IOrderService {
 		return null;
 	}
 
-	public List<IOrderEntry> findOrderEntryForStock(IStock stock) {
-		IQuery<IOrderEntry> query = modelService.getQuery(IOrderEntry.class);
-		query.and("stockid", COMPARATOR.EQUALS, stock.getId());
-		return query.execute();
-	}
-
 	@Override
 	public IOrderEntry addRefillForStockEntryToOrder(IStockEntry ise, IOrder order) {
 		int current = ise.getCurrentStock();
@@ -55,7 +45,7 @@ public class OrderService implements IOrderService {
 		int toOrder = max - current;
 		if (toOrder > 0) {
 			IOrderEntry orderEntry = order.addEntry(ise.getArticle(), ise.getStock(), ise.getProvider(), toOrder);
-			modelService.save(orderEntry);
+			CoreModelServiceHolder.get().save(orderEntry);
 			return orderEntry;
 		}
 		return null;
