9fc974f701118c0aa58324b6d223d40f0da00e18
pdenzler <pdenzler@DESKTOP-1LRDH28>
2024-12-04 13:21:48 +0100

    [26782] rebase

--- bundles/ch.elexis.core/src/ch/elexis/core/services/IOrderService.java
+++ bundles/ch.elexis.core/src/ch/elexis/core/services/IOrderService.java
@@ -1,12 +1,23 @@
 package ch.elexis.core.services;
 
+import java.util.List;
+
 import ch.elexis.core.jdt.Nullable;
 import ch.elexis.core.model.IOrder;
 import ch.elexis.core.model.IOrderEntry;
+import ch.elexis.core.model.IStock;
 import ch.elexis.core.model.IStockEntry;
 
 public interface IOrderService {
 	public IOrderEntry findOpenOrderEntryForStockEntry(IStockEntry ise);
+
+	/**
+	 * Return list of all order entries associated with the specified stock.
+	 * 
+	 * @param stock
+	 * @return
+	 */
+	public List<IOrderEntry> findOrderEntryForStock(IStock stock);
 
 	/**
 	 * Refill the stock for a given stock entry. That is, create an order entry for
