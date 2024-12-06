7f785234f21e9c4ec30d63063d4a8e46acbd34fa
thuster <huster@medevit.at>
2024-12-06 14:53:14 +0100

    [26799] use OsgiServiceUtil to get CoreOperationAdvisor in holder

--- bundles/ch.elexis.core.data/src/ch/elexis/core/data/extension/CoreOperationAdvisorHolder.java
+++ bundles/ch.elexis.core.data/src/ch/elexis/core/data/extension/CoreOperationAdvisorHolder.java
@@ -1,24 +1,17 @@
 package ch.elexis.core.data.extension;
 
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
+import ch.elexis.core.utils.OsgiServiceUtil;
 
 /**
  * @since 3.8 replaced extension point based solution
  */
-@Component(immediate = true)
 public class CoreOperationAdvisorHolder {
 
 	private static ICoreOperationAdvisor coreOperationAdvisor;
 
-	@Reference
-	public void setModelService(ICoreOperationAdvisor coreOperationAdvisor) {
-		CoreOperationAdvisorHolder.coreOperationAdvisor = coreOperationAdvisor;
-	}
-
 	public static ICoreOperationAdvisor get() {
 		if (coreOperationAdvisor == null) {
-			throw new IllegalStateException("No coreOperationAdvisor available!");
+			coreOperationAdvisor = OsgiServiceUtil.getService(ICoreOperationAdvisor.class).orElseThrow();
 		}
 		return coreOperationAdvisor;
 	}
