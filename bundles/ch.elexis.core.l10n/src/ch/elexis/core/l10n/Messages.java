1f5115ddb5bdca42a383baa3fb52b2719b9d6d64
Dalibor Aksic <93644806+Daksic28@users.noreply.github.com>
2025-02-18 10:59:13 +0100

    [26860] GS1 Standard ZurRose Messages (#786)

--- bundles/ch.elexis.core.l10n/src/ch/elexis/core/l10n/Messages.java
+++ bundles/ch.elexis.core.l10n/src/ch/elexis/core/l10n/Messages.java
@@ -5138,6 +5138,8 @@ public class Messages extends NLS {
 	public static String PdfPreview_DocXError;
 	public static String ScreenshotFunction_PreviewShell_Text;
 	public static String ScreenshotFunction_PrintJob_Title;
+	public static String Attention;
+	public static String HttpOrderTransportService_Dialog;
 	static { // load message values from bundle file
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
 	}
