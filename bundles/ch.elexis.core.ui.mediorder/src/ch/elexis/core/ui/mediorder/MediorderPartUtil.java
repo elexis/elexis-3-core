2213fbc9ffbc729b07f1a8d0dd9f22972650c596
pdenzler <pdenzler@DESKTOP-1LRDH28>
2025-03-12 14:32:06 +0100

    [26808] take article from default stock

--- bundles/ch.elexis.core.ui.mediorder/src/ch/elexis/core/ui/mediorder/MediorderPartUtil.java
+++ bundles/ch.elexis.core.ui.mediorder/src/ch/elexis/core/ui/mediorder/MediorderPartUtil.java
@@ -6,13 +6,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import ch.elexis.core.model.IMandator;
 import ch.elexis.core.model.IOrderEntry;
 import ch.elexis.core.model.IPatient;
 import ch.elexis.core.model.IPrescription;
 import ch.elexis.core.model.IStock;
 import ch.elexis.core.model.IStockEntry;
 import ch.elexis.core.model.prescription.EntryType;
+import ch.elexis.core.services.IContextService;
 import ch.elexis.core.services.IModelService;
+import ch.elexis.core.services.IStockService;
 import ch.elexis.core.services.holder.MedicationServiceHolder;
 import ch.elexis.core.services.holder.OrderServiceHolder;
 import ch.elexis.core.services.holder.StockServiceHolder;
@@ -120,7 +123,15 @@ public class MediorderPartUtil {
 		return state;
 	}
 
-	public static void removeStockEntry(IStockEntry entry, IModelService coreModelService) {
+	public static void removeStockEntry(IStockEntry entry, IModelService coreModelService,
+			IContextService contextService, IStockService stockService) {
+		if (entry.getCurrentStock() > 0) {
+			String mandatorId = contextService.getActiveMandator().map(IMandator::getId).orElse(null);
+			if (mandatorId == null) {
+				return;
+			}
+			stockService.performSingleReturn(entry.getArticle(), entry.getCurrentStock(), mandatorId);
+		}
 		coreModelService.remove(entry);
 		IStock stock = entry.getStock();
 		if (stock.getStockEntries().isEmpty()) {
@@ -217,5 +228,72 @@ public class MediorderPartUtil {
 			}
 		}
 		return filteredList;
+	}
+
+	/**
+	 * transfers the given entry from the default stock to the patient stock if
+	 * entry is {@link MediorderEntryState#REQUESTED} and enough items are available
+	 * 
+	 * @param entry
+	 * @param stockService
+	 * @param coreModelService
+	 * @param contextService
+	 */
+	public static void automaticallyFromDefaultStock(IStockEntry entry, IStockService stockService,
+			IModelService coreModelService, IContextService contextService) {
+		IStockEntry defaultStockEntry = stockService.findStockEntryForArticleInStock(stockService.getDefaultStock(),
+				entry.getArticle());
+		if (defaultStockEntry == null || defaultStockEntry.getCurrentStock() == 0) {
+			return;
+		}
+		if (MediorderEntryState.REQUESTED.equals(MediorderPartUtil.determineState(entry))) {
+			int amount = Math.min(defaultStockEntry.getCurrentStock(), entry.getMinimumStock());
+			useFromDefaultStock(entry, defaultStockEntry, amount, stockService, coreModelService, contextService);
+		}
+	}
+
+	public static String[] createValuesArray(IStockEntry entry, IStockService stockService) {
+		IStockEntry defaultStockEntry = stockService.findStockEntryForArticleInStock(stockService.getDefaultStock(),
+				entry.getArticle());
+		int maxValue = Math.min(
+				(defaultStockEntry != null ? defaultStockEntry.getCurrentStock() : 0) + entry.getCurrentStock(),
+				entry.getMaximumStock());
+
+		List<String> values = new ArrayList<>();
+		for (int i = 0; i <= maxValue; i++) {
+			values.add(String.valueOf(i));
+		}
+		return values.toArray(new String[0]);
+	}
+
+	/**
+	 * updating the stock entry by adjusting its current stock based on the given
+	 * amount
+	 * 
+	 * @param entry
+	 * @param article
+	 * @param amount
+	 * @param stockService
+	 * @param coreModelService
+	 * @param contextService
+	 */
+	public static void useFromDefaultStock(IStockEntry entry, IStockEntry article, int amount,
+			IStockService stockService, IModelService coreModelService, IContextService contextService) {
+		String mandatorId = contextService.getActiveMandator().map(IMandator::getId).orElse(null);
+		if (mandatorId == null) {
+			return;
+		}
+
+		int difference = amount - entry.getCurrentStock();
+		if (difference > 0) {
+			stockService.performSingleDisposal(article.getArticle(), difference, mandatorId);
+		} else if (difference < 0) {
+			difference *= -1;
+			stockService.performSingleReturn(article.getArticle(), difference, mandatorId);
+		}
+
+		entry.setCurrentStock(amount);
+		coreModelService.save(article);
+		coreModelService.save(entry);
 	}
 }
