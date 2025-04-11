e118bce6b11e342e278730bb52f5854541d6f7a1
Dalibor Aksic <93644806+Daksic28@users.noreply.github.com>
2025-04-11 11:06:19 +0200

    [27039] Automatisches Setzen des aktiven Mandators (#816)

--- bundles/ch.elexis.core.l10n/src/ch/elexis/core/l10n/Messages.java
+++ bundles/ch.elexis.core.l10n/src/ch/elexis/core/l10n/Messages.java
@@ -5163,6 +5163,7 @@ public class Messages extends NLS {
 	public static String HttpOrderTransportService_OrderAlreadySent_Message;
 	public static String HttpOrderTransportService_LinkText;
 	public static String HttpOrderTransportService_LinkTooltip;
+	public static String UserCasePreferences_StammarztAutoMandant;
 	static { // load message values from bundle file
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
 	}
