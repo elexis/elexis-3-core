3c0e273f8ab55b244339b86eaede50f28d8465d3
thuster <huster@medevit.at>
2025-06-12 14:30:13 +0200

    [27068] add law to PhysioLeistung entity

--- bundles/ch.elexis.core.jpa.entities/src/ch/elexis/core/jpa/entities/PhysioLeistung.java
+++ bundles/ch.elexis.core.jpa.entities/src/ch/elexis/core/jpa/entities/PhysioLeistung.java
@@ -49,6 +49,9 @@ public class PhysioLeistung extends AbstractEntityWithId implements EntityWithId
 	@Column(length = 255)
 	private String titel;
 
+	@Column(length = 3)
+	private String law;
+
 	@Lob
 	private String description;
 
@@ -144,5 +147,13 @@ public class PhysioLeistung extends AbstractEntityWithId implements EntityWithId
 	@Override
 	public void setLastupdate(Long lastupdate) {
 		this.lastupdate = lastupdate;
+	}
+
+	public String getLaw() {
+		return law;
+	}
+
+	public void setLaw(String law) {
+		this.law = law;
 	}
 }
