1589dd4408ab625003658f23e4489d5fdf6953f6
pdenzler <patrick.denzler@medelexis.ch>
2025-08-06 16:08:34 +0200

    [27226] add isConfidential to email template

--- bundles/ch.elexis.core.mail/src/ch/elexis/core/mail/MailConstants.java
+++ bundles/ch.elexis.core.mail/src/ch/elexis/core/mail/MailConstants.java
@@ -2,4 +2,6 @@ package ch.elexis.core.mail;
 
 public class MailConstants {
 	public static final String TEXTTEMPLATE_SUBJECT = "subject";
+	public static final String CONFIDENTIAL_MAIL = "confidential";
+	public static final String CONFIDENTIAL_STRING = "(Vertraulich)";
 }
