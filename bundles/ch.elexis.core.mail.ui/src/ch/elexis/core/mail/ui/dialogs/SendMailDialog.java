29c5ce2c1c8cea9854db25fe42d067b68ae31900
Dalibor Aksic <93644806+Daksic28@users.noreply.github.com>
2025-08-08 10:22:29 +0200

    [27162] Agenda Dialog und Email Dialog angepasst (#852)

--- bundles/ch.elexis.core.mail.ui/src/ch/elexis/core/mail/ui/dialogs/SendMailDialog.java
+++ bundles/ch.elexis.core.mail.ui/src/ch/elexis/core/mail/ui/dialogs/SendMailDialog.java
@@ -69,6 +69,7 @@ import ch.elexis.core.model.IBlobSecondary;
 import ch.elexis.core.model.IContact;
 import ch.elexis.core.model.IDocument;
 import ch.elexis.core.model.IMandator;
+import ch.elexis.core.model.IPatient;
 import ch.elexis.core.model.ITextTemplate;
 import ch.elexis.core.services.ITextReplacementService;
 import ch.elexis.core.services.holder.ConfigServiceHolder;
@@ -106,6 +107,7 @@ public class SendMailDialog extends TitleAreaDialog {
 	private ComboViewer templatesViewer;
 	private boolean doSend = true;
 	private LocalDateTime sentTime;
+	private String patID;
 
 	public SendMailDialog(Shell parentShell) {
 		super(parentShell);
@@ -362,7 +364,13 @@ public class SendMailDialog extends TitleAreaDialog {
 
 			attachmentsSelection = new DocumentsSelectionComposite(container, SWT.NONE);
 			attachmentsSelection.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
-			attachmentsSelection.setPatient(ContextServiceHolder.get().getActivePatient().orElse(null));
+
+			if (patID != null) {
+				attachmentsSelection.setPatient(CoreModelServiceHolder.get().load(patID, IPatient.class).orElse(null));
+			} else {
+				attachmentsSelection.setPatient(ContextServiceHolder.get().getActivePatient().orElse(null));
+			}
+
 			attachmentsSelection.addDoubleClickListener(new IDoubleClickListener() {
 				@Override
 				public void doubleClick(DoubleClickEvent event) {
@@ -373,9 +381,9 @@ public class SendMailDialog extends TitleAreaDialog {
 				}
 			});
 	
-
 			if (!doSend) {
 				lbl.setVisible(false);
+				searchField.setVisible(false);
 				templatesViewer.getCombo().setVisible(false);
 				attachments.setVisible(false);
 				attachmentsSelection.setVisible(false);
@@ -461,7 +469,11 @@ public class SendMailDialog extends TitleAreaDialog {
 	public void doSend(boolean doSend) {
 		this.doSend = doSend;
 	}
-	
+
+	public void setPatID(String patID) {
+		this.patID = patID;
+	}
+
 	private List<String> getSendMailAccounts() {
 		List<String> ret = new ArrayList<>();
 		List<String> accounts = MailClientComponent.getMailClient().getAccountsLocal();
