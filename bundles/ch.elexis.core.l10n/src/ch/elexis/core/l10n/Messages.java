7fe1edb71d2d3aa7c338ee2c700cb0e3f97dc652
Dalibor Aksic <93644806+Daksic28@users.noreply.github.com>
2025-08-26 13:24:22 +0200

    [27242] Labor Spalte Entnahme-/Beobachtungszeitpunkt hinzugefügt (#859)

--- bundles/ch.elexis.core.l10n/src/ch/elexis/core/l10n/Messages.java
+++ bundles/ch.elexis.core.l10n/src/ch/elexis/core/l10n/Messages.java
@@ -5212,6 +5212,7 @@ public class Messages extends NLS {
 	public static String GoogleLoginDialog_Timeout;
 	public static String Omnivore_demo_outsource_disabled;
 	public static String Texterstellung_demo_browse_disabled;
+	public static String LaborOrdersComposite_columnObservationTime;	
 	static { // load message values from bundle file
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
 	}
