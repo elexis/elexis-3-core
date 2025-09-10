18f429fb395a015f01ac957ca0b871e25b97a1a7
Marco Descher <marco@descher.at>
2025-09-10 13:29:22 +0200

    [27297] work in progress

--- bundles/ch.elexis.core/src/ch/elexis/core/eenv/AccessToken.java
+++ bundles/ch.elexis.core/src/ch/elexis/core/eenv/AccessToken.java
@@ -1,12 +1,15 @@
 package ch.elexis.core.eenv;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import ch.elexis.core.jdt.Nullable;
 
 public class AccessToken {
 
 	private final String token;
+	private final Date tokenIssuedAt;
 	private final Date accessTokenExpiration;
 	private final String username;
 	private final String refreshToken;
@@ -14,20 +17,24 @@ public class AccessToken {
 	private final String tokenEndpoint;
 	private final String clientId;
 
-	public AccessToken(String token, Date accessTokenExpiration, String username, String refreshToken,
-			Date refreshTokenExpiration) {
-		this(token, accessTokenExpiration, username, refreshToken, refreshTokenExpiration, null, null);
+	private final Map<String, String> claims;
+
+	public AccessToken(String token, Date tokenIssuedAt, Date accessTokenExpiration, String username,
+			String refreshToken, Date refreshTokenExpiration) {
+		this(token, tokenIssuedAt, accessTokenExpiration, username, refreshToken, refreshTokenExpiration, null, null);
 	}
 
-	public AccessToken(String token, Date accessTokenExpiration, String username, String refreshToken,
-			Date refreshTokenExpiration, String tokenEndpoint, String clientId) {
+	public AccessToken(String token, Date tokenIssuedAt, Date accessTokenExpiration, String username,
+			String refreshToken, Date refreshTokenExpiration, String tokenEndpoint, String clientId) {
 		this.token = token;
+		this.tokenIssuedAt = tokenIssuedAt;
 		this.accessTokenExpiration = accessTokenExpiration;
 		this.username = username;
 		this.refreshToken = refreshToken;
 		this.refreshTokenExpirationDate = refreshTokenExpiration;
 		this.tokenEndpoint = tokenEndpoint;
 		this.clientId = clientId;
+		this.claims = new HashMap<String, String>();
 	}
 
 	public String getToken() {
@@ -50,12 +57,24 @@ public class AccessToken {
 		return refreshTokenExpirationDate;
 	}
 
+	public Date getTokenIssuedAt() {
+		return tokenIssuedAt;
+	}
+
 	public @Nullable String getClientId() {
 		return clientId;
 	}
 
 	public @Nullable String getTokenEndpoint() {
 		return tokenEndpoint;
+	}
+
+	public void addClaim(String claim, String value) {
+		claims.put(claim, value);
+	}
+
+	public @Nullable String getClaim(String claim) {
+		return claims.get(claim);
 	}
 
 }
