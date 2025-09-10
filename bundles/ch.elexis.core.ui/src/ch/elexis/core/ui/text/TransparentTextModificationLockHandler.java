113091f73ae3d87c1c9a9892a8d758c612e354fb
thuster <huster@medevit.at>
2025-09-10 15:16:45 +0200

    [27287] rework text modification lock release 60 sec after last key

--- bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/text/TransparentTextModificationLockHandler.java
+++ bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/text/TransparentTextModificationLockHandler.java
@@ -24,11 +24,13 @@ import ch.elexis.core.ui.locks.LockResponseHelper;
  */
 class TransparentTextModificationLockHandler implements VerifyKeyListener {
 
-	private static final long LOCK_WINDOW_SECS = 60;
+	private static final int LOCK_WINDOW_SECS = 60;
 
 	private Logger logger;
 	private ScheduledExecutorService executor;
 	private EnhancedTextField enhancedTextField;
+
+	private UnlockRunnable currentUnlockRunnable;
 
 	public TransparentTextModificationLockHandler(EnhancedTextField enhancedTextField) {
 		this.enhancedTextField = enhancedTextField;
@@ -45,30 +47,66 @@ class TransparentTextModificationLockHandler implements VerifyKeyListener {
 		}
 
 		final IEncounter encounter = enhancedTextField.getEncounter();
-		if (!enhancedTextField.isUnlocked() && encounter != null && !LocalLockServiceHolder.get().isLocked(encounter)) {
-			BusyIndicator.showWhile(enhancedTextField.getDisplay(), () -> {
-				final LockResponse lockResponse = LocalLockServiceHolder.get().acquireLockBlocking(encounter, 1,
-						new NullProgressMonitor());
-				if (!lockResponse.isOk()) {
-					event.doit = false;
-					// TODO reload kons! -> needs resp events
-					LockResponseHelper.showInfo(lockResponse, encounter, logger);
-					return;
+		if (encounter != null) {
+			if (LocalLockServiceHolder.get().isLockedLocal(encounter)) {
+				if (currentUnlockRunnable != null) {
+					currentUnlockRunnable.setSeconds(LOCK_WINDOW_SECS);
 				}
-				// lock event unlocks the text field, so we can directly execute the
-				// modification
-				executor.schedule(() -> {
+			} else {
+				BusyIndicator.showWhile(enhancedTextField.getDisplay(), () -> {
+					final LockResponse lockResponse = LocalLockServiceHolder.get().acquireLockBlocking(encounter, 1,
+							new NullProgressMonitor());
+					if (!lockResponse.isOk()) {
+						event.doit = false;
+						// TODO reload kons! -> needs resp events
+						LockResponseHelper.showInfo(lockResponse, encounter, logger);
+						return;
+					}
+				});
+				currentUnlockRunnable = new UnlockRunnable(encounter, LOCK_WINDOW_SECS);
+				executor.schedule(currentUnlockRunnable, 1, TimeUnit.SECONDS);
+			}
+		}
+		event.doit = true;
+	}
+
+	private class UnlockRunnable implements Runnable {
+
+		private IEncounter encounter;
+
+		private int seconds;
+
+		public UnlockRunnable(IEncounter encounter, int seconds) {
+			this.encounter = encounter;
+			this.seconds = seconds;
+			logger.info("LOCK [" + encounter.getId() + "]");
+		}
+		
+		public void setSeconds(int seconds) {
+			this.seconds = seconds;
+		}
+
+		@Override
+		public void run() {
+			if (LocalLockServiceHolder.get().isLockedLocal(encounter)) {
+				if (seconds > 0) {
+					seconds--;
+					executor.schedule(this, 1, TimeUnit.SECONDS);
+				} else {
 					// maybe it was already released
-					if (LocalLockServiceHolder.get().isLocked(encounter)) {
-						LockResponse releaseLock = LocalLockServiceHolder.get().releaseLock(lockResponse.getLockInfo());
+					if (LocalLockServiceHolder.get().isLockedLocal(encounter)) {
+						logger.info("UNLOCK [" + encounter.getId() + "]");
+						LockResponse releaseLock = LocalLockServiceHolder.get().releaseLock(encounter);
 						if (!releaseLock.isOk()) {
 							LockResponseHelper.showInfo(releaseLock, encounter, logger);
 						}
+					} else {
+						logger.info("NO LOCK [" + encounter.getId() + "]");
 					}
-				}, LOCK_WINDOW_SECS, TimeUnit.SECONDS);
-
-			});
+				}
+			} else {
+				logger.info("UNLOCKED [" + encounter.getId() + "]");
+			}
 		}
-		event.doit = true;
 	}
 }
