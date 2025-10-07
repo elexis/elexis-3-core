43579d746ec903c1e850188afd809c3a31ac1326
thuster <huster@medevit.at>
2025-10-07 15:07:14 +0200

    [27403] update MandantHelper due to user role changes

--- bundles/ch.elexis.core.findings.util/src/ch/elexis/core/findings/util/fhir/transformer/helper/MandantHelper.java
+++ bundles/ch.elexis.core.findings.util/src/ch/elexis/core/findings/util/fhir/transformer/helper/MandantHelper.java
@@ -10,10 +10,10 @@ public class MandantHelper extends AbstractHelper {
 
 	public CodeableConcept getPractitionerRoleCode(String roleId) {
 		CodeableConcept code = new CodeableConcept();
-		if ("assistant".equals(roleId)) {
+		if ("mpa".equals(roleId) || "mpk".equals(roleId)) {
 			code.addCoding(new Coding(PractitionerRole.NURSE.getSystem(), PractitionerRole.NURSE.toCode(),
 					PractitionerRole.NURSE.toCode()));
-		} else if ("doctor".equals(roleId)) {
+		} else if ("medical-practitioner".equals(roleId) || "mandator".equals(roleId)) {
 			code.addCoding(new Coding(PractitionerRole.DOCTOR.getSystem(), PractitionerRole.DOCTOR.toCode(),
 					PractitionerRole.DOCTOR.toCode()));
 		} else if ("executive_doctor".equals(roleId)) {
