fb7d5510772c3cf3fc8ba3a2c1442fcd2193afcc
Marco Descher <marco@descher.at>
2025-10-22 07:40:13 +0200

    [27425] NPE at UserManagementPreferencePage

--- bundles/ch.elexis.core.ee.openapi/src-gen/ch/myelexis/server/client/ApiException.java
+++ bundles/ch.elexis.core.ee.openapi/src-gen/ch/myelexis/server/client/ApiException.java
@@ -13,10 +13,10 @@
 
 package ch.myelexis.server.client;
 
-import java.util.List;
 import java.util.Map;
+import java.util.List;
 
-@jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2025-10-14T13:28:11.344655+02:00[Europe/Vienna]", comments = "Generator version: 7.16.0")
+@jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2025-10-22T15:12:38.209802+02:00[Europe/Vienna]", comments = "Generator version: 7.16.0")
 public class ApiException extends Exception {
     private static final long serialVersionUID = 1L;
 
