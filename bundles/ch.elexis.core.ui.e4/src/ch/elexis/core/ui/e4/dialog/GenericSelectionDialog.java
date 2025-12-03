d62fb4e14856a593288e1f2fd268d16a90c96438
Dalibor Aksic <dalibor.aksic@medelexis.ch>
2025-12-03 16:46:49 +0100

    [27654] TARDOC-Dignität suchleiste hinzugefügt

--- bundles/ch.elexis.core.ui.e4/src/ch/elexis/core/ui/e4/dialog/GenericSelectionDialog.java
+++ bundles/ch.elexis.core.ui.e4/src/ch/elexis/core/ui/e4/dialog/GenericSelectionDialog.java
@@ -12,6 +12,8 @@ import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
@@ -20,7 +22,9 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 
+import ch.elexis.core.l10n.Messages;
 import ch.elexis.core.model.Identifiable;
 
 /**
@@ -69,6 +73,12 @@ public class GenericSelectionDialog extends TitleAreaDialog {
 		setMessage(message);
 
 		Composite ret = (Composite) super.createDialogArea(parent);
+		ret.setLayout(new GridLayout(1, false));
+
+		Text txtSearch = new Text(ret, SWT.SEARCH | SWT.CANCEL | SWT.ICON_SEARCH | SWT.BORDER);
+		txtSearch.setMessage(Messages.AddLinkDialog_searchCaption);
+		txtSearch.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+
 		ScrolledComposite sc = new ScrolledComposite(ret, SWT.H_SCROLL | SWT.V_SCROLL);
 
 		Composite child = new Composite(sc, SWT.NONE);
@@ -82,6 +92,7 @@ public class GenericSelectionDialog extends TitleAreaDialog {
 		for (Object object : input) {
 			Button button = new Button(child, SWT.CHECK);
 			button.setText(getLabel(object));
+			button.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
 			button.addSelectionListener(new SelectionAdapter() {
 				@Override
 				public void widgetSelected(SelectionEvent e) {
@@ -101,6 +112,29 @@ public class GenericSelectionDialog extends TitleAreaDialog {
 		sc.setContent(child);
 
 		updateSelectionUi();
+
+		txtSearch.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				String pattern = txtSearch.getText().trim().toLowerCase();
+
+				for (Map.Entry<Object, Button> entry : buttonMap.entrySet()) {
+					Button b = entry.getValue();
+					String text = b.getText().toLowerCase();
+					boolean visible = pattern.isEmpty() || text.contains(pattern);
+
+					GridData gd = (GridData) b.getLayoutData();
+					if (gd == null) {
+						gd = new GridData(SWT.FILL, SWT.CENTER, true, false);
+						b.setLayoutData(gd);
+					}
+					gd.exclude = !visible;
+					b.setVisible(visible);
+				}
+				child.layout(true, true);
+				sc.setMinSize(child.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+			}
+		});
 
 		return ret;
 	}
