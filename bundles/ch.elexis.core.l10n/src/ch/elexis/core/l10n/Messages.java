cc9762cc39607229e71496d71ea8ef4238a8c052
thuster <huster@medevit.at>
2026-01-27 16:14:50 +0100

    [27916] add section code internationalization

--- bundles/ch.elexis.core.l10n/src/ch/elexis/core/l10n/Messages.java
+++ bundles/ch.elexis.core.l10n/src/ch/elexis/core/l10n/Messages.java
@@ -5331,7 +5331,9 @@ public class Messages extends NLS {
 	public static String OrderManagement_DeleteOrder_Message;
 	public static String MandatorFilter_lockedMandatorTooltip;
 	public static String TardocSpecialistComposite_selectDignitiesTitle;
+	public static String SectionCodeComposite_selectSectionCodeTitle;
 	public static String RechnungsPrefs_TardocSpecialistLink;
+	public static String RechnungsPrefs_SectionCodeLink;
 	public static String RechnungsPrefs_BillsElectronically;
 	public static String RechnungsPrefs_AddChildren;
 	public static String RechnungsPrefs_FixProviderGroupTitle;
