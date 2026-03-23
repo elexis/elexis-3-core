9038a97453b2fcc43ac7df0de1b51da6e2f47dbc
thuster <huster@medevit.at>
2026-03-23 13:27:45 +0100

    [28130] do not mark entry of invoice correction if info is empty

--- bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/views/rechnung/InvoiceCorrectionView.java
+++ bundles/ch.elexis.core.ui/src/ch/elexis/core/ui/views/rechnung/InvoiceCorrectionView.java
@@ -1077,7 +1077,8 @@ public class InvoiceCorrectionView extends ViewPart implements IUnlockable {
 				LeistungDTO leistungDTO = (LeistungDTO) element;
 				java.util.Optional<IInvoiceBillRecordInfo> invoiceBillRecordInfo = InvoiceServiceHolder.get()
 						.getInvoiceInvoiceBillRecordInfo(invoiceCorrectionDTO.getInvoice(), leistungDTO.getBilled());
-				if (invoiceBillRecordInfo.isPresent()) {
+				if (invoiceBillRecordInfo.isPresent()
+						&& StringUtils.isNotBlank(invoiceBillRecordInfo.get().getInfo())) {
 					return CoreUiUtil.getColorForString("FFDDDD");
 				}
 				return super.getBackground(element);
@@ -1088,7 +1089,8 @@ public class InvoiceCorrectionView extends ViewPart implements IUnlockable {
 				LeistungDTO leistungDTO = (LeistungDTO) element;
 				java.util.Optional<IInvoiceBillRecordInfo> invoiceBillRecordInfo = InvoiceServiceHolder.get()
 						.getInvoiceInvoiceBillRecordInfo(invoiceCorrectionDTO.getInvoice(), leistungDTO.getBilled());
-				if (invoiceBillRecordInfo.isPresent()) {
+				if (invoiceBillRecordInfo.isPresent()
+						&& StringUtils.isNotBlank(invoiceBillRecordInfo.get().getInfo())) {
 					return invoiceBillRecordInfo.get().getInfo();
 				}
 				return super.getToolTipText(element);
