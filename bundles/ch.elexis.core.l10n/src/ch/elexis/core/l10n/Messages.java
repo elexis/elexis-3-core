76b57786d9c17f59d8b1b29b9de7fecc88c8bc6e
Dalibor Aksic <93644806+Daksic28@users.noreply.github.com>
2026-04-15 09:44:38 +0200

    [28174] Global Inbox Internationalisierung (#920)

--- bundles/ch.elexis.core.l10n/src/ch/elexis/core/l10n/Messages.java
+++ bundles/ch.elexis.core.l10n/src/ch/elexis/core/l10n/Messages.java
@@ -5558,6 +5558,7 @@ public class Messages extends NLS {
 	public static String ImageViewAll_EnterNewFilename;
 	public static String ImageViewAll_PleaseEnterNewFilename;
 	public static String AgendaDefinitionen_useColorizedPatientSearch;
+	public static String PreferencesServer_suffixModeLabel;
 
 
 
