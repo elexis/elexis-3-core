5baf0cf92ab03280c4ef4ceddedb1219ed4d519c
thuster <huster@medevit.at>
2026-09-14 14:32:54 +0200

    [28468] use separate AL TL scaling factors from billed

--- bundles/ch.elexis.core.jpa.entities/src/ch/elexis/core/jpa/entities/Verrechnet.java
+++ bundles/ch.elexis.core.jpa.entities/src/ch/elexis/core/jpa/entities/Verrechnet.java
@@ -24,7 +24,9 @@ import jakarta.persistence.Table;
 public class Verrechnet extends AbstractEntityWithId implements EntityWithId, EntityWithDeleted, EntityWithExtInfo {
 
 	public static final String EXT_VERRRECHNET_TL = "TL"; //$NON-NLS-1$
+	public static final String EXT_VERRRECHNET_TL_SCALE = "TL_SCALE"; //$NON-NLS-1$
 	public static final String EXT_VERRRECHNET_AL = "AL"; //$NON-NLS-1$
+	public static final String EXT_VERRRECHNET_AL_SCALE = "AL_SCALE"; //$NON-NLS-1$
 
 	// Transparently updated by the EntityListener
 	protected Long lastupdate;
@@ -83,7 +85,7 @@ public class Verrechnet extends AbstractEntityWithId implements EntityWithId, En
 		if (getScale() == 0) {
 			return 1.0;
 		}
-		return ((double) getScale()) / 100.0;
+		return (getScale()) / 100.0;
 	}
 
 	@Transient
@@ -97,7 +99,7 @@ public class Verrechnet extends AbstractEntityWithId implements EntityWithId, En
 		if (getScale2() == 0) {
 			return 1.0;
 		}
-		return ((double) getScale2()) / 100.0;
+		return (getScale2()) / 100.0;
 	}
 
 	@Transient
